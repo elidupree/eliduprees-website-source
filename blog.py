37ff3fe8c1cd1b477e8e11dd7e4a32a156d965d3
Eli Dupree <vcs@elidupree.com>
2013-06-17 18:20:01 -0400

    Some unsuccessful attempts to reformat the blog page

--- blog.py
+++ blog.py
@@ -3,16 +3,21 @@ import css
 import html_pages
 import top_bar
 
-css.insert("div.blog_post { margin-left:auto; margin-right:auto; border-top:1em solid gray; min-width:20em; max-width:35em; padding:0.75em; background-color:white; }")
+css.insert("div.blog_post { border-top:1em solid gray; min-width:20em; max-width:35em; padding:0.75em; background-color:white; }")
 def fake_post():
   return '''<div class="blog_post"><h1>Post title</h1><p>Lorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum</p><p>dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conv</p></div>'''
 
-css.insert("div.blog_leftbar_thingy { float:right; width:13.75em; padding:0.75em; border:1.5em solid gray; background-color:white; }")
-leftbar_thingy = '''<div class="blog_leftbar_thingy"><a href="/403">[Random post] I foobar yesterday</a><p>something else</p></div>'''
+css.insert("div.blog_right_bar { display:inline-block; vertical-align:top; width:13.75em; padding:0.75em; border:1.5em solid gray; background-color:white; }")
+right_bar = '''<div class="blog_right_bar"><a href="/403">[Random post] I foobar yesterday</a><p>something else</p></div>'''
 
-css.insert("div.blogstream { margin-left:auto; margin-right:auto; max-width:75em; background-color:gray; }")
-blog = '''<div class="blogstream">
-'''+leftbar_thingy+fake_post()+fake_post()+fake_post()+fake_post()+fake_post()+fake_post()+'''</div>'''
+css.insert("div.blog_left_padding { display:inline-block; max-width:12.75em; padding:0.75em; border:1.5em solid gray; background-color:white; }")
+left_padding = '''<div class="blog_left_padding">dfjkdf kdfkjfdjkdfkj dfjkdf jkdf dfjk dfjk dfjkdf jkdf djfk dfkjdf kjdf jkfd dfkj dfjk dfjkdf jkdf kdfj dfjk</div>'''
+
+
+css.insert("div.blog_stream { display:inline-block; min-width:20em; max-width:35em; }")
+css.insert("div.blog_page { margin-left:auto; margin-right:auto; max-width:75em; background-color:gray; }")
+blog = '''<div class="blog_page">'''+left_padding+'''<div class="blog_stream">
+'''+fake_post()+fake_post()+fake_post()+fake_post()+fake_post()+fake_post()+'''</div>'''+right_bar+'''</div>'''
 
 
 def show_blog():
