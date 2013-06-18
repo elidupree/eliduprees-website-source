6edd5f223ac5955b6371dc5c03a72c35f439bf87
Eli Dupree <vcs@elidupree.com>
2013-06-18 11:54:07 -0400

    reformatted top bar a bunch

--- top_bar.py
+++ top_bar.py
@@ -4,12 +4,17 @@ import css
 
 category_width = 10
 num_categories = 5
-all_categories_width = category_width * num_categories
 category_border_width = .8
 category_image_width = category_width - category_border_width
+all_categories_width = category_width * num_categories - category_border_width
 bar_height = 3
-home_width = 10
+home_image_width = bar_height
+home_text_width_twolines = 7
+home_width = home_image_width + home_text_width_twolines
+login_width = 5
 categories_hit_home_width = all_categories_width + 2*home_width
+categories_hit_login_width = all_categories_width + home_width + login_width
+categories_hit_home_image_width = all_categories_width + 2*home_image_width
 
 button_border_radius = 0.5
 
@@ -21,28 +26,31 @@ div.top_bar {
   font-family: Arial, Helvetica, sans-serif;
   background-color:black;
   background-image: url("/top-bar-background.png");
-  background-size: 100% '''+str(bar_height)+'''em; }
+  background-size: 100% 100%; }
 
 div.top_bar_home {
   position:absolute;
   width:'''+str(home_width)+'''em; height:'''+str(bar_height)+'''em; }
-img.top_bar_home_image{
+img.top_bar_home_image {
   vertical-align:top;
   display:inline-block;
-  width:'''+str(bar_height)+'''em; height:'''+str(bar_height)+'''em }
+  width:'''+str(home_image_width)+'''em; height:'''+str(bar_height)+'''em; }
 span.top_bar_home_text {
   padding: 0.25em 0em;
   display:inline-block;
+  width:'''+str(home_text_width_twolines - 1)+'''em; height:'''+str(bar_height - 0.5)+'''em;
   color:black; }
 
 div.top_bar_categories {
   margin-left:auto; margin-right:auto;
-  width:'''+str(all_categories_width)+'''em; height:'''+str(bar_height-.5)+'''em;
+  max-width:'''+str(all_categories_width)+'''em; height:'''+str(bar_height-.5)+'''em;
   background-color: transparent;
   border-top: 0.25em solid transparent; border-bottom: 0.25em solid transparent; }
 a.top_bar_category_link {
   display:inline-block;
-  margin-right:'''+str(category_border_width)+'''em; }
+  border-right:'''+str(category_border_width)+'''em solid transparent; }
+a.top_bar_category_link.far_right {
+  border-right:0; }
 a:link.top_bar_category_link{ color:yellow }
 a:visited.top_bar_category_link{ color:orange }
 span.top_bar_category {
@@ -53,7 +61,7 @@ span.top_bar_category {
   background-image: url("http://deqyc5bzdh53a.cloudfront.net/biscuits_2013_06_16.jpg");
   background-size:'''+str(category_image_width)+'''em '''+str(bar_height-.5)+'''em;
   border-radius:'''+str(button_border_radius)+'''em;
-  margin-bottom:-1em; } /* the negative margin-bottom is to eliminate a gutter whose origins we don't understand */
+  vertical-align: bottom; } /* since it's an inline-block, we need this to stop it from creating a gutter for potential descenders */
 span.top_bar_category_text{
   position:absolute; display:block;
   bottom:0; right:0; width:5em;
@@ -65,24 +73,39 @@ span.top_bar_category_text{
 div.top_bar_login {
   position:absolute;
   right:0; top:0;
-  height:'''+str(bar_height)+'''em;
-  padding: 0.25em 0.5em }
+  width:'''+str(login_width - 1)+'''em; height:'''+str(bar_height - 0.5)+'''em;
+  padding: 0.25em 0.5em;
+  text-align: right; }
 a.top_bar_login_link {
   color:black; text-decoration:underline; }
 
 @media screen and (max-width: '''+str(categories_hit_home_width)+'''em) {
   div.top_bar_categories {
-    display: inline-block;
     margin-left:'''+str(home_width)+'''em; margin-right:0;
   }
+}
+@media screen and (max-width: '''+str(categories_hit_login_width)+'''em) {
+  div.top_bar_categories {
+    
+    margin-left:auto; margin-right:auto;
+    margin-top: 1.5em; }
   div.top_bar_home {
-    float: none;
-  }
+    width: 20em; }
+  span.top_bar_home_text {
+    width: 15em; }
+  div.top_bar_login {
+    width: 20em; }
+}
+@media screen and (max-width: '''+str(categories_hit_home_image_width)+'''em) {
+  img.top_bar_home_image {
+    vertical-align:top;
+    display:inline-block;
+    width:1.5em; height:1.5em; }
 }
 ''')
 
 def home_string(you_are_here):
-  return '''<div class="top_bar_home"><a href="/"><img class="top_bar_home_image" src="/site-logo.png" /><span class="top_bar_home_text">Eli Dupree's<br/>website</span></a></div>'''
+  return '''<div class="top_bar_home"><a href="/"><img class="top_bar_home_image" src="/site-logo.png" /><span class="top_bar_home_text">Eli Dupree's website</span></a></div>'''
 def games_string(you_are_here):
   return '''<a class="top_bar_category_link" href="/games"><span class="top_bar_category"><span class="top_bar_category_text">Games</span></span></a>'''
 def comics_string(you_are_here):
@@ -92,7 +115,7 @@ def pacman_string(you_are_here):
 def blog_string(you_are_here):
   return '''<a class="top_bar_category_link" href="/blog"><span class="top_bar_category"><span class="top_bar_category_text">Blog</span></span></a>'''
 def shop_string(you_are_here):
-  return '''<a class="top_bar_category_link" href="https://secure.elidupree.com/shop"><span class="top_bar_category"><span class="top_bar_category_text">Shop</span></span></a>'''
+  return '''<a class="top_bar_category_link far_right" href="https://secure.elidupree.com/shop"><span class="top_bar_category"><span class="top_bar_category_text">Shop</span></span></a>'''
 
 def categories_wrap(contents):
   return '<div class="top_bar_categories">'+contents+'</div>'
@@ -108,5 +131,5 @@ def top_bar(category):
   pacman = pacman_string(category == "pacman")
   blog   =   blog_string(category ==   "blog")
   shop   =   shop_string(category ==   "shop")
-  login  = '''<div class="top_bar_login"><a class="top_bar_login_link" href="">Login /<br/>Register</a></div>'''
+  login  = '''<div class="top_bar_login"><a class="top_bar_login_link" href="">Login / Register</a></div>'''
   return bar_wrap(home+categories_wrap(games+comics+pacman+blog+shop)+login)
