f82dba7014dfaea169d52cecf5d839c2f02934e8
Eli Dupree <vcs@elidupree.com>
2013-06-17 22:15:17 -0400

    Updated the top bar a bunch

--- top_bar.py
+++ top_bar.py
@@ -2,20 +2,24 @@
 
 import css
 
-css.insert("div.top_bar_category{ display:inline-block; width:10em; text-align:center; background-color:red; }")
+css.insert('''a.top_bar_category_link{ display:inline-block; }
+a:link.top_bar_category_link{ color:yellow }
+a:visited.top_bar_category_link{ color:orange }''')
+css.insert('''span.top_bar_category{ position:relative; display:inline-block; width:9em; height:3em; border-right:1em solid black; text-align:center; background-color:red; background-image: url("http://deqyc5bzdh53a.cloudfront.net/biscuits_2013_06_16.jpg"); background-size: 9em 3em; margin-bottom:-1em; }''') # the negative margin-bottom is to eliminate a gutter whose origins we don't understand
+css.insert("span.top_bar_category_text{ position:absolute; display:block; bottom:0; background-color:black; font-size:120%; font-weight:bold; text-decoration:underline; width:5em; right:0; }")
 
 css.insert("div.top_bar_home{ float:left; background-color:black; color:yellow; }")
 css.insert("img.top_bar_home_image{ width:3em; height:3em }")
 def home_string(you_are_here):
-  return '''<div class="top_bar_home"><a href="/"><img class="top_bar_home_image" src="http://www.elidupree.com/main/avatars/Eli_Dupree.jpg" /> Eli Dupree's Website</a></div>'''
+  return '''<div class="top_bar_home"><a class="top_bar_category_link" href="/"><img class="top_bar_home_image" src="http://www.elidupree.com/main/avatars/Eli_Dupree.jpg" /> Eli Dupree's Website</a></div>'''
 def games_string(you_are_here):
-  return '''<div class="top_bar_category"><a href="/games">Games</a></div>'''
+  return '''<a class="top_bar_category_link" href="/games"><span class="top_bar_category"><span class="top_bar_category_text">Games</span></span></a>'''
 def comics_string(you_are_here):
-  return '''<div class="top_bar_category"><a href="/comics">Comics</a></div>'''
+  return '''<a class="top_bar_category_link" href="/comics"><span class="top_bar_category"><span class="top_bar_category_text">Comics</span></span></a>'''
 def blog_string(you_are_here):
-  return '''<div class="top_bar_category"><a href="/blog">Blog</a></div>'''
+  return '''<a class="top_bar_category_link" href="/blog"><span class="top_bar_category"><span class="top_bar_category_text">Blog</span></span></a>'''
 def shop_string(you_are_here):
-  return '''<div class="top_bar_category"><a href="https://secure.elidupree.com/shop">Shop</a></div>'''
+  return '''<a class="top_bar_category_link" href="https://secure.elidupree.com/shop"><span class="top_bar_category"><span class="top_bar_category_text">Shop</span></span></a>'''
 
 css.insert("div.top_bar_categories{ margin-left:auto; margin-right:auto; max-width:40em; background-color:orange; height:3em; }")
 def categories_wrap(contents):
