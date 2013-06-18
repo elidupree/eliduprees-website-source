d39a54fdc516f39f56ec974e34320a92797981c1
Eli Dupree <vcs@elidupree.com>
2013-06-17 20:17:49 -0400

    Also made the right margin go away, through great cleverness

--- blog.py
+++ blog.py
@@ -16,13 +16,22 @@ right_bar_padded_width = max_side_space - 2*(min_side_space_for_post)
 right_bar_content_width = right_bar_padded_width - (2*text_padding_width)
 
 min_space_for_full_post_width = min_side_space_for_post + post_padded_max_width + max_side_space
-min_space_for_two_columns = min_side_space_for_post + post_padded_min_width + max_side_space
+min_space_for_two_columns = post_padded_min_width + max_side_space - min_side_space_for_post
 
 css.insert("div.blog_post { border-top:"+str(post_separation)+"em solid gray; padding:"+str(text_padding_width)+"em; background-color:white; }")
 def fake_post():
   return '''<div class="blog_post"><h1>Post title</h1><p>Lorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum</p><p>dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conLorem ipsum dolor sit amet, conv</p></div>'''
 
-css.insert("div.blog_right_bar { display:inline-block; vertical-align:top; width:"+str(right_bar_content_width)+"em; padding:"+str(text_padding_width)+"em; border:1.5em solid gray; border-left:0; border-right:"+str(min_side_space_for_post)+"em solid gray; background-color:white; }")
+css.insert('''
+div.blog_right_bar {
+  display:inline-block;
+  vertical-align:top;
+  width:'''+str(right_bar_content_width)+'''em;
+  padding:'''+str(text_padding_width)+'''em;
+  border:1.5em solid gray;
+  border-left:0;
+  border-right:'''+str(min_side_space_for_post)+'''em solid gray;
+  background-color:white; }''')
 right_bar = '''<div class="blog_right_bar"><a href="/403">[Random post] I foobar yesterday</a><p>something else</p></div>'''
 
 
@@ -34,14 +43,20 @@ div.blog_stream { display:inline-block;
   border-right:'''+str(min_side_space_for_post)+'''em solid gray; }
 div.blog_stream_and_right_bar { float:right; }
 div.blog_bottom { clear:both; }
-div.blog_page { margin-left:auto; margin-right:auto; min-width:'''+str(min_space_for_two_columns - min_side_space_for_post)+'''em; max-width:'''+str(page_max_width)+'''em; background-color:gray; }
+div.blog_page { margin-left:auto; margin-right:auto; min-width:'''+str(min_space_for_two_columns)+'''em; max-width:'''+str(page_max_width)+'''em; background-color:gray; }
 
 @media screen and (max-width: '''+str(min_space_for_full_post_width)+'''em) {
   div.blog_stream    { margin-right:'''+str(max_side_space-min_side_space_for_post)+'''em; }
   div.blog_right_bar { margin-left:-'''+str(max_side_space-min_side_space_for_post)+'''em; }
 }
-@media screen and (max-width: '''+str(min_space_for_two_columns)+'''em) {
+@media screen and (max-width: '''+str(min_space_for_two_columns+2*min_side_space_for_post)+'''em) {
   div.blog_stream { border-left:0; width:'''+str(post_padded_min_width)+'''em; }
+  div.blog_right_bar { border-right:0; }
+}
+@media screen and (max-width: '''+str(min_space_for_two_columns+1*min_side_space_for_post)+'''em) {
+  div.blog_stream_and_right_bar { float:none; }
+  div.blog_stream    { margin-right:0; }
+  div.blog_right_bar { margin-left:0; }
 }
 ''')
 
