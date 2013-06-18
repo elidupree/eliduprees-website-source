b10a062896472908e0c5bb8c15fb46556675e3dd
Eli Dupree <vcs@elidupree.com>
2013-06-18 14:36:32 -0400

    Switched blog page to use margins instead of transparent borders.

--- blog.py
+++ blog.py
@@ -22,18 +22,22 @@ min_space_for_two_columns_and_all_but_left_margin = min_space_for_two_columns_an
 min_space_for_two_columns_and_all_but_side_margins = min_space_for_two_columns_and_all_but_left_margin - min_side_space_for_post
 min_space_for_two_columns_with_least_margins = min_space_for_two_columns_and_all_but_side_margins - min_side_space_for_post + maximally_pinched_margin
 
-background_color = "#bbbb44";
+background_color = "#80f580";
 
 css.insert('''
 
-div.blog_page { margin-left:auto; margin-right:auto; min-width:'''+str(min_space_for_two_columns_with_least_margins)+'''em; max-width:'''+str(page_max_width)+'''em; background-color:'''+background_color+'''; }
+div.blog_page {
+  margin-left:auto; margin-right:auto;
+  min-width:'''+str(min_space_for_two_columns_with_least_margins)+'''em;
+  max-width:'''+str(page_max_width)+'''em;
+  background-color:'''+background_color+'''; }
 
 div.blog_stream_and_right_bar { float:right; }
 div.blog_stream { display:inline-block;
   min-width:'''+str(post_padded_min_width)+'''em;
   max-width:'''+str(post_padded_max_width)+'''em;
-  border-left:''' +str(min_side_space_for_post)+'''em solid transparent;
-  border-right:'''+str(min_side_space_for_post)+'''em solid transparent; }
+  margin-left:''' +str(min_side_space_for_post)+'''em;
+  margin-right:'''+str(min_side_space_for_post)+'''em; }
 div.blog_bottom { clear:both; }
 
 div.blog_right_bar {
@@ -41,36 +45,36 @@ div.blog_right_bar {
   vertical-align:top;
   width:'''+str(right_bar_content_width)+'''em;
   padding:'''+str(text_padding_width)+'''em;
-  border-top:1.5em solid '''+background_color+''';
-  border-right:'''+str(min_side_space_for_post)+'''em solid '''+background_color+''';
+  margin-top:1.5em;
+  margin-right:'''+str(min_side_space_for_post)+'''em;
   background-color:white; }
 
 @media screen and (max-width: '''+str(min_space_for_full_post_width)+'''em) {
-  div.blog_stream    { margin-right:'''+str(max_side_space-min_side_space_for_post)+'''em; }
+  div.blog_stream    { margin-right:'''+str(max_side_space)+'''em; }
   div.blog_right_bar { margin-left:-'''+str(max_side_space-min_side_space_for_post)+'''em; }
 }
 @media screen and (max-width: '''+str(min_space_for_two_columns_and_all_margins)+'''em) {
-  div.blog_stream { border-left:0; width:'''+str(post_padded_min_width)+'''em; }
-  div.blog_right_bar { border-right:0; }
+  div.blog_stream { margin-left:0; width:'''+str(post_padded_min_width)+'''em; }
+  div.blog_right_bar { margin-right:0; }
 }
 @media screen and (max-width: '''+str(min_space_for_two_columns_and_all_but_left_margin)+'''em) {
   div.blog_stream_and_right_bar { float:none; }
-  div.blog_stream    { margin-right:0; }
+  div.blog_stream    { margin-right:'''+str(min_side_space_for_post)+'''em; }
   div.blog_right_bar { margin-left:0; }
 }
 @media screen and (max-width: '''+str(min_space_for_two_columns_and_all_but_side_margins)+'''em) {
-  div.blog_stream    { border-right:0; }
+  div.blog_stream    { margin-right:0; }
   div.blog_right_bar { float:right; }
 }
   
 div.blog_post {
-  border-top:'''+str(post_separation)+'''em solid '''+background_color+''';
+  margin-top:'''+str(post_separation)+'''em;
   padding:'''+str(text_padding_width)+'''em;
   background-color:white; }
 div.blog_end_links_2 {
   clear:both;
-  border-top:'''+str(post_separation/4)+'''em solid '''+background_color+''';
-  border-bottom:'''+str(post_separation+min_side_space_for_post)+'''em solid '''+background_color+''';
+  margin-top:'''+str(post_separation/4)+'''em;
+  margin-bottom:'''+str(post_separation+min_side_space_for_post)+'''em;
   text-align:center; }
 a.blog_page_nav_link {
   display: inline-block;
