983b0378852ab778a31c8e94e9e435c9f74e37ae
Eli Dupree <vcs@elidupree.com>
2013-06-20 23:48:43 -0400

    created way to use rel="tag" for tag links

--- tags.py
+++ tags.py
@@ -12,6 +12,6 @@ def tag_url(tag_string):
   validate(tag_string)
   return "/blog/tags/"+tag_string
 
-def tag_link(tag_string):
+def tag_link(tag_string, use_rel):
   validate(tag_string)
-  return '<a href="'+tag_url(tag_string)+'">'+tag_string+'</a>'
+  return '<a '+('rel="tag" ' if use_rel else '')+'href="'+tag_url(tag_string)+'">'+tag_string+'</a>'
