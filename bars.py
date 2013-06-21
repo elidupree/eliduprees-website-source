214a4ef580ab9f97fc36450b379e8157b3815bde
Eli Dupree <vcs@elidupree.com>
2013-06-20 22:05:43 -0400

    Moved bottom bar into bars.py because it's connected to the frame

--- bars.py
+++ bars.py
@@ -1,7 +1,8 @@
 
 import css
 import top_bar
-import bottom_bar
+
+bottom_bar_height = 1.5
 
 css.insert('''
 div.bars_outer_box {
@@ -9,9 +10,28 @@ div.bars_outer_box {
   position: relative;
 }
 div.bars_inner_box {
-  padding-bottom: '''+str(bottom_bar.bar_height)+'''
+  padding-bottom: '''+str(bottom_bar_height)+'''
 }
+
+div.bottom_bar {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0.25em 0;
+  height: '''+str(bottom_bar_height - 0.5)+'''em;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  background-color: black;
+  background-image: url("/top-bar-background.png");
+  background-size: 100% 100%; }
+
+address.eli_email {
+  display: inline;
+  font-style: normal; }
 ''')
 
+def bottom_bar():
+  return '<footer><div class="bottom_bar">Please share this! | <address class="eli_email">Contact: <a href="ma&#73;ltO&#58;web&#64;el&#105;dupree&#00046;&#99;om">web&#64;el&#105;dupree&#00046;&#99;om</a></address> | <a href="/policies">Policies</a> | <a href="https://github.com/elidupree/eliduprees-website-source">Website source (code CC-0, content CC-BY-SA)</a></div></footer>'
+
 def bars_wrap(category, html):
-  return '<div class="bars_outer_box">'+top_bar.top_bar(category)+'<div class="bars_inner_box">'+html+'</div>'+bottom_bar.bottom_bar()+'</div>'
+  return '<div class="bars_outer_box">'+top_bar.top_bar(category)+'<div class="bars_inner_box">'+html+'</div>'+bottom_bar()+'</div>'
