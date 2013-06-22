fec700259a84bd7fc82208bf1497091fadfac301
Eli Dupree <vcs@elidupree.com>
2013-06-22 18:56:36 -0400

    Tried some more stuff with prototyping the VC page

--- voldemorts_children.py
+++ voldemorts_children.py
@@ -7,15 +7,35 @@ import html_pages
 
 
 css.insert('''
-div.vc {
+div.vc_comic_and_nav {
   width: 750px;
   margin: 2em auto; }
-img.vc {
+div.vc_comic {
+  display: inline-block;
   width: 750px; }
-''')
+img.vc_comic {
+  width: 750px; }
+div.vc_nav {
+  display: inline-block;
+  color: #ffc800;
+  width: 375px;
+  text-align: center;
+  font-size: 300%; }
 
+@media screen and (min-width: 1150px) {
+  div.vc_comic_and_nav {
+    width: 1150px; }
+  div.vc_nav {
+    width: 200px; }
+  div.vc_nav.top.next {
+    display: none; }
+  div.vc_nav.bottom.prev {
+    display: none; }
+}
+''')
+print("make the in-comic next page link work")
 def vc_page_html(page):
-  return '<main><div class="vc"><img class="vc" src="http://deqyc5bzdh53a.cloudfront.net/VC_1.png" /></div></main>'
+  return '<main><div class="vc_comic_and_nav"><div class="vc_nav top prev">Previous</div><div class="vc_nav top next">Next</div><div class="vc_comic"><a href="next comic"><img class="vc_comic" src="http://deqyc5bzdh53a.cloudfront.net/VC_1.png" /></a></div><div class="vc_nav bottom prev">Previous</div><div class="vc_nav bottom next">Next</div></div></main>'
 
 vc_pages = [
   2
