bc8d77f4e6a46a7db2a0c7d6676aaab52d9d8eae
Eli Dupree <vcs@elidupree.com>
2013-06-22 20:40:45 -0400

    continued prototyping vc page

--- voldemorts_children.py
+++ voldemorts_children.py
@@ -5,37 +5,66 @@ import bars
 import html_pages
 
 
+vc_content_margin = "4em";
 
 css.insert('''
 div.vc_comic_and_nav {
   width: 750px;
   margin: 2em auto; }
+div.vc_comic_and_transcript {
+  margin: 5em 0; }
 div.vc_comic {
   display: inline-block;
   width: 750px; }
 img.vc_comic {
   width: 750px; }
-div.vc_nav {
+div.vc_transcript_outer {
+  width: 750px; }
+  
+div.vc_nav_bar {
+  width: 750px; }
+div.vc_nav_button {
   display: inline-block;
   color: #ffc800;
-  width: 375px;
+  /*background-color: #412f16;*/
+  border-radius: 16px;
+  width: 250px;
   text-align: center;
-  font-size: 300%; }
-
-@media screen and (min-width: 1150px) {
+  font-size: 300%;
+  font-weight: bold; }
+div.vc_nav_button a:link{ color:#7e7e40 }
+div.vc_nav_button a:visited{ color:#40557f }
+div.vc_nav_button.prev {
+  margin-left: 75px;
+  margin-right: 50px; }
+div.vc_nav_button.next {
+  margin-left: 50px;
+  margin-right: 75px; }
+div.vc_transcript_inner {
+  padding: 0.5em;
+  color:white; }
+  
+@media screen and (min-width: 1090px) {
   div.vc_comic_and_nav {
-    width: 1150px; }
-  div.vc_nav {
-    width: 200px; }
-  div.vc_nav.top.next {
-    display: none; }
-  div.vc_nav.bottom.prev {
-    display: none; }
+    width: 1090px; }
+  div.vc_transcript_outer {
+    display: inline-block;
+    width: 340px;
+    vertical-align: top; }
+  .vc_transcript_hidden div.vc_comic_and_nav {
+    width: 920px;
+    padding-left: 170px; }
+  .vc_transcript_hidden div.vc_transcript_outer {
+    width: 170px; }
 }
 ''')
+
+def vc_navbar():
+  return '<div class="vc_nav_bar"><div class="vc_nav_button prev"><a rel="prev" href="prev page">Previous</a></div><div class="vc_nav_button next"><a rel="next" href="next page">Next</a></div></div>'
+
 print("make the in-comic next page link work")
 def vc_page_html(page):
-  return '<main><div class="vc_comic_and_nav"><div class="vc_nav top prev">Previous</div><div class="vc_nav top next">Next</div><div class="vc_comic"><a href="next comic"><img class="vc_comic" src="http://deqyc5bzdh53a.cloudfront.net/VC_1.png" /></a></div><div class="vc_nav bottom prev">Previous</div><div class="vc_nav bottom next">Next</div></div></main>'
+  return '<main><div class="vc_comic_and_nav">'+vc_navbar()+'<div class="vc_comic_and_transcript"><div class="vc_comic"><a href="next comic"><img class="vc_comic" src="http://deqyc5bzdh53a.cloudfront.net/VC_1.png" /></a></div><div class="vc_transcript_outer"><div class="vc_transcript_inner">Transcript: <a href="javascript">(show)</a><br/><br/>Transcript transcript transcript dolor sit amet, consectetur adipistranscript <br/><br/>Transcript transcript transcript dolor sit amet, consectetur adipistranscript <br/><br/>Transcript transcript transcript dolor sit amet, consectetur adipistranscript <br/><br/>Transcript transcript transcript dolor sit amet, consectetur adipistranscript <br/><br/></div></div></div>'+vc_navbar()+'</div></main>'
 
 vc_pages = [
   2
