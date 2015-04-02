72373b1b9ada23a67c566ad00097540cf84d33a1
Eli Dupree <vcs@elidupree.com>
2015-04-02 17:43:52 -0400

    Used a different background for text posts.

--- blog.py
+++ blog.py
@@ -411,7 +411,7 @@ def index_entry_html(post_dict):
 def make_blog_page_body(main_contents, sidebar_contents):
   return '''
 <body><a class="skip" href="#content">Skip to content</a><a class="skip" href="#blog-sidebar">Skip to blog sidebar</a>
-  <div><img role="presentation" alt="" class="background" src="/media/blog-background.jpg" /></div>
+  <div><img role="presentation" alt="" class="background" src="/media/colorful-background.jpg" /></div>
   '''+bars.bars_wrap({"blog":True}, '''
     <main>
       <div id="content" class="blog_page">
