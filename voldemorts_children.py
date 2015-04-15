6cf0acc02b9f2204494154c13b9cdcbcdd940ceb
Eli Dupree <vcs@elidupree.com>
2015-04-15 14:11:35 -0400

    raised the contrast of some colors used in the VC transcript

--- voldemorts_children.py
+++ voldemorts_children.py
@@ -27,6 +27,7 @@ body.voldemorts_children.content_notices_disabled div.comic_nav_button.content_n
 body.voldemorts_children div.comic_transcript_inner {
   /*border: 1px dashed white;*/
   color: #fff1c0; }
+body.voldemorts_children div.comic_transcript_inner q { border: none; color: #fff1c0; }
 body.voldemorts_children .post_content_section {
   background-color: #cccccc }
 body.voldemorts_children .comment_body {
@@ -41,6 +42,7 @@ body.voldemorts_children .blog_post_metadata {
 print ("TODO: find a better way to make modified versions of CSS rules")
 
 
+print ("TODO: is there a standard way of making colors 'high-contrast enough'?")
 
 dialogue_50pct_grey = '#8c8c8c'
 dialogue_name_replace = {
@@ -53,7 +55,7 @@ dialogue_name_replace = {
 }
 css.insert('''
 body.voldemorts_children div.comic_transcript_inner .dialogue { font-weight: bold; }
-body.voldemorts_children div.comic_transcript_inner .TITLE { color: #804c00; /*#412f16;*/ }
+body.voldemorts_children div.comic_transcript_inner .TITLE { color: #c07200; /*#412f16;*/ }
 body.voldemorts_children div.comic_transcript_inner .TONKS { color: #bf98af; /*#7f6574;*/ }
 body.voldemorts_children div.comic_transcript_inner .GRANGER { color: #8080ff; /*#6060c0;*/ }
 body.voldemorts_children div.comic_transcript_inner .HARRY { color: #ff0000; }
@@ -61,20 +63,20 @@ body.voldemorts_children div.comic_transcript_inner .WIRELESS { color: #737373; 
 body.voldemorts_children div.comic_transcript_inner .VOLDEMORT { color: #80ff80; }
 body.voldemorts_children div.comic_transcript_inner .HARRYMORT { color: #ba823f; }
 body.voldemorts_children div.comic_transcript_inner .LESTRANGE { color: #c8ff00; }
-body.voldemorts_children div.comic_transcript_inner .SNAPE { color: #809e80; }
-body.voldemorts_children div.comic_transcript_inner .DUMBLEDORE { color: #8000c0; }
-body.voldemorts_children div.comic_transcript_inner .deep_purple { color: #8000c0; }
+body.voldemorts_children div.comic_transcript_inner .SNAPE { color: #9bc09b; /*#809e80;*/ }
+body.voldemorts_children div.comic_transcript_inner .DUMBLEDORE { color: #aa00ff; /*#8000c0;*/ }
+body.voldemorts_children div.comic_transcript_inner .deep_purple { color: #aa00ff; /*#8000c0;*/ }
 body.voldemorts_children div.comic_transcript_inner .ZABINI { color: #eec832; }
 body.voldemorts_children div.comic_transcript_inner .LUNA { color: #00ff00; }
 body.voldemorts_children div.comic_transcript_inner .grey { color: '''+dialogue_50pct_grey+'''; }
-body.voldemorts_children div.comic_transcript_inner .pure_blue { color: #0000ff; }
+body.voldemorts_children div.comic_transcript_inner .pure_blue { color: #3030ff; /*#0000ff;*/ }
 body.voldemorts_children div.comic_transcript_inner .bright_orange { color: #ffc800; }
 body.voldemorts_children div.comic_transcript_inner .bright_green { color: #00ff00; }
 body.voldemorts_children div.comic_transcript_inner .bright_red { color: #ff0000; }
 body.voldemorts_children div.comic_transcript_inner .cyan { color: #00ffff; }
 body.voldemorts_children div.comic_transcript_inner .pale_blue { color: #6483c4; }
 body.voldemorts_children div.comic_transcript_inner .light_pink { color: #ff80c0; }
-body.voldemorts_children div.comic_transcript_inner .dark_green { color: #326632; }
+body.voldemorts_children div.comic_transcript_inner .dark_green { color: #4b984b; /*#326632*/; }
 ''')
 
 
