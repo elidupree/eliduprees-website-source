9e5affae56b75cb8659f14d0f82517f4ee2396f8
Eli Dupree <vcs@elidupree.com>
2016-06-21 13:04:04 -0400

    Preliminary insertion of MailChimp sign-up form

--- blog.py
+++ blog.py
@@ -812,6 +812,28 @@ def sidebar_with_entries (index_entries, header, random_itemname):
       +'</div>')
   return '''
   <a class="random_post sidebar_standalone_link" id="random_post" data-itemname="''' + random_itemname +'''"></a>
+  
+  <!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="//elidupree.us13.list-manage.com/subscribe/post?u=4d65283e4a6612f93da4514e2&amp;id=122f9e2af8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4d65283e4a6612f93da4514e2_122f9e2af8" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
   <a class="sidebar_standalone_link Patreon_link" href="https://www.patreon.com/EliDupree"><img src="/media/patreon-logo.png?rr" alt="" /><span>$$$ Encourage me to make more cool stuff</span></a>
   '''+index
 
