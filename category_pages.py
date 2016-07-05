50202ca3426f2848eaa33f882a088b790b15d979
Eli Dupree <vcs@elidupree.com>
2016-07-05 14:31:39 -0400

    Improved content warnings appearance

--- category_pages.py
+++ category_pages.py
@@ -59,6 +59,7 @@ a.exhibit.fadeout>div.exhibit_start_reading {
 img.exhibit {
   display: block;
   position: relative;
+  z-index: 1;
   padding: 1em;
   background-color: rgba(255,255,255,0.3);
   margin-top: -6em;
@@ -127,8 +128,9 @@ span.title { font-style: italic; }
 
 .orphaned_page_link {display: block}
 
-.fake_warning_link {font-size: 71%; margin:0.4em 0; }
+.fake_warning_link {font-size: 71%; margin:0.4em 0; position: relative; z-index: 0;}
 .fake_reveal_warning_button {color: blue; text-decoration: underline;}
+html.javascript_enabled .fake_warning_link {display: inline-block;}
 html.javascript_enabled .fake_reveal_warning_button {display: block;}
 html.javascript_enabled .comic_warnings_list {display: none;}
 
@@ -139,6 +141,7 @@ html.debug_mode .not_debug {display: none;}
 
 javascript.do_after_body (r'''
 $(".fake_warning_link").click (function () {
+  $(this).css ("display", "block");
   $(this).children (".fake_reveal_warning_button").hide ();
   $(this).children (".comic_warnings_list").show ();
   return false;
@@ -177,9 +180,9 @@ def add_category_pages(page_dict):
   <div id="content">
     '''+ exhibit ("/voldemorts-children", "vc left", "/media/VC_0.png?rr",'''
         <p>What if Dumbledore's idea of placing Harry Potter with an abusive family didn't turn out so well?</p>
-        <p><span class="title">Voldemort's Children</span>, my ongoing Harry Potter fanfic graphic novel, explores possible answers. It's about 80% complete, but is on hiatus while my hands recover from an injury.</p>'''+ content_warning_summary ("<p>This comic depicts verbal and physical abuse; physical violence, with occasional cartoon blood and gore; ableist language; negative self-talk; some discussion of suicide.</p><p>As you read the comic, each page will be marked with the warnings that apply to that page.</p>"), "Start reading") +
+        <p><span class="title">Voldemort's Children</span>, my ongoing Harry Potter fanfic graphic novel, explores possible answers. It's about 80% complete, but is on hiatus while my hands recover from an injury.</p>'''+ content_warning_summary ("<p>Content warnings: This comic depicts verbal and physical abuse; physical violence, with occasional cartoon blood and gore; ableist language; negative self-talk; some discussion of suicide.</p><p>As you read the comic, each page will be marked with the warnings that apply to that page.</p>"), "Start reading") +
         exhibit ("/people-are-wrong-sometimes", "paws right", "/media/PAWS_thumbnail.png?rr",'''
-        <p>In <span class="title">People Are Wrong Sometimes</span>, two friends are about to leave high school and part ways. But do they really know each other? (10 pages)</p>'''+ content_warning_summary ("This comic depicts neurelitist statements; brief descriptions of bullying and violence; one instance of cartoon blood."), "Start reading") +
+        <p>In <span class="title">People Are Wrong Sometimes</span>, two friends are about to leave high school and part ways. But do they really know each other? (10 pages)</p>'''+ content_warning_summary ("<p>Content warnings: This comic depicts neurelitist statements; brief descriptions of bullying and violence; one instance of cartoon blood.</p>"), "Start reading") +
         exhibit ("/a-couple-of-badass-superheroes" , "acobs left", "/media/ACOBS_thumbnail.png?rr",'''
         <p><span class="title">A Couple of Badass Superheroes</span> go on a silly adventure. I wrote the first part September-December 2011 to get used to drawing using my tablet. (14 pages)</p>''', "Start reading") +'''
     <div class="category_page_bottom"></div>
