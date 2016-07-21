9bf43c1bf51a7a01b7659840dbc6694a94423297
Eli Dupree <vcs@elidupree.com>
2016-07-21 17:42:09 -0400

    More work on voice practice tool

--- voice_practice_tool.py
+++ voice_practice_tool.py
@@ -23,20 +23,72 @@ The histogram should appear here, but it hasn't. Maybe you don't have JavaScript
 $(function(){
   var audio = new (window.AudioContext || window.webkitAudioContext)();
   var canvas = document.getElementById("histogram_canvas").getContext("2d");
+  var current_player;
   
 var source;
   var analyzer = audio.createAnalyser ();
-  var recorder = audio.createScriptProcessor (4096, 1, 1);
-  var current_recording = {buffer: audio.createBuffer (1, audio.sampleRate, audio.sampleRate)}
+var recorder_buffer_length = 4096;
+  var rate = audio.sampleRate
+  var recorder = audio.createScriptProcessor (recorder_buffer_length, 1, 1);
+  var recording_1_second_width = 10;
+  var recording_height = 100;
+  function create_recording () {
+    output = {buffer: audio.createBuffer (1, audio.sampleRate, audio.sampleRate), next_sample: 0, lines: []};
+    output.canvas = $("<canvas/>").attr("width", recording_1_second_width).attr("height", recording_height).click (function () {
+      var player = audio.createBufferSource ();
+      player.buffer = output.buffer;
+      player.connect (audio.destination);
+      if (current_player) {current_player.stop ();}
+      player.start ();
+      current_player = player;
+    });
+    $("main").append (output.canvas);
+    output.canvas_context = output.canvas [0].getContext("2d");
+    return output;
+  }
+  function draw_recording (recording) {
+    var context = recording.canvas_context;
+    if(recording=== current_recording) {
+      context.fillStyle = "rgb(0, 0, 0)"
+      } else {
+      context.fillStyle = "rgb(50, 50, 50)"
+            }
+context.strokeStyle = "rgb(255, 0, 0)"
+    recording.canvas_context.fillRect (0, 0, recording.canvas.width (), recording.canvas.height ());
+context.beginPath ();
+    for (var I = 0; I <recording.lines.length;++I) {
+context.moveTo (I, recording_height/2 + recording.lines [I]*recording_height/512);
+context.lineTo (I, recording_height/2 - recording.lines [I]*recording_height/512);
+    }
+context.stroke ();
+    
+  }
+  var current_recording = create_recording ();
   analyzer.connect (recorder);
   
+  analyzer.maxDecibels = 0;
   analyzer.fftSize = 2048;
   var buffer_length = analyzer.frequencyBinCount; 
   var frequency_data = new Uint8Array(buffer_length);
   
   recorder.onaudioprocess = function (event) {
-    var input = events.inputBuffer.getChannelData (0);
-    
+    var input = event.inputBuffer.getChannelData (0);
+    var output = current_recording.buffer.getChannelData (0);
+    for (var sample = 0; sample <recorder_buffer_length;++sample) {
+      if (current_recording.next_sample >= current_recording.buffer.length) {
+        var old_buffer = current_recording.buffer;
+        var old_output = output;
+        current_recording.buffer = audio.createBuffer (1, current_recording.buffer.length*2, rate);
+        current_recording.canvas.attr("width", current_recording.canvas.width ()*2);
+        output = current_recording.buffer.getChannelData (0);
+        for (var I = 0; I <old_buffer.length;++I) {
+          output [I] = old_output [I];
+        }
+      }
+      output [current_recording.next_sample] = input [sample];
+      ++current_recording.next_sample;
+    }
+    draw_recording (current_recording);
   }
   
   navigator.getUserMedia = (navigator.getUserMedia ||
@@ -57,18 +109,27 @@ navigator.msGetUserMedia);
   }
 );
   
+  var last_line_added = audio.currentTime;
+  var line_adding_increment = 1/recording_1_second_width;
   function draw () {
     requestAnimationFrame (draw);
     analyzer.getByteFrequencyData (frequency_data);
+    var total = 0;
     canvas.fillStyle = "rgb(0, 0, 0)"
     canvas.strokeStyle = "rgb(255, 0, 0)"
     canvas.fillRect (0, 0, 1024, 256);
     canvas.beginPath ();
     for (var I = 0; I <1024;++I) {
+      total = total + frequency_data [I];
       canvas.moveTo (I, 256);
       canvas.lineTo (I, 256 - frequency_data [I]);
     }
     canvas.stroke ();
+    var average = total/1024;
+    if (audio.currentTime >= last_line_added + line_adding_increment) {
+      current_recording.lines.push (average);
+      last_line_added = last_line_added + line_adding_increment
+    }
   }
   draw ();
 });
