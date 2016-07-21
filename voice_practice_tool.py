e8c6de56b40c0bd6dbd9b6f83e50961545bcc6fe
Eli Dupree <vcs@elidupree.com>
2016-07-21 19:36:41 -0400

    Allowed splitting the recording, fixed some playback issues

--- voice_practice_tool.py
+++ voice_practice_tool.py
@@ -11,6 +11,7 @@ def add_page(page_dict):
       "Voice practice tool ⊂ Eli Dupree's website",
       r'''<style type="text/css">
 html,body {background-color: white;}
+canvas {float: left; margin:2px;}
     </style> 
     ''',
       '''<a class="skip" href="#content">Skip to content</a>
@@ -23,7 +24,7 @@ The histogram should appear here, but it hasn't. Maybe you don't have JavaScript
 $(function(){
   var audio = new (window.AudioContext || window.webkitAudioContext)();
   var canvas = document.getElementById("histogram_canvas").getContext("2d");
-  var current_player;
+
   
 var source;
   var analyzer = audio.createAnalyser ();
@@ -32,15 +33,56 @@ var recorder_buffer_length = 4096;
   var recorder = audio.createScriptProcessor (recorder_buffer_length, 1, 1);
   var recording_1_second_width = 10;
   var recording_height = 100;
+  var current_playback;
+  var current_recording;
+function stop_playback () {
+    if (current_playback) {
+    var old_player = current_playback.player;
+    var redraw = current_playback.recording;
+    current_playback = undefined;
+    old_player.stop ();
+    draw_recording (redraw);
+    }
+  }
+  function set_current_recording (recording) {
+    var old_recording = current_recording;
+    current_recording = recording;
+    if (old_recording) {draw_recording (old_recording);}
+  }
   function create_recording () {
-    output = {buffer: audio.createBuffer (1, audio.sampleRate, audio.sampleRate), next_sample: 0, lines: []};
-    output.canvas = $("<canvas/>").attr("width", recording_1_second_width).attr("height", recording_height).click (function () {
-      var player = audio.createBufferSource ();
-      player.buffer = output.buffer;
-      player.connect (audio.destination);
-      if (current_player) {current_player.stop ();}
-      player.start ();
-      current_player = player;
+    var output = {buffer: audio.createBuffer (1, audio.sampleRate, audio.sampleRate), next_sample: 0, lines: []};
+    output.canvas = $("<canvas/>").attr("width", recording_1_second_width).attr("height", recording_height).click (function (event) {
+      var offset = output.canvas.offset ();
+      var X = event.pageX - offset.left;
+      stop_playback ();
+      var start_position =X/recording_1_second_width;
+      var start_function = function () {
+        var start_position =current_playback.start_position + (audio.currentTime - current_playback.start_time);
+        var current_end = output.next_sample/rate;
+        if (start_position >current_end - 0.05) {
+          stop_playback ();
+          return;
+        }
+
+        var player = audio.createBufferSource ();
+        player.buffer = output.buffer;
+        var finished_function= function () {
+          if (!(current_playback && current_playback.player=== player)) {return;}
+          if (player.buffer === output.buffer) {stop_playback ();}
+          else {start_function ();}
+        };
+        player.onended = finished_function;
+        player.connect (audio.destination);
+        current_playback.player = player;
+        setTimeout (function () {
+          if (!(current_playback && current_playback.player=== player)) {return;}
+          player.stop ();
+          start_function ();
+          }, (current_end - start_position)*500);
+        player.start (audio.currentTime, start_position); 
+      };
+      current_playback = {start_time: audio.currentTime, start_position: start_position, recording: output};
+      start_function ();
     });
     $("main").append (output.canvas);
     output.canvas_context = output.canvas [0].getContext("2d");
@@ -61,9 +103,13 @@ context.moveTo (I, recording_height/2 + recording.lines [I]*recording_height/512
 context.lineTo (I, recording_height/2 - recording.lines [I]*recording_height/512);
     }
 context.stroke ();
-    
+    if (current_playback && current_playback.recording=== recording) {
+    context.strokeStyle = "rgb(255, 255, 0)"
+    context.beginPath ();
+      var X = (current_playback.start_position + (audio.currentTime - current_playback.start_time))*recording_1_second_width; context.moveTo (X, 0); context.lineTo (X, recording_height); context.stroke ();
+    }
   }
-  var current_recording = create_recording ();
+  current_recording = create_recording ();
   analyzer.connect (recorder);
   
   analyzer.maxDecibels = 0;
@@ -130,8 +176,11 @@ navigator.msGetUserMedia);
       current_recording.lines.push (average);
       last_line_added = last_line_added + line_adding_increment
     }
+    if (current_playback) {draw_recording (current_playback.recording);}
   }
   draw ();
+  
+  $("#histogram_canvas").click (function () {set_current_recording (create_recording ());});
 });
     </script>'''}
   )
