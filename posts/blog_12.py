7c243754ee66ea4f544e6014fc3505b8689c9576
Eli Dupree <vcs@elidupree.com>
2016-08-10 12:41:58 -0400

    finish+deploy post

--- posts/blog_12.py
+++ posts/blog_12.py
@@ -14,13 +14,13 @@ Here's a story you've probably heard before:
 
 The heroes stumble upon someone who has been magically imprisoned. Maybe it's a thousand-year-old spirit, maybe it's a regular person. Maybe it begs them to free it. Maybe they free it by accident. Maybe this is a game where you can choose whether to free it. Either way, freeing it is <strong>bad</strong>. If it eventually gets free, then it turns out to be evil. The heroes may have to spend the rest of the story fighting it.
 
-I've seen a lot of different stories that follow this pattern. On the other hand, I can hardly think of any stories with an imprisoned <em>good</em> being. The main exception is when there's a good counterpart to an imprisoned evil being in the same story.
+I've seen a lot of different stories that follow this pattern. On the other hand, I can hardly think of any stories with an imprisoned <em>good</em> being. The main exception is when there's a good counterpart to an imprisoned evil being in the same story. These stories seem to encourage the belief that "if someone is being punished, they must have done something to deserve it", which is generally a bad thing to believe.
 
 I'd like to change this pattern. I live in the United States, which injustly imprisons huge numbers of people. Because imprisoning people is generally bad in real life, I would like to make it look bad in my stories as well.
 
 <h2>Story idea #1</h2>
 
-Legend says that in ancient times, there was a battle between a great good spirit and a great evil spirit. In the end, the good spirit overcame the evil spirit and imprisoned it for one thousand years.
+Legend says that in ancient times, there was a battle between a great good spirit and a great <cut>evil spirit. In the end, the good spirit overcame the evil spirit and imprisoned it for one thousand years.
 
 Now, the good spirit still rules over the world, but the thousand years are almost over. The good spirit has sent a group of human heroes to perform a magical ritual around the prison, which will keep the evil spirit imprisoned for another thousand years. However, one of these heroes &ndash; the main character &ndash; starts to have doubts. Ze studies in ancient libraries, trying to find out the true history of the two spirits. The more ze reads, the less sure ze is that the imprisoned spirit is really evil. So when the time comes to perform the ritual, ze does it wrong on purpose. The spirit breaks free.
 
@@ -40,7 +40,13 @@ The heroes aren't sure what to do. On the one hand, they empathize with the spir
 
 So the heroes decide to show the spirit that humans are <em>not</em> always greedy and heartless. They travel to all the nearby towns. They convince the people to pay reparations to the spirit, and help it build a new home for itself. When the heroes return to the spirit, offering to free it AND pay reparations, it relents. It realizes that humans can be good, and gives up its revenge.
 
-I like this story idea because it respects the spirit's rights even though the spirit is violently angry. In real life, some people try to justify ignoring other people's rights just by pointing out that they protested in a violent way. Violent protest is not necessarily <em>good</em>, but it doesn't change the fact that they deserve their rights. If you don't like it when people protest violently, it is better to treat them well so that they don't have to protest at all, rather than to blame them for how they respond to their treatment.
+<h2>Discussion</h2>
+
+I like story idea #2 because it respects the spirit's rights even though the spirit is violently angry. In real life, some people try to justify ignoring other people's rights just by pointing out that they protested in a violent way. Violent protest is not necessarily <em>good</em>, but it doesn't change the fact that they deserve their rights. If you don't like it when people protest violently, it is better to treat them well so that they don't have to protest at all, rather than to blame them for how they respond to their treatment.
+
+That said, I'm not really inspired to write either story. Both the story ideas start with the <em>illusion</em> of the original "evil spirit" story. That could be good, because it tells the story of someone who has bad assumptions, but then learns to get past them. But it could also be bad, because it kind of repeats the original bad story for a while, even though it has a different ending.
+
+If I include this concept in my actual writing, I think I'd rather just do it in a straightforward way. That is, there would be an injustly imprisoned non-evil being, and the heroes would release it. It wouldn't look evil first. Of course, that idea is so straightforward that it doesn't form a complete story. It would just be an element in a larger story. Maybe this element will find its way into one of my big future story plans...
 ''', 
 },  
 
