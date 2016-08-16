4a1200556eb5e5e795f73986d0aa36a3c9ad6faf
Eli Dupree <vcs@elidupree.com>
2016-08-16 18:21:12 -0400

    Wrote a blog post

--- posts/blog_12.py
+++ posts/blog_12.py
@@ -50,6 +50,78 @@ If I include this concept in my actual writing, I think I'd rather just do it in
 ''', 
 },  
 
+{
+"title": "A Critique and Defense of Equality",
+"tags": ["philosophical"],
+"auto_paragraphs": True,
+"don't deploy": True,
+"contents":'''
+Suppose a genie appeared and offered to save the lives of 100 white people, and also save the lives of 5 black people. Should you accept? Of course you should.
+
+That's a critique I've heard a few times. It says: "Equality has no inherent value. What really matters is doing as much good as you can, for as many people as you can." I can't <em>completely</em> disagree with that &ndash; it was even one of the assumptions in my <a href="/blog/game-theory-contracts-altruism">mathematical model</a>. But it's much more complicated than that.
+
+At worst, the genie example proves that equality isn't in <strong>end in itself</strong>. Increasing equality might still <strong>cause</strong> good things to happen. In philosophy terms, it doesn't have <a href="https://en.wikipedia.org/wiki/Intrinsic_value_(ethics)">intrinsic value</a>, it only has <a href="https://en.wikipedia.org/wiki/Instrumental_value">instrumental value</a>.
+
+Usually, when I've seen other people use this argument, they've tried to twist it farther than that. They've implied: "Because equality doesn't have intrinsic value, we don't need to care about it at all." But there are many reasons to care about it. That's why I'm writing this post. In case you run into this argument somewhere yourself, let me prepare you with a defense:
+
+<h2>The Defense</h2>
+
+<h3>1. Equality as a rational incentive</h3>
+
+Suppose my neighbor and I are gathering firewood. I want to get as much firewood as <cut>possible. I could go cut some myself, which is a lot of work. Or I could steal some from my neighbor, which is much easier.
+
+Now, suppose that every evening, the village elder forces us to have equal amounts of firewood. If either of us has more firewood, the village elder <em>destroys</em> the extra wood. Are you mad at the elder? Is ze doing evil by destroying intrinsic value?
+
+And yet, now I'm not going to steal anymore. If I steal, I'll just lose more than I gain. If I want to get more firewood, my logical action is to cut it myself and share it with my neighbor, too. And I no longer have to worry about my neighbor stealing from me, either. The fact that we can trust each other more easily has significant value.
+
+This isn't perfect, of course. It only works on a small scale – if the group is too large, then you hardly get any reward from your individual labor. And it can often make the labor be split unequally. If we don't make some sort of agreement, whoever wants to maintain a 10% taller stack of firewood will end up doing 100% of the work. (If you've ever lived with someone who can tolerate more dirty dishes then you can, you probably have personal experience with this!)
+
+However, that doesn't mean it's useless. Enforcing equality is a blunt instrument, but the good often outweighs the bad.
+
+<h3>2. Equality as a proof-of-concept</h3>
+
+Is it possible for a person to walk along the street without ever being sexually harassed? Is it possible to walk into shops without being suspected as a thief? Is it possible ask your doctor about a medical problem and get advice that relates that actual problem?
+
+Of course it is. We know this because male people, white people, and thin cis people do these things all the time. If <em>everyone</em> got harassed, we wouldn't know that we could do anything about it. We might think it was an inevitable fact of life. But because it <em>doesn't</em> happen to some people, that proves that it's possible to prevent this evil.
+
+When people say "I want to be treated equally", they hardly ever mean "I want you to make that other person's life just the terrible as mine". They mean, "I want the same good treatment that I <em>know</em> you give to other people".
+
+Obviously, this doesn't apply to everything. If one person has 100 servants, that doesn't prove that everyone can have 100 servants.<footnote((Human servants, anyway. In a future robot utopia, many more things are possible.))> If there are limited resources, you can't give one person more without giving another person less. But there are many things, like harassment and abuse, which are probably <em>not</em> a matter of limited resources. We don't have to transfer them to someone else &ndash; we can just make them not happen at all.
+
+<h3>3. Equality as efficient resource distribution</h3>
+
+Which is more useful: To give three houses to someone who already has three houses, or to give one house to someone who has zero houses?
+
+Most resources have <strong>diminishing returns</strong>. The more you have, the less useful it is to get one more. A person with $10 would gain a lot from finding $100, but a person with $1 billion would hardly be any better off if they got another million &ndash; they're rich either way. 
+
+Therefore, if you're trying to hand out resources in the most efficient way, it's usually best to do it (approximately) equally. The more equality you use, the more value you produce. Equality still isn't technically an "intrinsic value", but it's hard to have a more direct relationship than that.
+
+On the same subject, the genie from the example doesn't exist. There's never only one choice &ndash; there are always other options. If someone has the power to save 100 white people's lives, they could probably spend the same resources to save just as many black people's lives, if not more. Even if we don't say the genie's racial bias is inherently bad, we could still critique the genie for spending resources inefficiently.
+
+
+<h3>4. Equality as friendship repair</h3>
+
+Suppose your friend really really wanted a piano, but couldn't afford one. Now suppose that you could snap your fingers and make a piano appear out of thin air. Would you do it? Of course you would.
+
+Rich people can do this. Not literally, but almost literally. Of course, if they do it too many times, they stop being rich. In order to stay rich, they need a justification for not doing this. That doesn't necessarily make them evil, but it does make it harder for them to be friends with poor people.
+
+But you can't <em>just</em> avoid being friends with poor people. In many places, rich and poor live <em>physically</em> close to each other. They walk past each other every day, but if they really get to know each other, the rich people will have to face the question of why they hold onto their wealth when other people need it much more than they do. Not everyone responds to this in the same way, but few of the responses are good. Some people decide that the poor <em>deserve</em> their conditions. Some people refuse to <em>believe</em> that those conditions are as bad as they actually are. Some people build walls and fences so they don't have to look at the issue. One way or another, they have to distance themselves and suppress their empathy.<footnote((A side note about empathy: as I've mentioned before, empathy isn't inherently good. I myself don't usually feel empathy for other people, but that doesn't stop me from working for other people's good. But in my case, not feeling empathy is my <em>natural</em> state. If someone would naturally feel empathy for others, but suppresses it for a specific group of people, they probably won't learn how to treat the group well.))>
+
+Similar things happen with other groups. White people have less empathy for black people. Male people find excuses to ignore female people's problems. On the other hand, in a world where everyone has equal power and resources (or at least much closer to equal), these things wouldn't have to happen. Because everyone would be in a more similar situation, they would have an easier time empathizing with each other.
+
+Like everything else, this wouldn't be perfect. Even with equal <em>physical</em> resources, <a href="/blog/neurodiversity">people will still experience the world in very different ways</a>, so they won't always understand each other's situations. And there will always be reasons for people to mistreat each other, even if everyone has equal power.
+
+<h2>Final thoughts</h2>
+
+Despite this defense, I still have some hesitation about the idea of equality. It's not a central part of my moral system. My reasons can be summed up in one sentence: Equality is not <strong>sufficient</strong> for justice. To reach a good world, we will have to fix the evils caused by inequality, but we will also have to do much more than that.
+
+Like a lot of people, I have an instinct to think "that's not fair" when people are treated unequally. "That's not fair" <em>can</em> be a useful way to think, as I argued above. However, it isn't always right. Many people think it's not "fair" to give extra accommodations for people's disabilities. And on the other hand, it <em>would</em> seem fair to apply an unjust law equally to everyone. My <em>own</em> sense of "fair" doesn't always agree with my sense of justice. Part of why I wrote this post was to try to reconcile those two things.
+
+Anyway, I hope this post helps people think about things. I'm not trying to argue that "equality is good" or "equality is bad" &ndash; it's much nicer to understand what <em>aspects</em> are good and bad <em>about</em> it.
+''',
+},
+
+
 ]
 
 """
