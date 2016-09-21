90be20114238f512a16328d808c8096f1f27c4b7
Eli Dupree <vcs@elidupree.com>
2016-09-21 13:31:44 -0400

    Wrote new post

--- posts/blog_13.py
+++ posts/blog_13.py
@@ -34,6 +34,55 @@ If you're talking to someone, and they call this kind of thing "love", don't say
 ''',
 },
 
+{
+"title": "Gods and atheism",
+"tags": ["philosophical"],
+"auto_paragraphs": True,
+"blurb": "I'm not writing this post to convince anybody, but many people have found my beliefs about gods interesting.",
+"contents":'''
+I'm not writing this post to convince anybody, but many people have found my beliefs about gods <em>interesting</em>, so I figured I'd describe them here.
+
+If someone asks, "are you an atheist?", I can comfortably say "yes", because I don't specifically believe in any gods. But if they ask "are you an atheist or an agnostic?", it gets more complicated. That's because the idea of a "god" can actually mean several <em>different</em> ideas, and I have different beliefs about each of them.
+
+People often lump these ideas together, but they are quite distinct:
+
+<h2>The Big Guy</h2>
+
+This god goes around smiting people, performing miracles, making commandments, and so on. It's basically an immortal human with magic powers.
+
+Most old religious books describe gods like the Big Guy. However, most religious people I've met don't really believe in a Big Guy &ndash; they believe in a different version of God. For instance, modern Christianity isn't really <cut>based on the descriptions in the Old Testament. Honestly, it shows up more as a talking point in bad atheist arguments (<q>Look! The idea of the Big Guy is silly! Therefore, ALL religious beliefs are silly!</q>).
+
+I believe that <strong>we have sufficient evidence to conclude that no gods of this type exist</strong>. If they did exist, they would leave lots of evidence. Since we haven't seen that, we can conclude that they don't. This makes me a <a href="https://en.wikipedia.org/wiki/Strong_atheist">strong atheist</a> about this type of god. It's not <em>inherently</em> impossible for the Big Guy to exist, though. If somebody climbed out of the ground tomorrow and started striking things with lightning and turning shrubs into goats, I'd say, "yup, you could call that a god". I wouldn't necessarily <em>respect</em> it, though. Most of the gods from old religious books are tyrants and mass murderers.
+
+<h2>The Designer</h2>
+
+This god designed the world we know about. You could think of it as "the programmer of the cosmos". It designed the physics, then just left the world running for billions of years. Has it changed the world at all since then? Was it trying to create humans, or were they just an accident? Nobody knows.
+
+It's impossible to find evidence against this god. No matter what you observe, the god could have just designed the world so that you would observe that. I suppose that makes me an <a href="https://en.wikipedia.org/wiki/Agnostic">agnostic</a> about this type of god. But my real beliefs are more like "This could technically exist, but I know it doesn't." That's probably how most people feel about <a href="https://en.wikipedia.org/wiki/Russell's_teapot">Russell's teapot</a>.
+
+There <em>is</em> a difference between the teapot and the god. If the teapot exists, that doesn't explain anything. But if the Designer exists, that could explain the world we know about. But I don't want an explanation of <em>just</em> the world I know about. I'll eventually learn about something else (perhaps the Designer itself), and then I'll want an explanation of that too. Therefore, what I really want is an explanation of everything that exists. The-world-we-know-about can have a designer outside of the-world-we-know-about. But everything-that-exists <em>can't</em> have a designer outside of everything-that-exists, because if you're outside of everything-that-exists, that means you don't exist.
+
+<h2>The Transcendent</h2>
+
+This god is all around us. Do you feel it? It is love. It is the hum of the universe.
+
+Specifically, this god <em>isn't</em> just an individual, intelligent person with great powers, like the Big Guy and the Designer are. So what <em>is</em> it, and does it exist?
+
+To answer that, I'll ask a similar question: <strong>does the <em>sky</em> exist?</strong>
+
+When we look upwards, we can say "there's the sky". The birds are in the sky. The clouds are in the sky. So when we say "the sky", do we mean "the atmosphere"? No, because we also say "the sun is in the sky" and "the stars are in the sky". Some of them are light-years away. Some of them have already exploded. If some of them are behind a hill, they are not "in the sky", even if they would be "in the sky" if you moved over by a few meters or waited an hour. It's clear that when we say "the sky", we're not talking about a specific thing that exists.
+
+But <strong>here's the important part:</strong> if someone says, "the sky is beautiful this evening", not even the most picky atheist will say "you fool, don't you know the sky doesn't exist?". That's because the atheist understood what the person meant. Even though the person <em>said</em> "the sky", as if they were trying to refer to a thing that exists, what they <em>meant</em> was about their human experience of the sky, which <em>does</em> exist.
+
+The Transcendent is similar. People have a lot of feelings, and they want to express them. Sometimes, "God" or "Divine" is the best word the people find to express them. That's not wrong.
+
+Sometimes, when people talk to me about the Transcendent, they act as if <em>everyone</em> must have these feelings. This frustrates me, because I don't have those feelings myself. It's obvious to me that their beliefs are incorrect. But they don't necessarily have incorrect beliefs about <strong>gods</strong>. They might just have <a href="/blog/neurodiversity">incorrect beliefs about <strong>brains</strong></a>.
+
+I'm not sure there's a term like "atheist" or "agnostic" for my perspective here. It has some things in common with <a href="https://en.wikipedia.org/wiki/Theological_noncognitivism">theological noncognitivism</a> &ndash; I think that descriptions of the Transcendent do not describe anything that <em>can</em> exist in a literal sense. But the difference is that my response is not, "ha ha, people who describe the Transcendent are thinking wrong!", but instead, "if they're not describing a literal existence, let me figure out what they <em>are</em> trying to express."
+''',
+},
+
+
 ]
 
 """
