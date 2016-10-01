1b4cbfdd40796a12ea587711ae0baaa577c26bf7
Eli Dupree <vcs@elidupree.com>
2016-10-01 14:21:55 -0400

    Began writing The Path

--- game_pages.py
+++ game_pages.py
@@ -8,6 +8,7 @@ import blog
 import pac_asteroids
 import hexy
 import green_caves
+import the_path
 
 
 import html_pages
@@ -295,6 +296,7 @@ def add_game_pages(page_dict):
   pac_asteroids.add_game (page_dict)
   hexy.add_game (page_dict)
   green_caves.add_game (page_dict)
+  the_path.add_game (page_dict)
   
   import voice_practice_tool
   voice_practice_tool.add_page (page_dict)
---
+++ the_path.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import utils
+import bars
+import blog
+	  
+def add_game(page_dict):
+  utils.make_page (page_dict,
+    '/games/the-path',
+      "The Path ⊂ Eli Dupree's website",
+      r'''<style type="text/css">
+html,body {background-color: black;}
+#game {position: relative;}
+.game_canvas {position: absolute; height: 100%; width: 100%;}
+.path_component {position: absolute; background-color: white; height: 1px; width: 10%;}
+    </style> 
+    <script type="text/javascript">
+$(function() {
+"use strict";
+
+var game_element = $("#game");
+game_element.height (600);
+var canvas_element = $("<canvas>").addClass ("game_canvas")
+game_element.append (canvas_element);
+var canvas_context = canvas_element[0].getContext ("2d");
+
+var paths = [{info: {max_speed: 1}, data: [{position: 0, speed: 0, element: $("<div/>") .addClass ("path_component")}]}];
+function tick() {
+  requestAnimationFrame (tick);
+  
+  paths.forEach (function(path) {
+    while (path.data.length <600) {
+      var previous = path.data [path.data.length - 1];
+      var current = {
+        position: previous.position + previous.speed,
+        speed: previous.speed + (Math.random()*2 - 1)/60/600
+      };
+      var max_speed =path.info.max_speed / 600;
+      if (current.speed >max_speed){current.speed = max_speed;}
+      if (current.speed <-max_speed){current.speed = -max_speed;}
+      //current.element = $("<div/>") .addClass ("path_component");
+      //game_element.append (current.element);
+      path.data.push (current);
+    }
+    //console.log (path.data [0]);
+    var deleted;
+    for (var i=0;i<10;++i){
+      deleted = path.data.shift();
+      //deleted.element.detach();
+    }
+    var width = game_element.width();
+    var height = game_element.height();
+    canvas_element.attr ("width", width).attr ("height", height);
+    canvas_context.fillStyle = "rgb(0,0,0)";
+    canvas_context.fillRect (0, 0, width, height);
+    canvas_context.fillStyle = "rgb(255, 255, 255)";
+    path.data.forEach (function(current, index) {
+      //current.element.css ("bottom", index).css ("left", game_element.width()*(current.position - deleted.position +0.45));
+      //console.log (current.position - deleted.position + 0.45);      console.log (width*(current.position - deleted.position + 0.45));
+      canvas_context.fillRect (width*(current.position - deleted.position + 0.45), height - index, width*0.10, 1);
+    });
+  });
+}
+tick();
+
+
+});
+    </script>''',
+      '''<a class="skip" href="#content">Skip to content</a>
+      '''+bars.bars_wrap({"games":True}, '''<main><div id="content">
+      
+      <div id="game"></div>
+
+    '''+ blog.comments_section ("the_path") +''' </div>
+  </div>
+</main>'''), {
+  "jQuery_before": True,
+  "blurb": "A unfinished online game.",
+  #"blurb_image": "/media/pac-asteroids-thumbnail.png?rr"
+}
+  )
