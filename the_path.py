dcdf658ede35ffd329ea996f203063194695b78f
Eli Dupree <vcs@elidupree.com>
2016-10-02 12:18:50 -0400

    Made both width and height conform to the window, parameterized some constants

--- the_path.py
+++ the_path.py
@@ -20,24 +20,34 @@ $(function() {
 "use strict";
 
 var game_element = $("#game");
-game_element.height (600);
+var top_bar = $(".top_bar");
+var bottom_bar = $(".bottom_bar");
+$(".bars_inner_box").css ("padding-bottom", 0);
+//var body = $("body");
+//game_element.height (600);
 var canvas_element = $("<canvas>").addClass ("game_canvas")
 game_element.append (canvas_element);
 var canvas_context = canvas_element[0].getContext ("2d");
 
-var game_height = game_element.height();
-var game_width = game_element.width();
-function linear_height (height) {
-  return game_element.height() - height;
+var visible_path_components = 600;
+var seconds_to_travel_visible = 10;
+var path_components_per_second = visible_path_components/seconds_to_travel_visible;
+var frames_per_second = 60;
+var path_components_per_frame = path_components_per_second/frames_per_second;
+var player_max_speed = 0.1; // in screens per second
+
+var game_height;
+var game_width;
+function linear_height (component_index) {
+  return game_element.height() *(1-component_index/visible_path_components);
 }
-function cylindrical_height (height) {
-  
-  return game_height - Math.sin (height*(Math.PI/2)/game_height)*game_height/(Math.PI/2);
+function cylindrical_height (component_index) {
+  return game_height - Math.sin (component_index/visible_path_components*(Math.PI/2))*game_height/(Math.PI/2);
 }
-var perspective_height = cylindrical_height
+var perspective_height = cylindrical_height;
 
 var player = {position: 0};
-var paths = [{info: {max_speed: 1}, data: [{position: 0, velocity: 0, acceleration: 0, element: $("<div/>") .addClass ("path_component")}]}];
+var paths = [{info: {max_speed: player_max_speed}, data: [{position: 0, velocity: 0, acceleration: 0, element: $("<div/>") .addClass ("path_component")}]}];
 
 var mouse_X = 0;
 var mouse_Y = 0;
@@ -50,23 +60,26 @@ game_element.mousemove (function (event) {
 function tick() {
   requestAnimationFrame (tick);
   
+  var game_top = top_bar.offset().top + top_bar.height();
+  var game_bottom = $(window).height() - bottom_bar.height();
+  game_element.height (game_bottom - game_top);
   var width = game_element.width();
   var height = game_element.height();
-  game_height = height ;
+  game_height = height;
   game_width = width;
   paths.forEach (function(path) {
-    while (path.data.length <600) {
+    while (path.data.length <visible_path_components) {
       var previous = path.data [path.data.length - 1];
       var current = {
-        position: previous.position + previous.velocity,
-        velocity: previous.velocity + previous.acceleration,
-        //acceleration: previous.acceleration + (Math.random()*2 - 1)/60/600,
+        position: previous.position + previous.velocity/path_components_per_second,
+        velocity: previous.velocity + previous.acceleration/path_components_per_second,
       };
-      var max_speed =path.info.max_speed / 600;
+      var max_speed =path.info.max_speed;
       
-      var bias = -previous.velocity*0.0001;
-      var max_acceleration = Math.min (previous.acceleration + max_speed*0.0006 + bias, (max_speed - previous.velocity)/30);
-      var min_acceleration = Math.max (previous.acceleration - max_speed*0.0006 + bias, (-max_speed - previous.velocity)/30);
+      var default_change_radius = max_speed*2.16/path_components_per_second;
+      var bias = -previous.velocity*0.36/path_components_per_second;
+      var max_acceleration = Math.min (previous.acceleration + default_change_radius + bias, (max_speed - previous.velocity)*20);
+      var min_acceleration = Math.max (previous.acceleration - default_change_radius + bias, (-max_speed - previous.velocity)*20);
       current.acceleration = min_acceleration +Math.random()*(max_acceleration - min_acceleration);
       
       
@@ -90,11 +103,13 @@ function tick() {
       path.data.push (current);
     }
     //console.log (path.data [0]);
+    
+    //TODO: avoid rounding error if we adjust the values
     var deleted;
-    //for (var i=0;i<10;++i){
+    for (var i=0;i<path_components_per_frame;++i){
       deleted = path.data.shift();
       //deleted.element.detach();
-    //}
+    }
     
     // you can't get TOO far away from the paths.
     // TODO: possibly better symbolism and gameplay if the paths stay near YOU instead
@@ -106,7 +121,7 @@ function tick() {
     }
     
     var component_width = function (index) {
-      return width*0.15*(height - index)/height; // * Math.sqrt ((1 + Math.abs (current.velocity)*width/(height/600)));
+      return width*0.15*(visible_path_components - index)/visible_path_components; // * Math.sqrt ((1 + Math.abs (current.velocity)*width/(height/visible_path_components)));
     };
     canvas_element.attr ("width", width).attr ("height", height);
     canvas_context.fillStyle = "rgb(0,0,0)";
@@ -114,7 +129,7 @@ function tick() {
     canvas_context.fillStyle = "rgb(255, 255, 255)";
     canvas_context.beginPath();
     var began = false;
-    var center_X = function (component, index) {return width*((component.position - player.position)*(height - index)/height + 0.5);};
+    var center_X = function (component, index) {return width*((component.position - player.position)*(visible_path_components - index)/visible_path_components+ 0.5);};
     path.data.forEach (function(current, index) {
      
       //current.element.css ("bottom", index).css ("left", game_element.width()*(current.position - deleted.position +0.45));
@@ -135,8 +150,8 @@ function tick() {
     canvas_context.fill();
   });
   
-  var player_velocity = Math.max (-1, Math.min (1, ((mouse_X/width) - 0.5)*10));
-  player.position += player_velocity/600;
+  var player_velocity_request = Math.max (-1, Math.min (1, ((mouse_X/width) - 0.5)*10));
+  player.position += player_velocity_request*player_max_speed/frames_per_second;
   
   canvas_context.beginPath();
   canvas_context.moveTo(width*0.47, height - 20);
@@ -158,7 +173,9 @@ tick();
       
       <div id="game"></div>
 
-    '''+ blog.comments_section ("the_path") +'''
+    '''+
+    # blog.comments_section ("the_path") +
+    '''
   </div>
 </main>'''), {
   "jQuery_before": True,
