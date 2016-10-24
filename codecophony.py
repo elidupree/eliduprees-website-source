cf5023984817a74a16ad528cb76807a817096243
Eli Dupree <vcs@elidupree.com>
2016-10-24 11:53:07 -0400

    Made some crude documentation

--- codecophony.py
+++ codecophony.py
@@ -42,6 +42,29 @@ flex: 1 1 50%;
 min-width: 0;
 }
 
+.project_select {
+  padding: 4%;
+}
+.project_select p {
+  text-align: center;
+  font-size: 130%;
+}
+.project_select h1 {
+  text-align: center;
+}
+.project_select h2 {
+  text-align: center;
+  font-weight: bold;
+  font-size: 150%;
+}
+.project_select button {
+  font-weight: bold;
+  font-size: 150%;
+}
+.project_select code {
+  white-space: nowrap;
+  background-color: #ddd;
+}
 .generated {
   border-top:3px solid black;
 }
@@ -65,7 +88,44 @@ span.no_underlay {position: absolute; top: 0; left: 0; z-index: 1;}
       '''+bars.bars_wrap({"games":True}, '''
 <main>
   <iframe sandbox="allow-scripts" id="sandbox" src="/media/codecophony-iframe.html"></iframe>
-  <div class="project_select "></div>
+  <div class="project_select ">
+    <h1>Welcome to Codecophony! This is an early prototype.</h1>
+    <p>Codecophony is a tool to compose music through programming, although it may be usable by non-programmers as well.</p>
+    <p>Create a new "project" using the button below, then edit it using the "edit" button. Then you'll see the edit screen. The left side lets you record sounds just like my <a href="/voice-practice-tool">voice practice tool</a>. The right side lets you create scripts. Here's an example script (try it out!):</p>
+    <pre>var notes = codecophony.scrawl (
+  `with instrument acoustic_guitar_steel duration 0.28
+
+play 0 then 2 then 3 then 5 then 7 lasting 4
+[with pitch -7 start -0.05 instrument trumpet
+  play 0 then 2 then 3 then 5 then 7 lasting 4
+]
+[with pitch 0 start -0.02
+  play -3 0 then -4 2 then -3 3 then -4 5 then -4 7 lasting 4
+]
+`
+)
+items.example_notes = notes;
+var example_sequence = items.example_sequence = codecophony.render_notes (notes);
+example_sequence.start += 4800;
+var a = items.input_sequence;
+codecophony.amplify (a, 0.14);
+items.combined_sequence = codecophony.add_sequences ([example_sequence, a]);
+</pre>
+  
+  <p>For this script to work, you will also need to import the <code>trumpet</code> and <code>acoustic_guitar_steel</code> instruments using the menu in the top right, and record something and rename the recording "input_sequence".</p>
+  
+  <p>These scripts are written in JavaScript. <code>codecophony</code> is a table containing various utility functions. <code>items</code> is a proxy table that lets you examine the "items" (recordings and other stuff) that you see on screen, and create new ones. A "sequence" is either a Float32Array, like those used by the web audio API, or <code>{start: &lt;starting time in number of 44100 Hz samples&gt;, data: Float32Array}</code>. lodash is available as <code>_</code>.</p>
+  
+  <h2>The codecophony.scrawl format:</h2>
+  <p><code>play</code> starts a new note at the default start time. <code>and</code> starts a new note at the same time as the last note you made. <code>then</code> starts a new note immediately after the last note ends. <code>with</code> sets defaults for all notes that come after it, until the end of the current [] block.</p>
+  
+  <p>Everything else sets the <em>attributes</em> of the note objects. <code>pitch</code> is additive, measured in number of semitones, starting at middle C. <code>duration</code> and <code>volume</code> are multiplicative. Everything else (like <code>instrument</code>) just overwrites its previous value. A number with no attribute name in front of it means pitch by default. Times are measured in seconds.</p>
+  
+  <p>A few other attributes exist. I'll eventually write full documentation.</p>
+  
+  <p>Your creations will be autosaved in your browser. I will eventually make a way to import and export projects, but I haven't yet. Right now, you can only copy/paste the scripts and save the generated sound files.</p>
+  
+  </div>
   <div class="project_editor "><div class="project_editor_inner ">
   <div class="tool_box">
     <div class="tool_box_top">
