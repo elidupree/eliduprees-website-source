3055e9115de7c23f73f121aac38b6747a8235e48
Eli Dupree <vcs@elidupree.com>
2016-10-28 19:23:04 -0400

    Kept writing The 23 Days Cult

--- posts/the_23_days_cult.py
+++ posts/the_23_days_cult.py
@@ -11,9 +11,13 @@ posts = [
   "head":'''<style>
 .death_note_page {
   width: 70%; text-align: justify; margin: 1.8em auto; text-indent: 0;
+  font-family: Georgia, serif
 }
 .death_note_page li{
   margin: 0;
+}
+div.death_note_page p{
+  text-indent: 0;
 }
   
 </style>''',
@@ -44,7 +48,7 @@ Light opened the notebook to the first page. But there was no name there. There 
 </div>
 
 <ul>
-<li>The human whose name is written in this note shall die.</li>
+<li>The humans whose names are written in this note shall die.</li>
 
 <li>This note will not take effect unless the writer has the subject's face in their mind when writing his/her name. Therefore, people sharing the same name will not be affected.</li>
 
@@ -64,7 +68,7 @@ Light nervously put zir hand on the corner of the page. <em>I guess I should che
 
 But the next page was blank. Light flipped another page. That one was also blank. In fact, the entire rest of the notebook was blank.
 
-<p><em>Well, that's a relief. Still… what do I do with this now? I don't know who the owner is, and they haven't showed up while I was thinking about it &ndash; or maybe &ndash; Light looked around quickly &ndash; maybe they <strong>did</strong> show up, but just didn't know they could trust me! I could just leave this here, so they can pick it up when I'm not looking. But then someone <strong>else</strong> might pick it up, and then that <strong>other</strong> person might report it! And if word gets out that someone found this "Death Note", anyone who <strong>saw</strong> the person drop it would be able to make the connection! I don't want to stop the owner from getting it back, but now I don't think I should take the risk of drawing any attention to it.</em></p>
+<p><em>Well, that's a relief. Still… what do I do with this now? I don't know who the owner is, and they haven't showed up while I was thinking about it &ndash; or maybe</em> &ndash; Light looked around quickly &ndash; <em>maybe they <strong>did</strong> show up, but just didn't know they could trust me! I could just leave this here, so they can pick it up when I'm not looking. But then someone <strong>else</strong> might pick it up, and then that <strong>other</strong> person might report it! And if word gets out that someone found this "Death Note", anyone who <strong>saw</strong> the person drop it would be able to make the connection! I don't want to stop the owner from getting it back, but now I don't think I should take the risk of drawing any attention to it.</em></p>
 
 <p><em>I guess I'd better hold on to this for now.</em></p>
 
@@ -72,7 +76,86 @@ Light stuck the notebook into zir bag and promptly forgot about it.
 
 <bigbreak>
 
+The next few weeks saw Light studying for the upcoming nationwide exams. Ze spent many evenings alone in their room, hard at work.
+
+Today was one such evening. Lights sat at a plain desk, in front of neatly organized shelves, steadily solving math problems. The TV news played in the background. <em>Why do I leave that on, anyway? I know it's all spin. Maybe it's a bad habit of mine. Or maybe biased news is better than no news. Who knows? I guess it helps to know what other people are watching, at least.</em>
+
+Light came to the end of a problem. <em>Now, where were my notes for the next class?</em> Light fished in zir bag, then fished some more, but didn't find them. Not wanting to waste my time, ze emptied the bag onto the desk.
+
+There it was again: <strong style="font-variant: small-caps">Death Note</strong>.
+
+<p><em>I almost forgot about that thing. I guess I'll never be able to return it…</em> Light idly flipped through it again, in case ze had missed anything, but didn't find anything new.</p>
+
+"The same assailant who attacked six people at a busy shopping district in Shinjuku yesterday," droned the news, "has struck again, taking eight people hostage at this daycare center… Police have identified the attacker as 42-year old Kurou Otaharada..."
+
+<p><em>Huh. Right there on the news, a name and a face. It's almost as if the universe is tempting me to write zir name in this "Death Note". Seems like a pretty clear-cut "kill one to save five" scenario. Well, kill one to save zero-to-eight, anyway. But ze killed multiple hostages yesterday, so we shouldn't exactly give zem the benefit of the doubt.</em></p>
+
+Light picked up a pen. <em>Am I really doing this? I know magic doesn't exist, right?</em>
+
+<p><em>But assuming magic doesn't exist, there's no reason <strong>not</strong> to do it either. Here goes nothing.</em></p>
+
+<p>Light wrote:</p>
+
+<div class="death_note_page">
+Kurou Otaharada
+</div>
+
+Light paused. <em>If I've gone this far, I might as well do the whole "cause of death" thing too. Supposedly, I've got 6 minutes and 40 seconds to come up with something. Let's see… what would be the best way for this situation to end?</em> Thinking quickly, Light continued:
+
+<div class="death_note_page">
+Kurou Otaharada
+
+Accident
+
+Has a change of heart and rushes to help the hostages, miraculously saving any that he's already injured, but then slips and falls while leaving the building, without hurting anybody else.
+</div>
+
+<p><em>Good enough, I guess. At least it's probably better than what will actually happen. What a pity magic isn't real.</em></p>
+
+Light sighed and went back to looking for zir class notes. Pretty soon, ze found them, tuned out the reporters' unnecessary speculation about the attacker's origin and motives, and got back to work for a few minutes.
+
+But then something on the news caught zir attention.
+
+"&ndash; it was unbelievable, even though he had stabbed her just a minute before, he stated treating her wounds as if he was a professional doctor &ndash;"
+
+<p><em>Wait. Was that one of the hostages? Are they already out of the building?</em></p>
+
+And then, in the corner of the screen, at the front of the building, someone else was coming out. Light could see zir face &ndash; it was Kurou Otaharada.
+
+And then the police were fanning out to intercept zem &ndash;
+
+And then ze was on the ground, even though none of the police had touched zem &ndash;
+
 <bigbreak>
+
+Light froze.
+
+<p><em>Magic <strong>can't</strong> be real, can it? I know that was a <strong>really</strong> unlikely coincidence, but magic being real has to be even <strong>more</strong> unlikely, right?</em></p>
+
+<p><em>But this isn't like regular fake evidence for magic, either. Usually, people see the coincidence first, <strong>then</strong> change their idea of magic to fit the evidence. It's not like a scientific experiment, where you pick a hypothesis first, then test that one hypothesis. But this &ndash; I <strong>did</strong> only have one prediction. I literally wrote it down!</em></p>
+
+<p><em>But I can't just say magic is real. At least I would need to test it more. Wait. Is that even true? If this coincidence wasn't big enough to convince me, why should two big coincidences in a row be enough?</em></p>
+
+<p><em>Fine. Let's say the chance of magic being real is <strong>one in a billion</strong>. What's the chance of a random person dying from a fall in a specific few minutes?</em></p>
+
+Light looked it up online.
+
+<p><em>Okay, so if we take 5 people per 100,000 per year…</em></p>
+
+<p><em>And multiply it by…</em></p>
+
+<p><em>And adjust it for…</em></p>
+
+<p><em>Then we end up with about…</em></p>
+
+<p><em><strong>One in a billion.</strong></em></p>
+
+<p><em>And then we have to multiply in the chance of Otaharada changing zir mind and knowing how to treat wounds like a doctor…</em></p>
+
+<p><em>Well, @#$%. Magic is real.</em></p>
+
+<p><em>Okay. I need to get my thoughts in order. Magic is real. I just killed somebody. And probably saved the lives of one or more other people. And mind-controlled the first person. Wait &ndash; <strong>mind-controlled?</strong> This is supposed to be a Death Note, and it also does mind control? I guess "details of the death" is pretty open-ended…</em></p>
+
 
 <bigbreak>
 
