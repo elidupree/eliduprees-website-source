4455784ff88cf3a45f09b9162f5d56ee019f410f
Eli Dupree <vcs@elidupree.com>
2016-11-30 10:51:40 -0500

    Listed pages for studio art

--- studio_art.py
+++ studio_art.py
@@ -20,3 +20,33 @@ html.studio_art div.comic_transcript_inner {
   color: black; }
 html.studio_art .complete_comic {color:#999;}
 ''')
+
+pages = [
+
+{"source": "postage_stamp.jpg", "chapter_start": "Breadth", "annotation":''''''},
+{"source": "fiery_stuff.jpg", "annotation":''''''},
+{"source": "coat.jpg", "annotation":''''''},
+{"source": "watch.jpg", "annotation":''''''},
+{"source": "escher.jpg", "annotation":''''''},
+{"source": "devil_phone.jpg", "annotation":''''''},
+{"source": "conte_figure.jpg", "annotation":''''''},
+{"source": "rosebush.jpg", "annotation":''''''},
+{"source": "negative_space.jpg", "annotation":''''''},
+{"source": "light_switch.jpg", "annotation":''''''},
+{"source": "landscape.jpg", "annotation":''''''},
+{"source": "seascape.jpg", "annotation":''''''},
+{"source": "looking_down.jpg", "chapter_start": "Concentration", "annotation":''''''},
+{"source": "pastel_gateway.jpg", "annotation":''''''},
+{"source": "flashlight.jpg", "annotation":''''''},
+{"source": "st_lukes_tower.jpg", "annotation":''''''},
+{"source": "5_pillars.jpg", "annotation":''''''},
+{"source": "scratchboard_gateway.jpg", "annotation":''''''},
+{"source": "stupid_slogan.jpg", "annotation":''''''},
+{"source": "hand_reaching.jpg", "annotation":''''''},
+{"source": "1_pillar.jpg", "annotation":''''''},
+{"source": "red_shirt.jpg", "annotation":''''''},
+{"source": "buddha.jpg", "annotation":''''''},
+{"source": "candle.jpg", "annotation":''''''},
+
+
+]
