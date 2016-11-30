d896a89cbc14ddb7ceaab55cf9ac0528d8a6fd39
Eli Dupree <vcs@elidupree.com>
2016-11-30 10:25:19 -0500

    Started work on publishing studio art portfolio

---
+++ studio_art.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import css
+
+
+css.insert('''
+
+html.studio_art div.bars_outer_box {
+  background-color: #ffffff; }
+html.studio_art div.comic_content_warning_text {
+  color: black; }
+html.studio_art .meta_controls_coloring {
+  color: #55f; }
+html.studio_art .comic_metabar {color:#555;}
+html.studio_art div.comic_toggle_content_warnings {
+  color: black; }
+
+html.studio_art div.comic_transcript_inner {
+  color: black; }
+html.studio_art .complete_comic {color:#999;}
+''')
