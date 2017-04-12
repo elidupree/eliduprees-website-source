45411549e0e4c1ab9527eeec851d1d5483a4ecab
Eli Dupree <vcs@elidupree.com>
2017-04-12 14:34:28 -0400

    note, dep

--- blog.py
+++ blog.py
@@ -1006,6 +1006,7 @@ current_blog_page_extras = [
 {"title": "New collection of drawings from a studio art class!", "stream_entry": ("/ap-studio-art", "New collection of drawings from a studio art class!")},
 {"title": "No blog post today", "stream_entry": ("/", "No blog post today. I have a sore in my mouth, so I can't dictate and/or type enough. I MIGHT post one on Friday if it gets better soon.")},
 {"title": "No blog post today-2", "stream_entry": ("/", "No blog post today. I have a sore in my mouth, so I can't dictate and/or type enough.")},
+{"title": "No blog post today-3", "stream_entry": ("/", "No blog post AGAIN today. This time I've had throat problems for days. :-(.")},
 ]
 def consider_list_for_current_page (list, limit = 8):
   for index in range (1, min (1 + limit, 1 + len( list))):
