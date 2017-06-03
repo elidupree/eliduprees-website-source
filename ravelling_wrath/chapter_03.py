1621cf7883e27884c91e95a209506683c7fabe94
Eli Dupree <vcs@elidupree.com>
2017-06-02 23:14:10 -0400

    Split off the already-written parts of chapter 3 into a separate file

---
+++ ravelling_wrath/chapter_03.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import datetime
+from post_contents_utils import *
+from ravelling_wrath.definitions import *
+
+posts = [
+{
+  "title":"Ravelling Wrath, chapter 3",
+  "blurb": blurb,
+  "auto_paragraphs": True,
+  "don't deploy": True,
+  "authors_notes":'''
+
+
+  ''',
+  "head":'''<style>
+div.blog_post h2 {
+  text-align: center;
+}
+
+  </style>''',
+  "contents": '''
+
+This chapter contains:
+; discussion of an abusive relationship; discussion of sexual assault; ?????
+'''+ content_warning_header ('''<p>Description of anti-gay attitudes; verbally abusive language; brief mention of sexual assault.</p>''')+'''
+
+<bigbreak>
+  
+?????
+
+I took her hand and we started walking.
+
+"You still haven't told me why you can't go to the place near the school," Yali said as we walked.
+
+"Ugh, I don't want to tell that story. I was kind of irresponsible."
+
+"Aww. I love hearing stories about you being irresponsible."
+
+"You do?! I only wasn't going to tell you because you're always telling me to be careful and stuff –"
+
+"I don't want you to get hurt <em>now</em>! But if it already happened – By the Stern, if I've missed out on four and a half weeks of fun stories because you thought I wouldn't like them…"
+
+"You totally have! I have <em>so</em> many to tell you. This one, though… eh…"
+
+"You don't have to if you don't want to," said Yali soothingly.
+
+"Hang on, I just remembered this story <em>also</em> has a point in it where I got really angry. So it's kind of killing two birds with one stone if I tell it. Sure, I'll do it," I said. I danced a few steps ahead and then looked back, waving my arms like a stage magician. "The Adventures of Rinn Akatura, Chapter 752: The Fast Food Place," I began. "So, there was this girl –"
+
+Yali smirked.
+
+"Hey! I haven't told you the best part yet! I haven't told you <em>any</em> of the parts!"
+
+Yali doubled over, laughing silently. I couldn't help laughing along a little bit. Then she straightened up. "Sorry. You can keep going now."
+
+I let Yali catch up with me and held her hand again. "So, there was this girl I liked. I'm not going to say who it was, because I think she wants to forget about all this now, but anyway. I asked her out, and we had a good time, so we went on another date after that, and so on. Although they were less 'dates' and more 'me taking her to the movies or whatever so that we could make out for the whole time'. It wasn't a real relationship, we were just horny teenagers." That felt a little weird to say. "Ha ha, Waiting God knows I'm still a horny teenager. Buuu-uuut I'm in a real relationship now –" I squeezed Yali's hand "– and I'm fine if you want to take it slow."
+
+"Uh... right," said Yali. "Go on with the story!"
+
+"Alright! So I kept taking her places, but I'd never had much spending money, so I ran out pretty fast. So I got myself an under-the-table job working in the kitchen at the fast food place."
+
+"Wow. How old were you?"
+
+"This was a three years ago. I was like 13. Sure it was illegal and stuff, but whatever. That got me enough money to keep paying for 'dates'. Until… ugh, I feel really stupid about this next part."
+
+"It's okay. I won't judge you."
+
+I glanced around to make sure no one else on the street was listening. "Yeeeeaaaah… we got caught at the movies with our hands down each other's pants."
+
+"Wow."
+
+"And then like… word got around. And like, <em>I</em> can laugh off that kind of thing, but I guess it hurt a lot more for her. She's basically been avoiding me ever since then, and I don't even blame her, I feel like total shit for putting her in that situation. And then I lost a bunch of friends over <em>that</em> too."
+
+"Did they drop you because of the rumors?"
+
+"No, that's the worst part. None of <em>my</em> friends would have a problem with some wild stuff –"
+
+"Right, silly me –"
+
+"– but I had people trying to, like, <em>high-five</em> me for 'getting some action'. But that's <em>not</em> what <em>happened</em>! It wasn't <em>cool</em>, it was a <em>mistake</em>, I <em>hurt</em> someone! Someone I <em>liked</em>! So if anyone thinks they can <em>congratulate</em> me for that, like if they think that's what I <em>wanted</em> – just, <em>argh!</em>"
+
+"It makes you really angry."
+
+"Yeah! If somebody just says 'fuck you, Rinn' or whatever, I know where I stand with them, you know? But when people <em>act</em> like they're on my side when they're <em>not</em>... I basically just told them to fuck off. I wish I had come up with something smarter to say."
+
+"Maybe we can use this. I can <em>see</em> you getting angry right now, more than we managed the other day."
+
+"You're totally right." I hadn't even noticed, but my whole body felt charged up.
+
+"You're hurting my fingers, by the way."
+
+"Shit, sorry." I let go of Yali's hand. She shook it out.
+
+"It's fine," she said. She looked at me curiously. "Before we get to the serious stuff, why don't you finish the story? You got caught in the movies, but what happened to the fast food place?"
+
+"Oh, right. Well, I was kind of upset for the next few days, so I wasn't really paying attention, and I let a cop see me going in and out of the kitchen. He started to make a fuss, so we all pretended I was just there to visit a cousin who worked there. But they basically told me not to come back after that, to make sure they wouldn't get in trouble. Kind of anticlimactic, I know."
+
+????
+
+I noticed that Yali was still smiling at me. "I can't believe you actually liked listening to <em>that</em> story," I said.
+
+"You have no <em>idea</em> how adorable you are."
+
+"Okay but seriously <em>that</em> story? That story sucked."
+
+Yali tried to make a more serious face. "I know it's <em>bad</em>, what happened. But there's just something about, about, about you being so… free to feel things. It makes me want to go with you on all your adventures. Even though when I actually think about it, I might be better off watching your adventures from a safe distance."
+
+I smirked. "You know, if you ever want to go on an adventure –"
+
+"No, I'm good. I can't wait to hear some more of your stories, though."
+
+"Okay! Let me tell you one of actually good ones. Like about that time when I –"
+
+"Just a minute. I really want to hear your stories. But the problem is, I'd love to spend <em>all afternoon</em> hearing them. And then we won't get to the thing we were going to do first."
+
+"Which thing – oh yeah, the thing that got me angry. Way to pick out another downer," I said.
+
+"Is it that bad?"
+
+"Do you know name and name??????" I said quietly.
+
+"I hope she dumps him already," Yali muttered.
+
+"I know, right? He treats her like shit! And I <em>like</em> her. Not in a romantic way, just in an I-really-don't-want-anything-bad-to-happen-to-her way. So I can't stand it when I see him talking down to her. But that's not the <em>real</em> problem. Not the real problem for me, I mean. Or it wasn't at first. It was just, you know…" I paused, fishing for words.
+
+"Just your normal. Your 'mad, but not <em>really</em> mad'."
+
+"Yeah, that's it. But then…" I grimaced. "There was this one time when he was, like, pushing her around, putting her down in front of everybody, and I told him to stop it. But he just kept at it, like he was daring me to do something about it. So I kind of punched him a little bit."
+
+"A little bit."
+
+"Okay, fine, more than a little bit. But it's not like I put him in the hospital or anything. He was fine. But a couple days later, <em>she</em> came up to me in private and told me that he was acting really bitter about it, and he was dumping all the blame on her."
+
+"Oh no."
+
+"Yeah. She basically begged me to leave him alone, to make sure that wouldn't happen again. And I obviously couldn't say no to that, you know? She's the victim here. But now whenever I see them talking together, it's like… I'm so mad, but I can't <em>do</em> anything about it. I just <em>stew</em> about it, for, like, an hour."
+
+"Only an hour? Not longer?"
+
+"Hey! An hour is a long time! For me, anyway."
+
+"I'm not judging. You can keep going."
+
+"That's the whole story though. It just sucks." I idly kicked the leg of the table. "So… what do you think? Do you see any way we can use this? Seems hard."
+
+"True. We probably shouldn't get me an abusive boyfriend in order to make you mad."
+
+"Oh my gods, I can't believe you even thought of that."
+
+Yali chuckled. Then she focused again. "These two stories have a lot in common," she said.
+
+"They do?"
+
+"Think about it. In both of them, you accidentally – Well, I was going to say 'accidentally hurt', but it wasn't <em>you</em> who hurt – actually, it was other people who did the actual hurting, both times. But you felt responsible. And both times, the person who got hurt was a girl you like."
+
+"That… I guess they <em>are</em> really similar."
+
+"And both of them… don't have any closure, and they end with you feeling like you don't know what you can do about it." Yali looked at me. "Would you say you felt… helpless?"
+
+"Ugh, I don't want to <em>say</em> I felt like that, but I don't know… maybe? Why do you ask?"
+
+"'Anger turns helplessness into strength,'" Yali intoned.
+
+"Hey, is that a quote from somewhere?"
+
+"We had a book full of sayings in the, the, when I was growing up. It was a really old book, so it still had some of the Blood God's sayings, from back when the Blood God was worshipped. That's one of them."
+
+"'Anger turns helplessness into strength,'" I repeated, trying it out. "I can see that. I'm not sure it's always true, though."
+
+"Same."
+????
+
+"I want you to try to
+
+?????
+
+Yali took my hand. Her voice was soft and serious. "Before we make plans, I want to make sure whether you really want to do this. It's okay if you say no, or that I'm dragging you into things too fast."
+
+"I'm fine with it. It seems like a good –"
+
+"Stop. Just this once, can I ask you to take some time and think it through? If this works, it'll be a really unpleasant experience for you. So I don't want to do it unless you're sure about it. Think about whether you'd want to do it if it wasn't my idea."
+
+"Okay," I said. I looked down at the table. "How can it be not your idea, though? It's, like… if it's not your idea, it's completely different."
+
+"Well… Suppose it was <em>your</em> idea. And you suggested it to me, and I said I'd do it if you wanted to, but I didn't really care one way or the other. Would you ask me to do it?"
+
+"I…" I thought about it.
+
+
+
+
+
+
+''',
+},
+]
+
