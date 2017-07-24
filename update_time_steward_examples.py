568a1f8e063610bf99740ab996c5062f9082fb2e
Eli Dupree <vcs@elidupree.com>
2017-07-24 12:22:15 -0400

    Made a script to copy over the data we use for the TimeSteward examples

---
+++ update_time_steward_examples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+import os
+import sys
+import os.path
+import shutil
+
+directory = sys.argv[1]
+shutil.copy(os.path.join (directory, "emscripten-examples.js", "./media/vendor/time-steward-examples/emscripten-examples.js")
+shutil.copy(os.path.join (directory, "emscripten-examples.css", "./media/vendor/time-steward-examples/emscripten-examples.css")
+shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/bouncy_circles.js", "./media/vendor/time-steward-examples/bouncy-circles.js")
+shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/simple_diffusion.js", "./media/vendor/time-steward-examples/simple-diffusion.js")
