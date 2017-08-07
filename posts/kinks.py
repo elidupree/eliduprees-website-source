c44e7eb36600ecda5282b860b1af1b6e78e4ae0a
Eli Dupree <vcs@elidupree.com>
2017-08-07 00:20:47 -0400

    more work on kink generator

--- posts/kinks.py
+++ posts/kinks.py
@@ -37,12 +37,12 @@ multiple_days = 7
 indefinitely = 7
 
 stimulation_list = [
-  simple_item ("push me around", 0, 1, 0, 2, immediate, half_hour, "TODO description", {"no_self": True}),
+  simple_item ("push me around", 0, 1, 0, 2, immediate, half_hour, "TODO description", {"no_self": True, "not_when_tied": True}),
   simple_item ("pin me down", 0, 1, 0, 3, immediate, half_hour, "TODO description", {"no_self": True}),
   simple_item ("grab me by the hair", 1, 3, 0, 2, immediate, several_minutes, "A great combination of the slight pain of pulling my hair with physical control over the position of my head. Just be careful not to bend my neck in a weird direction."),
   simple_item ("slap me in the face", 1, 4, 0, 0, immediate, several_minutes, "TODO description"),
-  simple_item ("put your hand threateningly on my throat and/or balls", 0, 1, 2, 4, immediate, immediate + 1, "I don't do actual choking because of the risks, but the threat is exciting.", {"no_self": True, "variants": [
-    simple_item ("put your hand threateningly on my "+noun, 0, 1, 2, 4, immediate, immediate + 1, "", {"no_self": True})
+  simple_item ("put your hand threateningly on my throat and/or balls", 0, 1, 2, 3, immediate, immediate + 1, "I don't do actual choking because of the risks, but the threat is exciting.", {"no_self": True, "variants": [
+    simple_item ("put your hand threateningly on my "+noun, 0, 1, 2, 4, immediate, immediate + 1, "", {"no_self": True, "weight":0.5})
     for noun in ["throat","balls"]
   ]}),
   simple_item ("pull off my clothes", 0, 2, 1, 4, several_minutes, several_minutes, "This kind of requires my participation, but only a little.", {"weight":3, "no_self": True}), # no_self because there's already a participation version
@@ -59,8 +59,8 @@ stimulation_list = [
     simple_item (verb+" me in the "+noun, 1 + intensity, 5 + intensity, 0, 2, time, half_hour, "")
     for (verb, time, intensity) in [("slap", immediate, 0), ("whip", immediate + 1, 2)] for noun in ["chest","nipples","chest and stomach","inner thighs"]
   ]}),
-  simple_item ("spray me with water", 0, 3, 1, 5, hours, hours, "TODO description"),
-  simple_item ("dunk me in water", 1, 2, 1, 4, hours, half_hour, "TODO description"),
+  simple_item ("spray me with water", 0, 3, 1, 5, hours, hours, "TODO description", {"not_when_tied": True}),
+  simple_item ("dunk me in water", 1, 2, 1, 4, hours, half_hour, "TODO description", {"not_when_tied": True}),
   simple_item ("hit me in the balls", 2, 7, 1, 3, immediate, immediate + 1, '''Some people worry that the sudden pain of ball-kicking can risk causing a heart attack. I normally avoid risk, but in this case, I think that I get a lot less pain than most people with testicles do. Maybe mine are desensitized because I've hit them for fun so many times, or maybe they've just always been less sensitive. When I was a little kid, I got hit there once during a soccer game, and my reaction was like "This pain is kind of weird, but why is everyone acting so empathetic? It's not THAT bad."<br><br>The real caveat is that if my balls get hit a lot of times, they'll be achy for the next few days, which is inconvenient. Hitting them <em>hard</em> is good if you do it once or a few times, but hitting them <em>repeatedly</em> is a bigger commitment.<br><br>Since I've done this to myself a lot of times, I've noticed some interesting things about its effects. One is that getting hit in the balls makes me slightly more aroused for a while, on a mostly physical level – it's separate from the mental arousal I get from feeling vulnerable. Another thing is that hitting myself in the balls can help clear gas pressure from my digestive system. My reaction must make me tense some combination of muscles that I don't have conscious control over.''', {"variants":[
     simple_item (verb+" me in the balls", 2, 7, 1, 3, immediate, immediate + 1, "", {"weight":0.6})
     for verb in ["punch","punch","kick"]
@@ -74,7 +74,12 @@ stimulation_list = [
 
 participation_list = [
   simple_item ("strip naked", 0, 1, 0, 3, several_minutes, several_minutes, "TODO description", {"weight":2}),
-  simple_item ("do exercises (e.g. squats, situps, jumping jacks)", 1, 5, 0, 2, several_minutes, hours, "TODO description"),
+  simple_item ("do exercises (e.g. squats, sit-ups, jumping jacks)", 1, 5, 0, 2, several_minutes, hours, "I wish I could include push-ups in this list, but my hand problems currently rule them out.", {"variants":[
+    simple_item ("do "+str(number)+" "+exercises, intensity, intensity, 0, 2, time, time, "", {"weight": 0.1})
+    for (exercises, multiplier) in [("squats", 1), ("sit-ups", 1), ("jumping jacks", 2)]
+    for (number, intensity, time) in [(20*multiplier, 2, 2), (30*multiplier, 3, 2), (40*multiplier, 4, 1+multiplier), (60*multiplier, 5, 1+multiplier)]
+  ] #TODO: walking? Running? Bicycling?
+  }),
   simple_item ("get in a cold shower", 3, 5, 0, 0, hours, half_hour, "TODO description"),
   simple_item ("get in a shower with my clothes still on", 1, 2, 0, 2, hours, half_hour, "TODO description"),
   simple_item ("sleep naked overnight", 0, 0, 0, 2, multiple_days, multiple_days, "TODO description"),
@@ -89,12 +94,12 @@ conditions_list = [
   simple_item ("I'm gagged", 0, 2, 0, 2, half_hour, half_hour, "Honestly, gags aren't very believable. They can't actually stop you from yelling – or even talking understandably – unless they dangerously block your airways. But it's still exciting to have one pushed into my mouth, because it's an intimate space that I wouldn't normally let such things into.", {"inflict": "gag me"}),
   simple_item ("I'm kneeling", 0, 2, 0, 0, immediate, hours, "Aside from the symbolic meaning of kneeling, I'm also a fairly tall person. Kneeling forces me to me look up at my partners instead of down, which makes them feel bigger and more dangerous.", {"assume": "kneel", "enhance": True}),
   simple_item ("I'm forced to remain naked/partially naked", 0, 1, 0, 3, half_hour, multiple_days, """In reality, I don't have any discomfort about being naked around other people. But if I <em>pretend</em> that I do, then have nudity "forced" on me, it can become a great vulnerability thing. It's not the fact of <em>being</em> naked that's exciting, it's the idea that you can keep me that way whether I want you to or not.<br><br>Interestingly, wearing just my underpants doesn't feel the most vulnerable. I think it has to do with the point of reference. If I'm topless, it feels like "I have clothes on <em>except that my chest and underarms are exposed</em>." If I'm wearing underpants and a <em>shirt</em>, it feels like "I have clothes on <em>except that my thighs are exposed</em>." But with just underpants, it feels like "I'm naked <em>except that my crotch is covered</em>." """, {"variants": [
-    simple_item ("I'm forced to remain "+state, 0, 1, 0, 3, half_hour, multiple_days, "", {"maintain": "remain "+state, "long_term": True})
+    simple_item ("I'm forced to remain "+state, 0, 1, 0, 3, half_hour, multiple_days, "", {"maintain": "remain "+state, "non_interfering": True})
     for state in ["naked", "topless", "wearing only a shirt", "wearing only a shirt and underpants"]
   ]}),
   simple_item ("I'm forced to remain naked when it's cold", 1, 3, 0, 3, half_hour, hours, "TODO description", {}),
-  simple_item ("I have clothespins on my nipples", 0, 5, 0, 3, immediate + 1, half_hour, """Interestingly, depending on the exact angle, clothespins can cause anything from "no pain at all" to "lots of pain". If you want, you can order me to adjust them to whatever level of pain you specify.""", {"inflict": "put clothespins on my nipples"}),
-  simple_item ("I'm wearing a collar", 0, 1, 0, 2, several_minutes, hours, "Collars aren't as exciting for me as they are for people who do power dynamics, but the slight pressure on my neck still makes me feel nice and vulnerable.", {"inflict": "put a collar on me"}),
+  simple_item ("I have clothespins on my nipples", 0, 5, 0, 3, immediate + 1, half_hour, """Interestingly, depending on the exact angle, clothespins can cause anything from "no pain at all" to "lots of pain". If you want, you can order me to adjust them to whatever level of pain you specify.""", {"inflict": "put clothespins on my nipples", "non_interfering": True}),
+  simple_item ("I'm wearing a collar", 0, 1, 0, 2, several_minutes, hours, "Collars aren't as exciting for me as they are for people who do power dynamics, but the slight pressure on my neck still makes me feel nice and vulnerable.", {"inflict": "put a collar on me", "maintain": "wear a collar"}),
   simple_item ("I'm forced to stand in a stress position, like with my legs bent and/or my arms over my head", 2, 5, 0, 4, immediate + 1, half_hour, "Having my muscles straining makes all pleasure sensations much more intense. It's one of the same reasons that I enjoy struggling in bondage.", {"maintain": "stand in a stress position", "enhance": True}),
   simple_item ("I'm in a predicament bondage position", 4, 7, 0, 4, half_hour, half_hour, "TODO description", {"inflict": "put me in predicament bondage", "enhance": True}),
   simple_item ("I'm forced to stay perfectly still", 0, 1, 0, 1, immediate, half_hour, """I'm really good at suppressing my own reactions, but it does take a mental effort. There's a lot of facets to this kind of restraint. First, I can't protect myself against your touch because of my own suppression. Second, when you do touch me, it not only forces the sensation on me, but also forces the internal mental effort. And finally, there's the fear of however you might "punish" me if I mess up and move.""", {"maintain": "stay perfectly still", "enhance": True, "possible_tied": True}),
@@ -123,10 +128,10 @@ bondage_list = [
   brief_item ("legs tied together at both the ankles and the knees", half_hour, hours, "Whenever I'm tied up, I get an extra surge of vulnerability whenever one of my automatic movements is blocked. Tying my legs at the knees blocks a <em>lot</em> of automatic movements that I don't even think about normally.", {}),
   brief_item ("big toes tied together", half_hour, hours, "Like with the knees, tying my toes blocks a lot of movements that I don't <em>expect</em> to be blocked.", {}),
   brief_item ("ankles and toes tied to a bed frame above, with my body face-down", half_hour, hours, "Having my feet exposed and tortured isn't my <em>main</em> kink, but this position is great for it. It makes me feel so helpless. Not only am I unable to stop you, but it's hard for me to even see what you're doing.", {}),
-  brief_item ("hands and/or ankles tied together loosely, so I can still walk but it's awkward", half_hour, all_afternoon, "This one is good if you want to keep me tied up for a while without getting uncomfortable, or if you want to force me to move around while partially tied up.", {"long_term": True}),
+  brief_item ("hands and/or ankles tied together loosely, so I can still walk but it's awkward", half_hour, all_afternoon, "This one is good if you want to keep me tied up for a while without getting uncomfortable, or if you want to force me to move around while partially tied up.", {"non_interfering": True}),
   brief_item ("ankles each tied to the same thigh", half_hour, hours, "Note: We can't do the same thing with my arms, because I need to be careful about ulnar nerve problems from folding up the elbows.", {}),
   brief_item ("tied thoroughly to a rigid object (bed frame? pole?) so that I'm as immobile as possible", half_hour, half_hour, "", {}),
-  brief_item ("ankles tied to my waist so that I can't stand up all the way", half_hour, all_afternoon, "This could be part of a stress position. Or you could force me to walk around while I'm tied like this.", {"long_term": True}),
+  brief_item ("ankles tied to my waist so that I can't stand up all the way", half_hour, all_afternoon, "This could be part of a stress position. Or you could force me to walk around while I'm tied like this.", {"non_interfering": True}),
   brief_item ("nipple-clothespins tied to an overhead hook, so I have to stand on my tiptoes", half_hour, half_hour, "", {}),
   brief_item ("nipple-clothespins tied to an overhead hook to wrists/thumbs/ankles/toes, so I have to keep holding up my arms/legs", half_hour, half_hour, "", {}),
 ]
@@ -266,6 +271,7 @@ function rename (item, name) {
 preprocessed_lists.forEach(function(list) {
   var varying = [];
   list.list = list.list.filter(function(item) {
+    item.original_list = list.id
     if (item.variants) {
       Array.prototype.push.apply (varying, item.variants);
       return false;
@@ -282,17 +288,17 @@ preprocessed_lists.forEach(function(list) {
   list.list.forEach(function(item) {
     item.max_sensation = Math.max (item.max_unpleasantness, item.max_pleasantness);
     if (item.inflict) {
-      preprocessed_lists[stimulation].list.push (adjust (item, {name: item.inflict, original_list: list.name}));
+      preprocessed_lists[stimulation].list.push (adjust (item, {name: item.inflict, from_different_list: true}));
     }
-    if (item.assume) {
-      preprocessed_lists[participation].list.push (adjust (item, {name: item.assume, original_list: list.name}));
-    }
+    /*if (item.assume) {
+      preprocessed_lists[participation].list.push (adjust (item, {name: item.assume, from_different_list: true}));
+    }*/
   });
 });
 Array.prototype.push.apply (preprocessed_lists[participation].list, preprocessed_lists[stimulation].list.filter (item => !item.no_self).map (item => {
   return adjust (item, {
     name: item.name.replace (/\bme\b/, "myself").replace (/\bmy\b/, "my own").replace (/\byour\b/, "my"),
-    weight: (item.weight || 1) / (item.max_unpleasantness > 3 ? 3 : 1000),
+    weight: (item.weight || 1) * item.max_unpleasantness * item.max_unpleasantness / 100,
   });
 }));
 
@@ -366,21 +372,28 @@ function make_me_participate(lists) {
       return "while " + choose (lists[conditions].filter (item => item.enhance && !item.maintain)) + ", " + inflict(lists);
     }
     function challenge_to_maintain (lists) {
-      var inflict_first = choose (lists [stimulation].filter (item => item.max_unpleasantness >= 5 && !item.original_list));
+      var inflict_first = choose (lists [stimulation].filter (item => item.max_unpleasantness >= 5 && !item.from_different_list));
       return "challenge me to " + maintain_condition(lists, true) + " while you " + inflict_first + ". If I can't keep it up, " + choose (lists [stimulation].filter (item => item.max_sensation >= 7 && item.name !== inflict_first));
     }
-  
+function casually_force_condition (lists) {
+      return "casually force me to " + choose (lists [participation].filter (item => item.maintain && item.non_interfering)) + " for a while";
+    }
+function casually_inflict (lists) {
+      return "from time to time, casually " + choose (lists [stimulation].filter (item => item.min_time <= 2));
+    }
+    
 function global_generate (parameters, generators) {
-  var filter = function (item) {
+  var filter = function (item, list) {
     return (
       item.min_unpleasantness <= parameters.max_unpleasantness &&
       item.min_time <= parameters.max_time &&
       (parameters.caveat_allowed || !item.caveat) &&
-      (parameters.unsure_allowed || !item.unsure)
+      (parameters.unsure_allowed || !item.unsure) &&
+      (!parameters.remote_only || list.id !== "stimulation")
     )
   }
   var filtered_lists = preprocessed_lists.map (function (list) {return list.list.filter (function(item) {
-    return filter(item) && (!parameters.filter || parameters.filter(item, list))
+    return filter(item, list) && (!parameters.filter || parameters.filter(item, list))
   });});
 
   //[].concat(
@@ -398,6 +411,9 @@ function global_generate (parameters, generators) {
 function UI_generate(generators, parameters) {
   parameters = Object.assign({}, parameters);
   var max_time = Math.min($("#max_time").val(), parameters.max_time || 7);
+  if ($("#remote").prop ("checked")) {
+    parameters.remote_only = true
+  }
   delete parameters.max_time;
   return global_generate(Object.assign({max_unpleasantness: $("#max_unpleasantness").val(), max_time, caveat_allowed: true}, parameters), generators);
 }
@@ -414,10 +430,11 @@ a way to tie me up
 function button (text, generators, parameters) {
   return $("<button>").css("display", "block").text ("Pick "+text).click (function() {
     var result = UI_generate(generators, parameters);
-    for (var index = 1; index < $("#number").val();++index) {
+    var number = $("#number").val();
+    for (var index = 1; index < number;++index) {
       result += "\n"+UI_generate(generators, parameters);
     }
-    display.text(result);
+    display.text(result).css("height", ""+(2+number*10/7)+"em");
   });
 }
 
@@ -427,12 +444,19 @@ $("#kink_generator").append (
   
   "Generate ",
   $("<input>", {id: "number", type:"number", min: 1, max: 100, value: 7,}),
-  "activities, with no more than",
+  " activities, with no more than ",
   $("<input>", {id: "max_unpleasantness", type:"number", min: 0, max: 10, value: 10,}),
-  "unpleasantness, which take no longer than",
+  " unpleasantness, which take no longer than ",
   $("<input>", {id: "max_time", type:"number", min: 1, max: 7, value: 7,}),
-  "time",
+  " time",
+  $("<br>"),
+  $("<input>", {id: "remote", type:"checkbox",}),
+  $("<label>", {for:"remote", text:"only list things that we can do remotely (e.g. by email or video chat)"}),
   
+  button("any thing for us to do when we're hanging out", 
+    [inflict, inflict, inflict, I_must_participate, I_must_participate, maintain_while_inflict, inflict_until_surrender, threaten_unless_surrender, challenge_to_maintain, casually_force_condition, casually_inflict],
+    {}
+  ),
   button("something to do to me while I'm tied up", 
     [inflict, inflict, inflict, inflict, inflict, inflict, maintain_while_inflict, inflict_until_surrender, threaten_unless_surrender, challenge_to_maintain],
     {filter: function (item, list) {
@@ -440,8 +464,16 @@ $("#kink_generator").append (
     }}
   ),
   button("a punishment for losing one round of a game", 
-    [I_must_strip, inflict, inflict, I_must_participate, I_must_participate, maintain_while_inflict ],
-    { max_time: 3, }
+    [I_must_strip, inflict, inflict, inflict, I_must_participate, I_must_participate, maintain_while_inflict ],
+    {
+      max_time: 3,
+      filter: function (item, list) {
+        return (item.original_list !== "stimulation" || item.max_sensation >= 4) &&
+               (item.original_list !== "conditions" || item.non_interfering) &&
+               (item.name !== "strip naked")
+
+      }
+    }
   ),
   
   display
