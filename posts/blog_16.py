ead1228a96e02062e10ac72a4499031d4b2fd674
Eli Dupree <vcs@elidupree.com>
2017-08-16 16:11:07 -0400

    wrote blog post

--- posts/blog_16.py
+++ posts/blog_16.py
@@ -208,6 +208,66 @@ My current work is fixing that. The old TimeSteward did a lot of extra computati
 ''',
 },
 
+{
+"title": "Consent and Conflictedness",
+"tags": ["sex"],
+"auto_paragraphs": True,
+"blurb": "",
+"contents":'''
+
+Suppose I have a friend who knows I'm interested in them sexually, but hasn't decided to do anything sexual with me before. One day, this friend comes up to me, obviously <strong>upset</strong>, and says they want to have sex with me. What should I do?
+
+On one hand, I have an immediate feeling of "<em>this is wrong</em>". I feel like, while they're in this emotional state, doing anything would risk hurting them in the long-term. On the other hand, I believe that people have the right to choose what risks to take for themselves, and it feels wrong to say "I'm refusing for <em>your</em> sake" when they clearly indicated consent. When I was younger, a lot of adults did things that hurt me while saying they were helping me. I don't want to act like that towards anyone else.
+
+I came up with this post while trying to reconcile those two feelings.
+
+<h2>Parts of self</h2>
+
+A person isn't a <cut>rigid box with fixed opinions about everything. Their outward beliefs may change based on how they're feeling or what context they're in. They may also have internal conflict, where they disagree with <em>themselves</em> about what to think.
+
+It can help to model this as <em>parts of self</em>. Ideally, most parts of yourself agree about who you are and what you want. But there will always be parts that have different interests, or even disagree about what you, as a whole, should be doing. You could think of it as a sort of internal family argument, like this:
+
+"I want ice cream!"
+
+"But you know that when we eat ice cream, we get a headache. I don't want us to get a headache!"
+
+"But I wanna!"
+
+If this person does end up eating ice cream, the second part might feel angry or betrayed. It might come out in thoughts like "Why did I do that?! I'm angry at myself for eating the ice cream when I knew I'd get a headache." By thinking of these as separate <em>parts of self</em>, we can help them make compromises or set boundaries with each other, instead of escalating fighting.
+
+When I was a teenager, I made an internal decision that I wouldn't make any big decisions without consensus from <em>all</em> of my parts of self. All parts of me agreed that they wouldn't take risks without consulting the rest of me first. This helped protect me from a lot of things. Not everyone can get to this ideal right away, but pretty much everyone can find some ways to <em>improve</em> their internal cooperation.
+
+Anyway, this isn't a full post about "parts of self" theory. It's more about…
+
+<h2>Interacting with <em>other</em> people who have internal conflict</h2>
+
+I said I don't want to stop someone from taking a risk for <em>themselves</em>. But of course I would stop someone from risking harm to a <em>different</em> person. And if a person has internal conflict, it's like they're two different people. I wouldn't necessarily let one part harm the other part just because it wanted to.
+
+In my example, the friend probably has a lot of internal conflict. One part of them has a lot of desire, and <em>that part</em> is obviously giving consent. But what about the other parts? At the very least, I should stop my friend and try communicating with them. "If we do this, how will you feel about it tomorrow? In a week? Since you've never wanted to do this before, I assumed you weren't comfortable doing it – was I wrong? Has that changed?"
+
+There are three general ways this can go:
+<ol class="big_list">
+<li>"Oh crap, you're right. I'd feel terrible tomorrow."<br><br>Result: Hooray! I helped my friend communicate with themselves and make the best decision.</li>
+<li>"Actually, I've been wanting this for a while, I just never found the right time to bring it up. Being upset today just made me finally think – what am I waiting for?"<br><br>Result: It worked out! It looks like I actually do have consent from all parts of them. And getting that out in the open may prevent more miscommunications in the future.</li>
+<li>"I don't care! I want this <em>now</em>, why are you hesitating?"<br><br>Result: Uh oh. The currently-active part of my friend is refusing to give the other parts a voice, probably because it knows they won't be okay with it. I have to stand my ground. "Look, I'm your friend and I want you to be happy, but I'm <em>also</em> the friend of the 'you' of tomorrow and the 'you' of next week. I'm not going to do something if I think it will make them feel bad."
+</ol>
+
+<h2>If the conflict continues</h2>
+
+"But," you say, "what if I'm a part of self who wants to have sex, but I share my body with another part that <em>never</em> wants to have sex? Are you saying that nobody should ever have sex with me because the other part doesn't consent?"
+
+Well, that's a more difficult problem.
+
+Consent is a scale, where you should try to get <em>as much consent as possible</em> in order to <em>minimize</em> the risk of hurting someone. Some things are definitely abuse, but even if you have enough consent that it's not abuse, that doesn't necessarily mean everything's fine. That's why you shouldn't accept consent from drunk people, even though some people are pretty alert when they're drunk. Maybe you can get tolerable consent from that drunk person, but it wouldn't be too hard for you to wait until they're not drunk, and get <em>better</em> consent.
+
+With someone who has long-term internal conflict, there might not be such an easy way to get better consent. So unlike with the drunk person, I can't say "Aha, you shouldn't do that, because this other way is so much better!" It's a difficult problem. But I <em>do</em> know that it's worth a lot of work to try to solve this problem.
+
+Do the communication work. Try to help the person develop a better relationship with themselves, so they can find arrangements that all parts of themselves can accept.
+
+
+''',
+},
+
 ]
 
 """
