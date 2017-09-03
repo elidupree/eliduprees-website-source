4f9f14dd47c3e760bc1f5d9f22868d48fa922c84
Eli Dupree <vcs@elidupree.com>
2017-09-03 19:01:00 -0400

    Use the latest versions of the TimeSteward examples

--- update_time_steward_examples.py
+++ update_time_steward_examples.py
@@ -10,5 +10,5 @@ import shutil
 directory = sys.argv[1]
 shutil.copy(os.path.join (directory, "emscripten-examples.js"), "./media/vendor/time-steward-examples/emscripten-examples.js")
 shutil.copy(os.path.join (directory, "emscripten-examples.css"), "./media/vendor/time-steward-examples/emscripten-examples.css")
-shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/bouncy_circles.js"), "./media/vendor/time-steward-examples/bouncy-circles.js")
-shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/simple_diffusion.js"), "./media/vendor/time-steward-examples/simple-diffusion.js")
+shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/bouncy_circles_rowless.js"), "./media/vendor/time-steward-examples/bouncy-circles.js")
+shutil.copy(os.path.join (directory, "target/asmjs-unknown-emscripten/release/examples/simple_diffusion_rowless.js"), "./media/vendor/time-steward-examples/simple-diffusion.js")
