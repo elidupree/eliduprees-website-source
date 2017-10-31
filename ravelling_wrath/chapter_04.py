609c58b21d2d06b9a8cdeca985830c1c6abcafed
Eli Dupree <vcs@elidupree.com>
2017-10-30 23:46:56 -0400

    rav work

--- ravelling_wrath/chapter_04.py
+++ ravelling_wrath/chapter_04.py
@@ -25,7 +25,67 @@ This chapter contains:
   
 <h2>Chapter 4: The Releasing Game</h2>
 
+"The first one is the Releasing Game…" she'd said.
 
+It was gonna be a <em>long</em> night.
+
+I had a giant lump in my stomach. Yali had stopped me from stuffing myself too much, but it wasn't about the food, anyway. I wanted to do what she wanted me to. But I just couldn't.
+
+The explanation had been simple enough. "I'm going to have you do a series of games," she'd said. "Really, they're mental exercises, but it's more fun to call them games."
+
+"The first one is the Releasing Game, where you let your body feel whatever it <em>wants</em> to feel. The game is to let your anger run wild. Then once you're fully angry, we can do the other games, like the Calming Game, where you try to bring yourself down again."
+
+I had let her go on with the explanation. If I had trouble getting mad for the games, Yali'd said, she could help me like she had in the ordeal we've just done. And there were a lot of variations of the games – there was the Controlling Game where you stay angry but just don't <em>do</em> anything, the Expressing Game where you yell about things that don't <em>hurt</em> anyone, and so on. I listened to all of it and acted like I was interested, and I even asked her some questions about it. But I was only dragging it out. The longer the explanation went, the longer I could put off actually <em>doing</em> it.
+
+But finally, I couldn't think of any more questions. The time had come to get started. But…
+
+"I can't," I moaned.
+
+"What's the matter? You know I'm always here to support you if –"
+
+"I don't know. It's stupid."
+
+"Stupid is okay. Go on, say the stupid thing. We can find something smart to say later."
+
+"Okay. Okay," I said. "I… I can't get angry. I'll hurt someone."
+
+"You know this is an exercise, right? It's not –"
+
+"Of course I fuckin' know," I said glumly. "I just… I don't believe it. I mean, I believe it, but I <em>really</em> believe it. I mean –"
+
+"You don't believe it in your heart."
+
+"Yeah. That."
+
+Then Yali had gone quiet. And that's where we were now. I was just slumped on her couch, fidgeting with the tail end of my shirt, feeling like shit.
+
+While I sat, Yali idly started cleaning up our dirty dishes. And every now and then, she gave a long look back at me. She was probably thinking about how to help me with this. <em>She</em> was trying to help <em>me!</em> Why did <em>she</em> have to help <em>me</em> with something I <em>should</em> be able to just <em><strong>do</strong>?!</em>
+
+Gods, she was even doing something <em>productive</em> while I was just sitting there messing around with my shirt. I was being such an asshole. "I can help you with the dishes –"
+
+"Don't worry about it."
+
+Stern take it all.
+
+When she was done, she came back and sat with me. She held my hand. I looked away from her.
+
+"It's okay, it's okay," she murmured. "Maybe I can help your heart believe what it needs to."
+
+?????
+
+"Let me yield when I must yield, even if it costs my pride."
+
+That was from a prayer to the Stern God. "Fuck the Stern," I said automatically.
+
+Yali looked uncomfortable. "Rinn, I know you hate the Stern, but I just can't. It's the Stern Temple that pays for me to live here."
+
+"Since when do the Stern pay for anything that helps anyone?!"
+
+Yali gave me a dirty look. "The point is, ?????. so, so, let's say I had a car, and I wanted to keep the car in my garage forever. I was going to be an expert at keeping my car in the garage. And I didn't even have a driver's license."
+
+"Haha, that sounds pretty stupid."
+
+"Now one day, a thief comes along and steals the car. And we catch the thief, and, and, and everything's in order, except now I have to get the car <em>back</em> into my garage, but I still don't know how to drive.
 
 
 ''',
