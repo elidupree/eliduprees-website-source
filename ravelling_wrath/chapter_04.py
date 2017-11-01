028c639336d6d5803975c1c27e78aa96c52f1787
Eli Dupree <vcs@elidupree.com>
2017-11-01 14:23:23 -0400

    rav work

--- ravelling_wrath/chapter_04.py
+++ ravelling_wrath/chapter_04.py
@@ -71,9 +71,21 @@ When she was done, she came back and sat with me. She held my hand. I looked awa
 
 "It's okay, it's okay," she murmured. "Maybe I can help your heart believe what it needs to."
 
-?????
+"Of <em>course</em> you can," I said bitterly.
 
-"Let me yield when I must yield, even if it costs my pride."
+"If you're not ready, I can –"
+
+"It's not that, it's just, why do you always have to help me with everything? Why can't I be the one helping you sometimes?"
+
+"You <em>will</em> be helping. You're helping protect me from dying, which, which, which, if you don't remember, would <em>always</em> happen if you <em>weren't</em> the Blood Child…"
+
+"It doesn't feel that way."
+
+"We're helping each other."
+
+"You know what, never mind, just do the thing. Make me believe the shit."
+
+"Okay." Yali squeezed my hand. "You're trying to tell yourself that the anger isn't a <em>real</em> danger, so it's okay. But I'd like you to try thinking about it as if it <em>is</em> real, but it's a, a, an inherent part of the process. 'Let me yield when I must yield, even if it costs my pride.'"
 
 That was from a prayer to the Stern God. "Fuck the Stern," I said automatically.
 
@@ -81,11 +93,94 @@ Yali looked uncomfortable. "Rinn, I know you hate the Stern, but I just can't. I
 
 "Since when do the Stern pay for anything that helps anyone?!"
 
-Yali gave me a dirty look. "The point is, ?????. so, so, let's say I had a car, and I wanted to keep the car in my garage forever. I was going to be an expert at keeping my car in the garage. And I didn't even have a driver's license."
+Yali gave me a dirty look. "The point is, it's all part of one process. So, so, let's say I had a car, and I wanted to keep the car in my garage forever. I was going to be an expert at keeping my car in the garage. And I didn't even have a driver's license."
 
 "Haha, that sounds pretty stupid."
 
-"Now one day, a thief comes along and steals the car. And we catch the thief, and, and, and everything's in order, except now I have to get the car <em>back</em> into my garage, but I still don't know how to drive.
+"Now one day, a thief comes along and steals the car. And we catch the thief, and, and, and everything's in order, except now I have to get the car <em>back</em> into my garage, but I still don't know how to drive. So, do you think I should have done anything differently?"
+
+"Well, duh, of course you should've learned to drive."
+
+"But to do that, I'd have had to drive my car <em>out</em> of my garage at least once, and I want to be an expert at <em>keeping</em> my car in my garage, –"
+
+"Well course you should drive your car out of your garage for practice, it's not like it has to stay out of there forev– <em>ohhhhhhhh.</em>"
+
+Yali smiled and waited for me to go on.
+
+"So… it's actually like learning to drive? Not that I've actually learned to drive, it's not worth the effort, in most of the city you can go places faster by running than by driving, like I actually raced my parents once in their car and – but that's not the point, it's like, I'd be taking my anger out for a drive? That actually sounds kinda fun – no, I can't! You could get hurt!"
+
+"It's okay if I get hurt a little. Remember, it's part of the process."
+
+"But…"
+
+"Hmm." Yali stopped and thought. "Well, you don't <em>have</em> to hurt me. You can do other angry things if it makes you more comfortable."
+
+"If it makes <em>me</em> more comfortable?! Are you for serious right now?!"
+
+"I, I, –"
+
+"Oh my gods, you <em>are</em> serious, I – okay. Okay," I tried to sort out my thoughts. "I don't <em>get</em> it, but you're clearly serious, so, I can – I – fine. We can do the thing. Wait, what do we actually do?"
+
+"We start with the Releasing Game. Think of how you felt during the ordeal. Bring that feeling to the forefront of your mind…"
+
+I tried. "I told you, it doesn't work that way, it's not happening <em>right now</em>…"
+
+"That's okay. I expected this. With <em>you</em>, we need a physical trigger. So, go sit in the chair again."
+
+"Ugh, I'm still sore from sitting there already!"
+
+"Perfect! Keep complaining! Get angry about it! But also go do it."
+
+"FINE. I can't believe you're making me do this, you asshole – no, I don't mean you're actually an asshole –"
+
+"It's okay, keep going! Let the anger fill you, lash out however you feel like –"
+
+I sat down in the chair. "Ugh, fuck, you asshole…" Even though she wasn't officially forcing me this time, all those closed-in feelings came rushing back when I looked at the table that way again. Then, to make things worse, Yali pushed the wooden blocks in front of me. "Those FUCKING blocks!" I exploded. I grabbed one of them and pounded it on the table "I can't BELIEVE you made me do this all day, you asshole, you complete, complete slimeball! You –" I swung the block up in the air, then hesitated.
+
+"You can throw it at me if you want." Yali raised her arms to cover her face. "I'm ready."
+
+I hadn't thought I was going to do it, but I couldn't resist. "<em>Fuck! You!</em>" I hurled it straight at her.
+
+"Owww!" she yelled as the block bounced off her arm.
+
+"Oh shit, I didn't mean to –"
+
+"I didn't mean to say 'ow', I thought I was ready –"
+
+"I'm sorry, I promise I won't –"
+
+"Don't worry about that!" Yali leaned forward excitedly. "Now let's do the Calming Game! Return to calm!"
+
+"What?!"
+
+"You want to be able to do that, right?"
+
+"Well – that just wasn't what I was expecting – okay, I get it –" Slowly, I sat up straight and forced my hands to relax. "Okay," I said uncertainly.
+
+Yali took a close look at me. "Are you <em>calm</em>, or are you just acting that way?"
+
+"We-ell…"
+
+"That's okay. You don't have to get it perfect on the first try. Now, Releasing Game again!"
+
+"But –"
+
+"You don't have to throw things at me this time. You can throw them, uh, over there. At the couch."
+
+I grabbed a block. "Fuck you, couch! This is all your fault!" Yali snickered.
+
+And so it went on. <em>Releasing Game. Calming Game. Releasing Game. Controlling Game. Calming Game.</em>.
+
+?????
+
+"The Expressing Game is the hardest," I said one day.
+
+"Oh?"
+
+"I mean, the more I yell about it, the more I want to <em>do</em> something, you know? ?????
+
+"Good to know. I thought letting you yell about it would give you an outlet, but I guess it doesn't work that way."
+
 
 
 ''',
