375da22b0a4b9dbf93b474b9f46b7fa70c5ca12d
Eli Dupree <vcs@elidupree.com>
2018-03-07 02:04:10 -0500

    rav work

--- ravelling_wrath/chapter_04.py
+++ ravelling_wrath/chapter_04.py
@@ -759,11 +759,11 @@ I tried to turn my head, to get another look over at Yali, but my muscles ignore
 
 I made her face appear among the stars. Her eyes were blazing pinpricks of light. Her hand was a constellation reaching out towards me. She was crowned with cold fire that shone from an eternity away.
 
-I fell for centuries, drifting through nothingness, never getting closer to the stars.
+I fell for centuries, drifting through nothingness, never getting closer to the stars. Gears the size of galaxies stood motionless, as if the clockwork of the universe had been left unpowered until it slowly ground down to a halt. And what was I? I was not even a cog in the machine, but a floating speck of dust, that would eventually, a million years later, settle in a corner and be forgotten.
 
-At last, I felt a looming presence, like a giant hand looming over me. Maybe Yali's, or maybe something else's, I had no way to tell. The hand slowly closed over me, shutting out the stars one by one.
+At last, I felt a looming presence, like a giant hand hovering over me. Maybe Yali's, or maybe something else's, I had no way to tell. The hand slowly descended around me, forming a vast and empty enclosure around me, shutting out the stars one by one.
 
-In the emptiness of the hand's embrace, I waited, wondering what would happen next. Until, bit by bit, I somehow became aware that the <em>hand</em> was waiting for <em>me</em>. The void it had created was expectant. I reached out, not knowing how. I had no arms to crawl with, no voice to yell into the darkness, and even my mind was empty of ideas. Yet somehow I still reached out, as if with my life force itself, to scratch a foothold into this fragment of the universe that was being offered to me.
+In the emptiness of the hand's embrace, I waited, wondering what would happen next. Until, bit by bit, I somehow became aware that the <em>hand</em> was waiting for <em>me</em>. The void it had created was expectant. I reached out, not knowing how. I had no arms to crawl with, no voice to yell into the darkness, no mind to make sense of it with ideas. Yet somehow I still reached out, as if with my life force itself, to scratch a foothold into this fragment of the universe that was being offered to me.
 
 As my life force touched the void, these images all winked away – the hand, the stars, and even the void itself. They left me blinking into a different light that was much closer, much more real and mundane.
 
