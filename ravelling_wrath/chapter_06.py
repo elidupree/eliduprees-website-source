d724e10a57f9efc0fc7aef1d060a23e625d80b70
Eli Dupree <vcs@elidupree.com>
2018-04-21 00:34:32 -0400

    rav work

--- ravelling_wrath/chapter_06.py
+++ ravelling_wrath/chapter_06.py
@@ -25,7 +25,25 @@ This chapter contains:
   
 <h2>Chapter 6: The Starry Void</h2>
 
+I quickly sat up and looked around.
 
+There was a sky full of stars in every direction, but this time, they looked like real stars instead of spooky dream stars. The next thing I saw was a desk with a computer screen, for some reason. Other than that, there was just the sky, the floor, and – there she was!
+
+"Yali! You're here!!"
+
+I jumped to my feet, then immediately slipped on the smooth floor. I almost fell on my face, but I caught myself with my arms and sprang back up again. Concentrating, I hurried to Yali's side without slipping again.
+
+"Yali! Can you hear me?"
+
+She was lying face-up on the floor, just like I had been, but she hadn't reacted at all. I dropped to my knees and grabbed her shoulders, my heart pounding.
+
+"Uhhhhhhh," she moaned, staring into space.
+
+It was a lot like when she'd been exhausted after she used the Seeing. Could it be another weird magic thing? "I'm here, it's me, Rinn. Can you –"
+
+Yali lurched upwards and her eyes zeroed in on me. "Your feelings!" she choked out. "Are you having any anger?"
+
+"Thank the gods, you're alright!" I hugged her quickly. "This is great! We're both here and –" my mind caught up with what she'd said "– and I'm not angry at all, in fact, I feel great!"
 ''',
 },
 ]
