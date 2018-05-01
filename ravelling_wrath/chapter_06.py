3c7c4acdb563eec8a066e7057b6db236d28ae1fa
Eli Dupree <vcs@elidupree.com>
2018-05-01 11:36:01 -0400

    rav work from Apr 20

--- ravelling_wrath/chapter_06.py
+++ ravelling_wrath/chapter_06.py
@@ -31,7 +31,7 @@ There was a sky full of stars in every direction, but this time, they looked lik
 
 "Yali! You're here!!"
 
-I jumped to my feet, then immediately slipped on the smooth floor. I almost fell on my face, but I caught myself with my arms and sprang back up again. Concentrating, I hurried to Yali's side without slipping again.
+I jumped to my feet, then immediately slipped. The floor was unbelievably smooth. I almost fell on my face, but I caught myself with my arms and sprang back up again. Concentrating, I hurried to Yali's side without slipping again.
 
 "Yali! Can you hear me?"
 
@@ -43,7 +43,27 @@ It was a lot like when she'd been exhausted after she used the Seeing. Could it 
 
 Yali lurched upwards and her eyes zeroed in on me. "Your feelings!" she choked out. "Are you having any anger?"
 
-"Thank the gods, you're alright!" I hugged her quickly. "This is great! We're both here and –" my mind caught up with what she'd said "– and I'm not angry at all, in fact, I feel great!"
+"Thank the gods, you're alright!" I hugged her quickly. "This is great! We're both here and –" my brain caught up with what she'd actually said "– and I'm not angry at all, in fact, I feel great!"
+
+?????
+
+[????? Took a look around]. Well, first, I pulled off the hoodie Yali gave me and chucked it on the ground. That was much better. Without that loose clothing, I could feel the air around me again. Then I took a proper look at everything.
+
+We were standing [????? but Yali is sitting] on the middle of a smooth, translucent white platform, a circle about 20 meters wide. There was really almost nothing on it. The only features were a desk with a computer, and a fancy telescope mounted to the floor next to it. Other than that, there were no walls, no landscape, nothing – only the starry sky in literally every direction. I couldn't even see anything else beyond the edges of the platform. I carefully knelt down at the edge and looked over… and saw <em>more</em> stars stretching out endlessly below us.
+
+"Woah…" I said. "What happens if we fall off?!"
+
+"You wouldn't fall very far," said Yali. "The gravity goes right back towards the star."
+
+"What star?"
+
+Yali patted the platform under us.
+
+"We're standing on top of a <em>star</em>?!"
+
+"<em>Every</em> star you see up there is a disk just like this one. That's what we're going to be navigating across."
+
+
 ''',
 },
 ]
