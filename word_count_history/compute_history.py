dda4094f6ab0a08576be437a1ce6f9ba26a5ff79
Eli Dupree <vcs@elidupree.com>
2018-09-23 14:30:10 -0400

    fixes

--- word_count_history/compute_history.py
+++ word_count_history/compute_history.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from git import *
+import git
 import os
 import time
 import re
@@ -15,27 +16,32 @@ commit = repo.commit ("master")
 def word_count (string):
   return len(re. findall (r"\w[\w']*", string))
 
+current_date = None
+previous_words = None
 while True:
   commit_date = time.strftime ("%B %-d, %Y", time.gmtime (commit.committed_date))
   
   commit_words = 0
   for chapter_number in range (1, 20):
     try: 
-      chapter = repo.git.show ("{}:ravelling_wrath/chapter_{:02}.py".format (commit.hexsha, chapter_number)))
-      contents = re.search (r"contents.*?'''(.*)"'''", chapter, re.DOTALL).group (1)
+      chapter = repo.git.show ("{}:ravelling_wrath/chapter_{:02}.py".format (commit.hexsha, chapter_number))
+      contents = re.search (r"contents.*?'''(.*)'''", chapter, re.DOTALL).group (1)
       commit_words = commit_words + word_count (contents)
-    except git.exc.GetCommandError:
-      completed = (chapter_number == 0)
+    except git.exc.GitCommandError:
+      completed = (chapter_number == 1)
       break
     
-    
   
-  if completed or commit_date != current_date:
+  if completed or (commit_date != current_date):
     if previous_words is not None:
-      print (current_date + ": " + str (previous_words - commit_words))
+      change = previous_words - commit_words
+      if change != 0:
+        print (current_date + ": " + str (previous_words - commit_words))
     current_date = commit_date
     previous_words = commit_words
     
   if completed:
     break
+    
+  commit = commit.parents[0]
 
