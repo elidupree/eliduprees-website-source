b62c743d4bf0ed4a8ceef7558dd9e81fe6c2680b
Eli Dupree <vcs@elidupree.com>
2018-10-04 21:59:26 -0400

    Created files for the remaining rav chapters, in case I want to write notes or prose in them in advance

---
+++ ravelling_wrath/chapter_14.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import datetime
+from post_contents_utils import *
+from ravelling_wrath.definitions import *
+
+posts = [
+{
+  "title":"Ravelling Wrath, chapter 9",
+  "blurb": blurb,
+  "auto_paragraphs": True,
+  "don't deploy": True,
+  "authors_notes":'''
+
+
+  ''',
+  "head":head,
+  "contents": chapter_start ("Chapter 9: ", '''<p></p>''') +'''
+
+
+[????? in the fight:]
+
+"Am I worthy of my blood?"
+
+"Are your victims worthy of theirs?"
+
+''',
+},
+]
+
