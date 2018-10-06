1fb214f57b4bb3598e696b8c9c4de40da59124c1
Eli Dupree <vcs@elidupree.com>
2018-10-06 17:02:46 -0400

    rav work

--- ravelling_wrath/chapter_08.py
+++ ravelling_wrath/chapter_08.py
@@ -18,6 +18,79 @@ posts = [
   "head":head,
   "contents": chapter_start ("Chapter 8: ", '''<p></p>''') +'''
   
+"So," I said, summarizing the last hour of discussion, "the Alchemist is, like, infinitely far away. They broke their star, and it's not on the pattern anymore. We don't know if they did it on purpose. We don't know if they have any control over where it's going. And <em>we</em> have no way to get off the pattern to follow them, because what they did was probably an Alchemist power. And we have no way to communicate with them. And Yali's power is useless as usual, because apparently the Waiting God still think this is part of the challenge. And even if they <em>did</em> figure out where we are, and <em>could</em> move their star on purpose, they might crash into us really fast, because they don't have the pattern to slow them down at the right moment. It would be even worse than what happened with Morrow, because, uh…"
+
+"Because Morrow was slowed down by air resistance. All of our bodies would be destroyed, hostile intent or no."
+
+"Right, because there's air in space."
+
+"Right."
+
+?????
+
+"Her? I thought you called the Alchemist 'he' earlier. "
+
+"I just say whatever feels right at the time!"
+
+"Woah, so is the Alchemist, like, genderfluid?" I said. "Also, hang on, what's the Alchemist's name?"
+
+"I'm in her mind!! Do people normally think about their own name??"
+
+"<em>I</em> do," I said, smirking. "Every morning I wake up and say, 'Sure is a fine day to be Rinn Akatura!" I was exaggerating, but only a little.
+
+"You should ask for his, or her, name," said Justicar. "So that we know what to call him. Or her."
+
+Unfortunately for Justicar, Morrow was already [????? back to interacting with Alchemist]
+
+?????
+
+
+"So, now that you're here, what's your name? We've just been calling you 'the Alchemist' this whole time."
+
+The Alchemist wobbled back and forth shyly.
+
+"It's okay!! Rinn's a friend!" said Morrow.
+
+The Alchemist hesitated. "Well I, maybe, my name? There are, a few different things people call me. Maybe it wouldn't be so bad? To just be the Alchemist? Do some alchemizing?"
+
+"Alchemy," corrected Justicar. The Alchemist flinched.
+
+"Technically," said Yali, "The Alchemist's ability to make potions <em>isn't</em> alchemy. It has more in common with sorcery."
+
+"That is impossible," said Justicar. "The titles are given to us by the gods. They cannot be incorrect."
+
+"Maybe what <em>we</em> know as 'alchemy' is called that because it's similar to what the <em>Alchemist</em> does, not the other way around. But in any case –"
+
+"Oh my gods, you guys," I said.
+
+"Everybody shut UPPPPP!" yelled Morrow. "Alchemist has something to say!!"
+
+Alchemist smiled nervously.
+
+"Are you, uh, sure about that?" I said to Morrow. "They don't seem to be –"
+
+"I said shut UPPPPP!!"
+
+"Okay, fine."
+
+After a very awkward silence, Alchemist managed to psych themself up into saying something. "I – me? I can make potions?"
+
+"It is one of the abilities available to the Alchemist," said Yali. "Once you learn how to do it, you should be able to store parts of the Broken God's power in a physical medium,
+
+?????
+
+"Now that were altogether – all five of us – we can…" I wasn't quite sure where I was going with this, but there is something that felt almost nostalgic about the five of us all being together and friendly with each other – or at least tolerating each other, I guess. There was an irritating doubt in the back of my mind, but for now, I just wanted to have fun. "Let's have a campfire! And then we can tell stories around the campfire!"
+
+"We could also tell stories without setting anything on fire," said Justicar.
+
+"Yeah, but then we wouldn't get to set things on fire." I dropped a pile of logs out of thin air onto the middle of the star. Then I took a big, exaggerated breath inwards, and breathed fire all over the logs. Alchemist startled. "Haha, didn't mean to spook ya!" I said. The logs weren't properly on fire yet, so I gave them another few fiery breaths until they caught.
+
+"Now everyone sit around the fire!" I said. They all did, to varying degrees of reluctance. Yali and I sat on a camping mat, with Yali sleepily putting in one arm around my shoulders. On the other side, Morrow and Alchemist were in a similar position. Alchemist initially seemed nervous about the fire, but once they relaxed and started warming themself, their face melted into a dreamy smile. It was really heartwarming.
+
+Justicar insisted on staying in a stiff chair a slight distance away.
+
+
+    
 [????? the missing star]
 
 ?????
