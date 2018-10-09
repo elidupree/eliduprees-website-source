e5095cb4fd2d415f90c42d65b7d73cc10a1e3b67
Eli Dupree <vcs@elidupree.com>
2018-10-08 21:55:26 -0400

    rav work

--- ravelling_wrath/chapter_14.py
+++ ravelling_wrath/chapter_14.py
@@ -27,6 +27,19 @@ posts = [
 
 
 
+"You're actually the daughter of Arinyo Seti? I mean, I knew you had the same last name, but it's a common name, I didn't think…" I babbled.
+
+"It's easier if people don't know that about me."
+
+The thoughts slowly clicked into place in my brain. "Holy shit, is she the one who… Wait, shit, I didn't mean to bring that up, I'm sorry –"
+
+Yali looked at me incredulously. Then she laughed her harsh laugh again. "I <em>did</em> ask you not to bring that up, didn't I. But I see no reason not to tell you now."
+
+<em>I see no reason not to tell you now.</em> Her words were infinitely cold. I could already sense that telling me would not be a kindness.
+
+
+
+
 [????? Broken Gate at the end of this chapter, not the beginning of the next. Like scratching the entire interior of your body, can't tell if it's comfort or pain]
 
 ''',
