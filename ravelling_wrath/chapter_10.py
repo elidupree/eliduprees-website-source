6d7189c5a4906e323cdb27bde9ea016d7f8c3b62
Eli Dupree <vcs@elidupree.com>
2018-10-14 20:37:39 -0400

    rav work

--- ravelling_wrath/chapter_10.py
+++ ravelling_wrath/chapter_10.py
@@ -42,11 +42,57 @@ Up ahead of us was a big room where the floor was divided into a square grid, wi
 "The question is, how will we get past the dead end directly in front of us?"
 
 Sure enough,
+
+?????
+
+"I was not so different from you once. I [????? thought the world belonged to me? Multiple examples]."
+
+
+?????
+
+"parties with underage drinking"
+
+"Sure, I've been to some of those. Only we didn't call them 'parties with underage drinking', we called them 'parties'."
+?????
+
+"I had sex with many different men."
+
+"Haha, that's where you went wrong. Shoulda had sex with girls instead."
+
+Justicar looked a little frustrated.
+
+"You got a problem with that? With me liking girls?"
+
+Justicar sighed. "I won't rise to your provocation."
+
+"So you <em>do</em> have a problem with it!" I said triumphantly.
+
+"I am not homophobic!" said Justicar angrily. She struggled to find the right words. "It didn't matter <em>who</em> I was having sex with! I –"
+
+"Also, you say you 'had sex' with these men when you were [????? very drunk]. Isn't that… rape?"
+
+"I put myself in that situation willingly."
+
+"What, like you got [????? very drunk] specifically to have sex with some dude?"
+
+"It doesn't matter if –"
+
+"Then I don't see how it was consensual," I ?????.
+
+Justicar glared at me. "Are you so eager to avoid taking responsibility for your actions that you must see victimhood everywhere, even when I tell you myself that I was not a victim?"
+
+"What the fuck! Of course I take responsibility for my actions! When I hurt someone else, that's my responsibility. When someone else hurts me, that's their responsibility. It's not that complicated!"
+
+
+
+[????? Descriptions like "I devalued my body", perhaps unintentionally explicit about avoiding something.] I had no self-respect."
+
+Up to this point, I had mostly just been messing with her. But [????? this was serious. I got all charged up.] "That's your <em>real</em> problem," I said coldly. "But I've got loads of self-respect.
  
 ?????
 
 
-"The lower gods act without thinking. The upper gods tarry in thought without taking meaningful action. Only the Stern, which stands in the middle, has the [????? balance necessary to run a society]. That is why the Stern Temple controls three fifths of the city, and why the Stern God ?????, despite what it lacks in raw power."
+"The lower gods act without thinking. The upper gods tarry in thought without taking meaningful action. Only the Stern, which stands in the middle, has the [????? balance necessary to run a society]. That is why the Stern Temple officially controls three fifths of the city, and why the Stern God ?????, despite what it lacks in raw power."
 
 "Lacks in raw power? I thought all the gods were equally powerful."
 
