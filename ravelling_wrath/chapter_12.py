0d7b238f279e33e2aa2faa106f9b4aa78cb57c8c
Eli Dupree <vcs@elidupree.com>
2018-10-27 09:18:34 -0400

    rav work

--- ravelling_wrath/chapter_12.py
+++ ravelling_wrath/chapter_12.py
@@ -17,6 +17,17 @@ posts = [
   ''',
   "head":head,
   "contents": chapter_start ("Chapter 12: ", '''<p></p>''') +'''
+  
+  
+It slammed into place. The pattern. The blazing, uncompromising pattern of the Stern.
+
+I was crushed between iron walls. Manacles locked themselves around my bones and jerked me into place, leaving me immobile. Calipers poked at me from every side, measuring me in ways I didn't know I could be measured. Bars slid and rotated into position, squishing me into a different shape like I was a sack of putty being forced into a square hole.
+
+I struggled to stay in my original shape.
+
+<em>Wrong</em>, came the overwhelming force of the god. Every part of me that didn't fit the pattern was <em>wrong</em>. If it couldn't be squeezed into place, I should rip it off myself.
+
+I twisted and fought, trying to get out. But when I pulled my arm against the manacles, it burned with pain and ?????. <em>This arm is betraying me. It is not a true part of me. I should rip it off.</em>
 
 
 ''',
