5fb897931c7db9373849d8f0be89d66af4b49643
Eli Dupree <vcs@elidupree.com>
2018-10-27 00:22:56 -0400

    rav work

--- ravelling_wrath/chapter_19.py
+++ ravelling_wrath/chapter_19.py
@@ -28,11 +28,15 @@ It was –
 
 If I said it was over, I would sit down, and I wouldn't get up again for a long time. But there were many things I still needed to do.
 
-First, I would have to look at Rinn. There would still be injuries on her body. But logically, I knew that she was not in pain, and the healing potion I had given her had seen to that. So there was no need for me to feel anxious about what I saw.
+First, I would have to look at Rinn. There would still be injuries on her body. But logically, I knew that she was not in pain, and that the injuries would not be permanent. The healing potion I had given her had seen to that. So there was no need for me to feel anxious about what I saw.
 
 I looked.
 
+[????? seeing Rinn makes Yali angry at Justicar, want to attack/kick her body]
 
+The tradition was clear: anyone who defiles a corpse will be cursed for life. I was not sure if the curse was literal, but this would not be a good time to risk being cursed, not after everything we had just survived. So I looked away.
+
+Then I tried again, looking at her the way a doctor might, seeing only the practical facts and not my tangled feelings about them.
 
 ?????
 
@@ -42,9 +46,11 @@ Could I put her in a different position?
 
 Also, how would I transport her? One way or another, she would have to arrive at the final portal. She was light enough that I could carry her, but if I slipped or jostled her along the way, it could reinjure her.
 
+[????? Once Rinn is relatively stable, Looking at Rinn's face,] If I thought about what I wanted to do with <em>her</em>, that would <em>also</em> make me have to calm myself down, just in a different way. It was easier if I just didn't think about it.
+
 ?????
 
-If I left Justicar's body now, there would be no one left to say the prayer for the dead. According to tradition, anyone who left a body un[?????honored] would be cursed for life. I was not sure if the curse was literal, but this would not be a good time to risk being cursed, not after everything we had just survived. But which prayer should I say? Every god had its own prayer for the dead. I knew all the Waiting prayers by heart, but [????? it seemed offensive] to say a Waiting prayer when it was the Waiting God who had created the situation that caused her death. The Stern prayer was what she would want, but the Stern prayer also said "[????? may your sacrifice be successful]". I would not be able to say that sincerely. And in the eyes of the gods, an insincere prayer was the same as no prayer at all.
+If I left Justicar's body now, there would be no one left to say the prayer for the dead. [???? this was moved up, replace?] According to tradition, anyone who left a body un[?????honored] would be cursed for life. I was not sure if the curse was literal, but this would not be a good time to risk being cursed, not after everything we had just survived. But which prayer should I say? Every god had its own prayer for the dead. I knew all the Waiting prayers by heart, but [????? it seemed offensive] to say a Waiting prayer when it was the Waiting God who had created the situation that caused her death. The Stern prayer was what she would want, but the Stern prayer also said "[????? may your sacrifice be successful]". I would not be able to say that sincerely. And in the eyes of the gods, an insincere prayer was the same as no prayer at all.
 
 Could I say a Blood prayer?
 
