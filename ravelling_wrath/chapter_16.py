27ed47053401cf72fec2f3aadf5f883d62669dba
Eli Dupree <vcs@elidupree.com>
2018-10-31 10:04:19 -0400

    rav work

--- ravelling_wrath/chapter_16.py
+++ ravelling_wrath/chapter_16.py
@@ -17,6 +17,63 @@ posts = [
   ''',
   "head":head,
   "contents": chapter_start ("Chapter 16: ", '''<p></p>''') +'''
+?????
+
+I watched Alchemist walk away, waiting until they were fully out of sight. No matter how serious things where, I really didn't want to scare Alchemist more than I already had.
+
+Then I turned back to Yali. "So –"
+
+Yali caught my wrist and pulled me down on the mat with her. Before I could react, she kissed me sloppily on the lips.
+
+She did it clumsily and kinda smashed my lip against my teeth, but I didn't care. It had been so long, so <em>long</em>, since anyone had touched me like this. I pressed back into her, my lips and tongue drinking up the warm wetness of her own, my ?????. For the moment, I forgot everything. I could stay like this forever, drowning in Yali.
+
+[????? We flopped down so we were lying down.] Yali never stopped crying, but she kissed me again and again. [????? I drifted in bliss]
+
+?????
+
+[????? Noticed something was different, tiny doubt that something was very wrong, not like the Yali I knew]
+
+Reluctantly, I pulled my lips away from her. "Yali, what –"
+
+That only made Yali cry harder. Before I could finish talking, she pressed me down on the mat and closed her lips over mine again.
+
+But now that I had started, I wasn't going to stop. I wrestled her head away from me so that I could talk. "Yali, I don't understand! I mean, don't get me wrong, this is fuckin' hot, but, I thought, you always said –"
+
+Yali struggled and tried to kiss me again, but I kept holding on. Once it sank in that I wasn't going to let her go without a response, she stiffened. Her face scrunched up again, sobbing uncontrollably. Her mouth moved awkwardly, trying to form words. "I – I – I don't want to die!" she blubbered. "I don't want to die before I ever have sex consensually!"
+
+"I don't understand, you always said we're not going to die, what –"
+
+"Justicar is going to kill us!" she blurted loudly, the words crashing at my ears.
+
+"Did you See that?!!"
+
+"No, but –"
+
+I gripped her shoulders. "Then it's NOT going to happen! [????? We're going to make a plan]"
+
+"But my plans, they… I thought I knew what I was doing! But everything I planned… I almost got you killed! And then Morrow – it's all my fault!!"
+
+"Come on, <em>Justicar</em> tried to kill me. You <em>saved</em> me. Just because you didn't foresee it by magic –"
+
+"It's not <em>just</em> that I didn't <em>foresee</em> it," Yali sobbed. "I was the one who told Justicar what happened. I didn't think –"
+
+"Look, it's not your fault, there's no way you could have know AFTER EVERYTHING WE'VE BEEN THROUGH, YOU SOLD ME OUT TO THE STERN <strong>AGAIN?!?!!</strong>"
+
+For a moment, I was stunned. [????? A whole second voice had ripped out of me. This was the <em>truth</em> roaring up through my lungs. I was finally fed up with saying all this shit to put a nice coating over the truth that should have been glaring in all our faces. No, no, that wasn't me!] I pushed myself away from Yali in terror. "No no no, not now, I can't let the Blood God take over –"
+
+Yali's eyes were wide. "When… when did I sell you out to the Stern the <em>first</em> time?"
+
+"Shut up shut up SHUT UP!!" The Blood God was roaring inside of me, ?????
+
+
+
+?????
+
+"[????? the god speaking through me?]"
+
+"Not exactly… The gods can't form human words on their own. It was more like… you were <em>interpreting</em> for the god."
+
+?????
 
 [????? not sure if this chapter or the next – in any case, towards the end of the big reveal
 
@@ -54,9 +111,7 @@ posts = [
 
 "Your what?"
 
-"Your awakening of blood. It's… an important concept in Blood law. Let me see if my memories know enough to explain it. It's like, like, like, the time when you've fully realized how much of a violation it was. So, so, if someone was abused, but they're still telling themself they can pretend it didn't happen, their blood hasn't awakened to the crime yet. So the timer hasn't started for their revenge.
-
-''',
+"Your awakening of blood. It's… an important concept in Blood law. Let me see if my memories know enough to explain it. It's like, like, like, the time when you've fully realized how much of a violation it was. So, so, if someone was abused, but they're still telling themself they can pretend it didn't happen, their blood hasn't awakened to the crime yet. So the timer hasn't started for their revenge. ''',
 },
 ]
 
