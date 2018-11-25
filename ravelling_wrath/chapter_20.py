614f3ffd96ebd818fba9f170673a82a202a1a5c1
Eli Dupree <vcs@elidupree.com>
2018-11-24 20:25:42 -0500

    rav work

--- ravelling_wrath/chapter_20.py
+++ ravelling_wrath/chapter_20.py
@@ -34,7 +34,113 @@ The floorboards creaked as I heard the thump-thump-thump of a heavy shape rushin
 
 I tried to sit up again. A hand pushed me down. "Shhhhh. Shhhhh. It'll be okay," said a soothing voice.
 
-''',
+"Yaaaaaallllllleeeeeeeehhhhhhhh," I moaned with relief.
+
+"Shhhhh. The potion I gave you is still in effect. ????? "[????? She went on in the same vein for a while]
+
+When she paused for breath, I first out the words, "Yeeeeaaaaaahhhhhhhh, but, I need to <em>peeeeee</em>." 
+
+?????
+
+Yali knelt down next to me and pulled my limp arm over her shoulder. Once I locked my arms in place, she slowly stood, carrying most of my weight. I unsteadily stumbled my feet over each other, bouncing randomly against Yali's body as we walked towards the bathroom.
+
+"Where ahhhh we?" I said. It was, like, a regular indoors room, but not one I'd seen before.
+
+"I took us to a hotel. I told them you passed out drunk at ????? and needed a place to stay."
+
+I almost laughed, but it hurt and I sort of choked instead.
+
+"Wait a minute… wait a minute!!! We're back from the Otherworld?? Oh duh, if I needed to pee then – wait, how –"
+
+"I'll explain everything in, in a minute," said Yali, in between trying to keep us both balanced on our feet.
+
+Together, we stumbled into the bathroom. Standing in front of the toilet, Yali shifted awkwardly. "I can, can, give you your privacy, if, if you think you've got, got, –"
+
+"I mean, you've seen me naked before –"
+
+"Not consensually!" said Yali quickly.
+
+"Oh gods, I'm sorry about that, I totally wasn't thinking about –"
+
+"What?! I meant not consensually for <em>you</em>! You weren't, weren't, weren't in a sound state of mind when I put the Eye on you! And now wouldn't be consensual either, you're clearly under duress because –"
+
+"Whatevaahhh," I said as I dropped my pants. I wriggled out of her arms and collapsed onto the toilet seat. She tried to look away, but immediately had to support me to keep me from falling over.
+
+
+
+?????
+
+"I'm very grateful that you didn't, didn't let me… In the Broken God's world, when I was…"
+
+I was confused for a moment, then realized that she must be referring to when she had sort of come on to me sexually. "Of course!" I said, embarrassed. "I mean, there was obviously something wrong! If I'd have fucked you in a situation like that, that would make me an asshole!" I didn't mention how close I had felt to just… letting things happen.
+
+"Now, when we eventually do have sex, it can be something beautiful, instead of, instead of, desperate."
+
+"Eventually…" I echoed, a little disappointed.
+
+Her face darkened. "I know what you're thinking. And there's about 40% of me that wants to just throw it all to the Seeking and, and, do it, right now. But…" She swallowed. "I always knew my trauma wasn't going to go away overnight. And now I have even more trauma than when we started." ?????
+
+?????
+
+She smiled ruefully. "You've only <em>known</em> me while I had the Seeing, and the memories. Now that those have been taken away… I only have my own memories to live with again. I'm okay <em>now</em>, because I'm filling up my mind was taking care of you… but pretty soon… it's going to come back. I might need… a lot of, of, emotional support, from you, soon… if that's okay…"
+
+"Of course," I breathed.
+
+"Although I'll understand if you have… other duties…"
+
+"Duties? What duties?"
+
+"You know, if you become the new High Priest of Blood."
+
+"WHAAAAAAAAAAAAAAAAAAAAAAT?????!!!!!!" [????? physical description of rinn's reaction] There were so many things that were impossible about that, I didn't even know where to start. "But I thought – I thought former Ravellers couldn't even <em>be</em> priests!"
+
+Yali smirked. "And <em>I</em> thought Rinn Akatura didn't care about the rules."
+
+?????
+
+<bigbreak>
+
+It wasn't a long time before I got to see [????? what Yali's trauma looked like] for myself.
+
+One morning I woke up early and noticed that she wasn't in bed. She wasn't at her computer, either. I found her sitting on the kitchen floor, leaning her head into the wall and crying.
+
+"Hey, anything I can help with?" I said, trying to sound gentle. She didn't react.
+
+I knelt down next to her and reached out to her. I picked up her hands in mine and gently squeezed. She weakly squeezed back.
+
+"I'm here," I said. "Let's [????? get you somewhere more comfortable?]
+
+[????? Yali is still unresponsive for a while, Rinn stays there to be supportive. Finally Yali chokes out:]
+
+"She's dead because of me."
+
+"Who in the – wait, you can't mean <em>Justicar??</em> Screw her!"
+
+My words didn't seem to get through. Yali kept muttering into the wall. "If only I just hadn't made you stab me… If only I just hadn't told her…"
+
+"Come on, it was literally 100% her own fault she got killed! You literally gave her a chance to back out when she was in the middle of trying to kill us! Not to mention you <em>literally saved my life, so –</em>"
+
+"Rinn," said Yali heavily.
+
+"Yeah?"
+
+She turned her head halfway towards me. "Can you honestly tell me that she'd still be dead right now if I hadn't told her what happened?"
+
+"You can't think like that! ?????!"
+
+"I'm not… disagreeing with you." Yali took a ragged breath. "But… think about it… can you honestly tell me she'd still be dead right now if I hadn't told her what happened?"
+
+"Stern take it…" I thought about it. "Yeah! I can!" I said aggressively.
+
+"You… can?"
+
+"Yeah, YOU think about it! So let's pretend I don't stab you and we all get to the Broken God's world just fine. What happens then? <em>Morrow</em>! It's not like he went off the rails because I was [????? upset], he was going to lose control no matter what! And there's no way Justicar wouldn't have heard about it, and then she'd be trying to kill <em>Morrow</em> until one of <em>them</em> was dead. He might have even attacked her first! So unless you'd be happier if Morrow was dead than Justicar, you can just shut right up about blaming yourself, it's hot bullshit!"
+
+Yali stared at me blankly.
+
+"
+
+ ''',
 },
 ]
 
