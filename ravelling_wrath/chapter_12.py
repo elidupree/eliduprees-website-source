9c02dded2f08a3484e17328b73e63aef969f1378
Eli Dupree <vcs@elidupree.com>
2018-12-12 23:11:44 -0500

    rav work

--- ravelling_wrath/chapter_12.py
+++ ravelling_wrath/chapter_12.py
@@ -31,29 +31,57 @@ I twisted and fought, trying to get out. But when I pulled my arm against the ma
 
 ?????
 
+My body crumpled onto the floor.
 
+Everything felt heavy. I had been dumped on a hard stone floor, surrounded by square walls of white marble. Above me, a dim orange glow filtered through a sky dark with clouds. The floor was painful under me, but I couldn't bring myself to get up.
 
-
-[????? I was going to have to go look for Yali. Again. Frustration at the jobs, but feeling defeated, it probably won't work anyway?]
+What was even the point? If I got up, I was just going to have to go look for Yali. Again. How many weeks had I spent grinding through the Seeking God's puzzles, for only minutes of her? And now I was in the world of the Stern. It would probably make me break my back and never give me what I wanted at all. Why did the gods have to put me through this? I felt like a defeated, burned-out shell. I was never going to be with Yali again.
 
 And even if I found her, what good would it do? She would just be taken away from me again. And again, and again, until the day I died. And then they would all walk over me.
 
-That didn't make sense. I tried to shake my head clear. Was it the Blood God? Being mad that I hadn't killed Yali, I guess? That didn't quite fit either.
+That didn't make sense. I tried to shake my head clear. Was it the Blood God? Being mad that I hadn't killed Yali, I guess? That didn't quite fit either. Just in case, I tried to listen for the Blood God's will, the way I had learned in the last layer. But there was nothing. Emptiness. A hole inside of me.
 
-
-?????
+I ought to get up, I thought. My back was killing me. I <em>really</em> ought to get up. But somehow, it just didn't happen. I couldn't summon up the motivation to move.
 
 What had happened to me?
 
 I have never felt like this before. Up to this point, the Ravelling had been… certainly an <em>interesting</em> experience… but I had basically felt the way I would normally feel about all of it. But this… I had never felt like this before in my life. Listlessly, I tried lifting up a hand and making a fist. It didn't even feel like my hand. The fist didn't feel kickass. It was just… <em>a</em> hand that happened to be connected to my arm.
 
-I dropped my arm down to the floor again. If I didn't get to feel anything, what was the point of even getting up?
+I dropped my arm down to the floor again. If I didn't get to feel anything, what was the point of even getting up? Even the setting of my heart only felt like a distant echo.
 
-[????? The Stern God's will appears]
+I don't know how long I lay there. The moments stretched out into a trickle of agonizing time.
 
-I yanked myself to my feet like a puppet.
+And then… it appeared.
+
+It was a burning haze pressing in on every side of my mind. An iron pillar jamming into my soul, trying to force me up. A spear of anxiety and guilt, a driving need to do what I must. <em>I MUST get up. I MUST get up. I MUST.</em>
+
+I didn't have to guess what this was. It was the Stern God.
+
+Why was the Stern God allowed in my head? I didn't have the energy to question it. I had to get up. I ripped my tiredness aside. I yanked myself to my feet like a puppet. My muscles burned with the pain of being used before they were ready. They still weren't ready. I forced them to do the next thing anyway.
+
+<em>I must [????? prepare in an obedient way],</em> I thought. I stumbled to the center of the room. In front of me, there was a pedestal with a uniform neatly folded on top of it. I was to put on the uniform before continuing. Across from me, two grim-faced, white marble statues blocked the doorway with their spears, waiting for me to obey.
+
+?????
+
+The uniform was made of ill fitting, stiff cloth that chafed at my skin.
 
 [????? The uniform; make sure to clarify what happens to Rinn's phone]
+
+?????
+
+Normally, I would have fought against it, refused to [????? show obedience (bow down?)]. But now, I didn't even care anymore. <em>Joke's on you, Stern God, I don't give a shit.</em>
+
+?????
+
+[????? Setting out on the corridor]
+
+Despite the rigid walls everywhere, there was no ceiling. Every room and corridor was open to the sky. I gazed up at the walls resentfully. Why couldn't I just climb up one of the walls and then take a shortcut across the top? But that was forbidden. I had to stay on my own path. I lowered my head again and kept walking. Wait, what the fuck? I was Rinn Akatura. Since when did I ever not do something because it was forbidden? Struggling to hold on to my own thoughts, I manifested a ladder and set it against the wall.
+
+I took one step up the ladder, then another. Why was I climbing this way? It wouldn't work anyway. No, it was trying to make me forget! Stubbornly, I held on and kept climbing. Each step was harder than the last. Gravity weighed me down. I felt like my arms and legs were made of lead. With an earth-shattering effort, I forced my head almost up to the top of the wall. Just one more step, and I'd be able to see over the edge.
+
+Terrible gravity slammed me down, breaking my ladder to splinters and crushing my body against the marble floor. I was face down, my jaw twisted to the side and my bones grinding against the rock, defying all my efforts to lift them. And moments after the pain began, my stomach churned with the horrible realization of how badly I had failed. A wave of guilt and anxiety threatened to drown me. I had done wrong, wrong, WRONG. And now I was being punished. I could not hide from the Stern.
+
+Slowly, the gravity let me rise again. I dragged my leaden body to its feet, badly shaken. Could the Stern God just <em>do</em> that to me? When I thought of trying to climb the wall again, I shuddered and shied away.
 
 ?????
 
@@ -62,6 +90,10 @@ There was that word again – the Blood God's word, if you could call it a word,
 "Lask… ueh," I said out loud. It didn't sound anything like it sounded in my head, but it would have to do. Was it the word for a Raveller? No… the Farseer was <em>Waiting God laskueh</em>, but when I tried thinking of myself as <em>Blood God laskueh</em>, it made the Blood God angry. Justicar was <em>Stern God laskueh</em>, but Morrow wasn't <em>Seeking God laskueh</em> and Alchemist wasn't <em>Broken God laskueh</em> – they just weren't.
 
 Thinking about this was a waste of time.
+
+?????
+
+I stared at the statues blocking the Alchemist's Path. I could feel the Stern God watching me with contempt. It had made sure I knew what was wrong to take a different Raveller's path. But it was going to <em>let</em> me do this, and let the mark of my wrongdoing hang over me.
 
 ?????
 
