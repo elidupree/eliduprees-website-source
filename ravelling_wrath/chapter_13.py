937daaa30b8804b9c83c0837f0d7a6c0a67794fc
Eli Dupree <vcs@elidupree.com>
2018-12-15 22:43:04 -0500

    rav work

--- ravelling_wrath/chapter_13.py
+++ ravelling_wrath/chapter_13.py
@@ -17,10 +17,45 @@ posts = [
   ''',
   "head":head,
   "contents": chapter_start ("Chapter 13: ", '''<p></p>''') +'''
+  
+Time moved slowly around me, agonizingly slowly. I didn't understand what was happening. In my vision, in front of me, there was a picture of my blade sticking out of the side of her body's ribcage. That didn't make any sense. It wasn't real.
 
+Then her scream came crashing through my mind. Suddenly, my blood was pounding through every vessel, so hard, painfully hard. Why? My body had decided to tense up and flood itself with terror. Why? I didn't understand.
 
+My muscles pulled in all directions at once. I crashed to the floor.
 
-''',
+It was an old, familiar feeling, my body tumbling down, getting bruises on my arms and smacking my jaw into the ground. It wrenched out a memory from deep inside me. <EM>I AM RINN AKATURA!!!</em> I screamed inside, trying to speak it with my mouth but barely managing a huff of air.
+
+?????
+
+If I saw where she was, the other me would lunge at her again. I crawled wildly in the other direction, clawing at the floor. I had to stay away, stay away from her, no matter what.
+
+Her heavy panting filtered through to my senses. "All this planning, and I forgot how much pain <em>hurts!</em>" she gasped. It was at an angle behind me. That gave me her location! I could – no! I shook my head wildly, trying to forget where she was.
+
+?????
+
+Her heavy hands gripped my shoulders. I tried to thrash away. Her voice echoed in my years. "Rinn, are you there?"
+
+"I AM!!" I screamed. "I CAN'T HOLD ON!!!"
+
+"You can," she said decisively. "Just keep holding on. You are real. This body is yours."
+
+The sound of her voice helped me focus on what was real. I
+?????
+
+Looking at her was the most terrifying thing I'd ever done. I wanted 100 things of the same time. I wanted to run away. I wanted to hug her. I wanted to get down on the floor and beg for forgiveness and sob into her legs. I reached out to do it all at the same time. But I was frozen in terror.
+
+"I apologize for putting you through this," she said earnestly. "But it was the only way to wake up the part of you that was still Rinn."
+
+My voice came out shakily. "Are you seriously… apologizing to me? I – I stabbed you. I stabbed you! I stabbed, I <em>stabbed</em> you!! I – no! No no no –"
+
+"It's okay. Look, I'm okay. I took a potion, I'm already healed."
+
+I looked at her chest, uncomprehending. There was supposed to be a wound there, ?????. It was like it had never happened.
+
+"Say my name," she said.
+
+I opened my mouth. I knew her name, of course I knew her name. I had said her name so many times, shouting it across the schoolyard, whispering lovingly while she held me in her arms… I had whispered – "Yali. Oh gods, Yali." All the fear and tension drained out of me. I was back. Mostly. ''',
 },
 ]
 
