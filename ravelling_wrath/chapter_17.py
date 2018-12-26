851ed5ffe0bd7a5f7066158209515778918273e1
Eli Dupree <vcs@elidupree.com>
2018-12-26 14:25:15 -0500

    rav work

--- ravelling_wrath/chapter_17.py
+++ ravelling_wrath/chapter_17.py
@@ -107,7 +107,7 @@ Yali spoke gravely. "The Blood God is a god of anger, and it has been starving f
 
 "It may be necessary," said Yali gravely.
 
-[?????"Well, fuck." I looked down at my hands. wait no, more aggressive: "Fuck!" I yelled. "[I ]] "<em>May</em> be, though? You think we can get away without a fight?"
+[?????"Well, fuck." I looked down at my hands. wait no, more aggressive: "Fuck!" I yelled. "[I fight people a lot but I never wanted to actually kill anyone]!"] "<em>May</em> be, though? You think we can get away without a fight?"
 
 "The final portal won't open until the conflict is resolved. But there are other possibilities. She doesn't have to <em>die</em> if she can be persuaded to take the unravelling potion –"
 
