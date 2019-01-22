ee9bf66e88382f6f7a6e1bcf9216302b919b835c
Eli Dupree <vcs@elidupree.com>
2019-01-21 19:44:13 -0500

    rav work

--- ravelling_wrath/chapter_20.py
+++ ravelling_wrath/chapter_20.py
@@ -24,11 +24,11 @@ It would have been so easy to just go back to sleep again. ?????. But ?????. My 
 
 I stubbornly clung onto consciousness.
 
-I tried to sit up. It didn't work very well. I just sort of twitched like a dying fish. Getting up was a huge pain. I should just go back to sleep. But I really did need to get up. I needed to pee. It was a pretty unfamiliar sensation.
+I tried to sit up. It didn't work very well. I just sort of twitched like a dying fish. Getting up was a huge pain. I should just go back to sleep. But I really did need to get up. I needed to pee. [????? Replace: It was a pretty unfamiliar sensation.
 
 Right, that hadn't happened in a while…
 
-<em>Wait a minute, if I need to pee, does that mean I'm not in the Otherworld anymore?! Where am I?</em> I decided to ask that out loud, in case Mom or Dad or someone else was listening. "Ooeauuhhh uaaahhhh," I said.
+<em>Wait a minute, if I need to pee, does that mean I'm not in the Otherworld anymore?! Where am I?</em>] I decided to ask that out loud, in case Mom or Dad or someone else was listening. "Ooeauuhhh uaaahhhh," I said.
 
 The floorboards creaked as I heard the thump-thump-thump of a heavy shape rushing to my side.
 
@@ -36,17 +36,21 @@ I tried to sit up again. A hand pushed me down. "Shhhhh. Shhhhh. It'll be okay,"
 
 "Yaaaaaallllllleeeeeeeehhhhhhhh," I moaned with relief.
 
-"Shhhhh. The potion I gave you is still in effect. ????? "[????? She went on in the same vein for a while]
+"Shhhhh. The potion I gave you is still in effect. You're probably not in shape to stand yet. Even if the healing is completely finished, the effects on your soul are, are, are, you might not be fully conscious yet, so your control over your body might not be complete. It's better to relax while…" Yali kept explaining the details for a while, but it all turned to fuzz when it went into my ears.
 
-When she paused for breath, I first out the words, "Yeeeeaaaaaahhhhhhhh, but, I need to <em>peeeeee</em>." 
+When she paused for breath, I forced out the words, "Yeeeeaaaaaahhhhhhhh, but, I need to <em>peeeeee</em>."
 
-?????
+"Oh. I – I could –" she looked around. "Well, if… don't try to move anything too quickly, but, how's your shoulders?"
+
+I slowly tried flexing my shoulders in all directions. It was an effort, and one of my <em>arms</em> flopped around out of my control, but the <em>shoulders</em> didn't seem to have any problems. "Guh azzz new," I slurred.
+
+"That's good. That means the healing has finished like it was supposed to. So, so… just keep being careful… let's see what we can do."
 
 Yali knelt down next to me and pulled my limp arm over her shoulder. Once I locked my arms in place, she slowly stood, carrying most of my weight. I unsteadily stumbled my feet over each other, bouncing randomly against Yali's body as we walked towards the bathroom.
 
 "Where ahhhh we?" I said. It was, like, a regular indoors room, but not one I'd seen before.
 
-"I took us to a hotel. I told them you passed out drunk at ????? and needed a place to stay."
+"I took us to a hotel. I told them you passed out drunk at an early Unravelling Week celebration and needed a place to stay."
 
 I almost laughed, but it hurt and I sort of choked instead.
 
@@ -66,6 +70,42 @@ Together, we stumbled into the bathroom. Standing in front of the toilet, Yali s
 
 "Whatevaahhh," I said as I dropped my pants. I wriggled out of her arms and collapsed onto the toilet seat. She tried to look away, but immediately had to support me to keep me from falling over.
 
+"Ahhhhh…" I moaned with relief. "I feel like I haven't peed in weeks!"
+
+"Months, actually."
+
+I hurt myself laughing again. It was a pretty comical situation, me letting go on the toilet while Yali was stuck awkwardly holding onto my shoulders to keep me balanced. "This is gonna be a hilarious story someday…" I said.
+
+Yali smiled despite herself. "How very Waiting of you," she said.
+
+<bigbreak>
+
+[????? Summary language of Yali taking care of Rinn for hours or up to a day, and also explaining what she did with the Waiting God]
+
+[????? Yali serves Rinn some food]
+
+It looked delicious. "Hang on, what do I eat this with?"
+
+"I think it came with a plastic fork…" Yali fiddled with the packaging.
+
+"Whatever," I said, manifesting a fork-shaped Blood Blade and digging in.
+
+"Is that –" said Yali with astonishment.
+
+I held up the fork and we both stared at it.
+
+"Uh…" I said, belatedly realizing what I'd done. "I thought… um… there's no way we're still in the Otherworld, is there?!"
+
+"No…" Yali took the fork and snapped it in half with her fingers. I didn't feel anything through the fork, either. "Manifested matter is weak here, even if you're a god. But, but, you know what this means?!" she said excitedly.
+
+"No, I don't, I'm, like, <em>completely</em> bamboozled."
+
+"It means that when the Blood God rejuvenated, when it took some of your threads – it <em>left to the threads for summoning Blood Blades with you.</em>"
+
+"That's… wow." ?????
+
+
+
 
 
 ?????
@@ -78,23 +118,35 @@ I was confused for a moment, then realized that she must be referring to when sh
 
 "Eventually…" I echoed, a little disappointed.
 
-Her face darkened. "I know what you're thinking. And there's about 40% of me that wants to just throw it all to the Seeking and, and, do it, right now. But…" She swallowed. "I always knew my trauma wasn't going to go away overnight. And now I have even more trauma than when we started." ?????
+Her face darkened. "I know what you're thinking. And there's about 40% of me that wants to just throw it all to the Seeking and, and, do it, right now. But…" She paused. "I always knew my trauma wasn't going to go away overnight. I had a plan for it. And if I don't stick to that plan, if I try to have sex before I've dealt with the trauma, it, it, things will happen, that, aren't what I want. That hasn't changed. In fact… when I think about it, I probably have even <em>more</em> trauma than when we started."
 
-?????
+"Well, shit…"
 
-She smiled ruefully. "You've only <em>known</em> me while I had the Seeing, and the memories. Now that those have been taken away… I only have my own memories to live with again. I'm okay <em>now</em>, because I'm filling up my mind was taking care of you… but pretty soon… it's going to come back. I might need… a lot of, of, emotional support, from you, soon… if that's okay…"
+"I succeeded. We both got through the Ravelling alive. But there was probably a cost."
+
+"Uh, so, I don't mean to, like, doubt you or anything –"
+
+"Go on, say it."
+
+"You keep saying 'probably'… and you seem pretty calm about all this…"
+
+She smiled ruefully. "I'm okay <em>now</em>, because I'm filling up my mind with taking care of you. But pretty soon, you won't need me for everything, and then… other things are going to come back. You've only <em>known</em> me while I had the Seeing, and the memories. Now that those are gone… I only have my own memories to live with again. I might need… a lot of, of, emotional support, from you, soon… if that's okay…"
 
 "Of course," I breathed.
 
 "Although I'll understand if you have… other duties…"
 
-"Duties? What duties?"
+"Duties? How the fuck am I going to have 'duties'??"
 
 "You know, if you become the new High Priest of Blood."
 
-"WHAAAAAAAAAAAAAAAAAAAAAAT?????!!!!!!" [????? physical description of rinn's reaction] There were so many things that were impossible about that, I didn't even know where to start. "But I thought – I thought former Ravellers couldn't even <em>be</em> priests!"
+"WHAAAAAAAAAAAAAAAAAAAAAAT?????!!!!!!" I was completely blown away. I probably would have literally fallen down if I wasn't already sitting. There were so many things that were impossible about that, I didn't even know where to start. "But I thought – I thought former Ravellers couldn't even <em>be</em> priests!"
 
 Yali smirked. "And <em>I</em> thought Rinn Akatura didn't care about the rules."
+
+"And more importantly, how do you even become a – <em>there isn't even a Blood Temple!!"
+
+"That's just the thing, though, isn't it? The Blood God has rejuvenated. And now some of us here in the material world will need to recreate the Blood Temple. And, I'm not asking you to decide on anything right now, but you… you're the one who knows the Blood God inside and out. The only person alive
 
 ?????
 
