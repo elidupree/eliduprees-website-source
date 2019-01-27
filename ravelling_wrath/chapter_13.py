b7502b569d140ccac285801023f3d9984ac63ff8
Eli Dupree <vcs@elidupree.com>
2019-01-26 19:45:02 -0500

    rav work

--- ravelling_wrath/chapter_13.py
+++ ravelling_wrath/chapter_13.py
@@ -24,7 +24,7 @@ Then her scream came crashing through my mind. Suddenly, my blood was pounding t
 
 My muscles pulled in all directions at once. I crashed to the floor.
 
-It was an old, familiar feeling, my body tumbling down, getting bruises on my arms and smacking my jaw into the ground. It wrenched out a memory from deep inside me. <EM>I AM RINN AKATURA!!!</em> I screamed inside, trying to speak it with my mouth but barely managing a huff of air.
+It was an old, familiar feeling, my body tumbling down, getting bruises on my arms and smacking my jaw into the ground. It wrenched out a memory from deep inside me. <em>I AM RINN AKATURA!!!</em> I screamed inside, trying to speak it with my mouth but barely managing a huff of air.
 
 ?????
 
@@ -57,24 +57,27 @@ I looked at her chest, uncomprehending. There was supposed to be a wound there, 
 
 I opened my mouth. I knew her name, of course I knew her name. I had said her name so many times, shouting it across the schoolyard, whispering it lovingly while she held me in her arms… I had whispered – "Yali. Oh gods, Yali." The topmost layer of fear and tension drained out of me. I was back. At least halfway.
 
-I ?????. "But – oh gods, oh gods, oh gods, how – what –" I held out my hand, opening and closing it as if it was a strange thing that wasn't part of me. This hand had done what? Suddenly I was shivering violently, my hands tingling and tears squeezing painfully from my eyes. ????? [????? Yali hugs Rinn]
+[????? Introduce a new medium-term promise by this point?]
+
+I ?????. "But – oh gods, oh gods, oh gods, how – what –" I held out my hand, opening and closing it as if it was a strange thing that wasn't part of me. This hand had done what? Suddenly I was shivering violently, my hands tingling and tears squeezing painfully from my eyes. Yali moved in, wrapping her big arms around me. I wanted to feel like those arms were warm and reassuring. I wanted to feel like they were holding me up when I was falling down. But the cracks inside me were too deep.
 
 And then came the pain and rage. I tried to jump up, to run at the wall and beat against it. But Yali was holding me too tightly. I struggled against her, thrashing back and forth. "LET ME GO!!" I screamed.
 
 "I'm not going to – oof! – to let you go if you're going to – ow! – to hurt yourself!" gasped Yali, struggling to hold on.
 
 "I DON'T CARE! LET ME GO, LET ME GO, –" I managed to jerk away from her and jump up. Blinded by rage, I slammed my fist into the stone wall with all my strength. I pulled my hand back for a second swing, and I was about to do it again when the pain hit. My hand felt like it was splitting apart. Normally, pain would fuel my anger, but now, I was suddenly too exhausted to be angry. I slumped against the wall, clutching my broken hand, and sank down to the floor again, curling up in the corner and sobbing.
-?????
+
+I was dimly aware of Yali and Alchemist sitting down around me, trying to comfort me. But I couldn't get up. I couldn't see past the pain.
 
 It was supposed to <em>end</em>. There was supposed to be a point where I'd be like "okay, I'm done being upset now" and just manifest my hand back together and get on with figuring out what I needed to do next. But it wasn't happening. Even with Yali and Alchemist cuddling me from both sides, it wasn't happening.
 
-"I can't even fix it… I can't concentrate on manifesting, I can't visualize what my hand is supposed to be like…"
+"I can't even fix it… I can't concentrate on manifesting, I can't even picture what my hand is supposed to be like…"
 
 "I could, maybe, make a potion?"
 
 "Do it," said Yali.
 
-Alchemist coached a potion out of the air offered handed it to me. With my good hand, I drank it down. It tasted sweet and filling. As soon as it went down, my hand was filled with warmth, ?????ing and ?????ing it back into his ?????proper shape.
+Alchemist coaxed a potion out of the air offered handed it to me. With my good hand, I drank it down. It tasted sweet and filling. As soon as it went down, my hand was filled with warmth, wiping away the pain and massaging it back into its original shape.
 
 "Gods, you're the best," I mumbled. I kissed Alchemist without thinking. Wait a minute, I had just kissed Alchemist when I hadn't even kissed Yali! Well, I could fix that. I turned to the other side and kissed Yali too. Yali kissed me back tenderly, then guided me towards Alchemist again. They went back and forth, kissing me in turn.
 
@@ -94,6 +97,26 @@ Yali held me down. "I'll be using this Seeing regularly. If anything is going to
 
 ?????
 
+"What if you, used, the…" began Alchemist.
+
+"I'm not going to use that. It's not needed."
+
+"But, look how scared she is!" said Alchemist insistently.
+
+"If she goes straight to the portal, there won't be any danger. There is no need to –"
+
+"No need to what?" I asked.
+
+"It doesn't matter –"
+
+"Yes it does! It's something that could help keep you safe, isn't it?! You have to tell me!"
+
+Yali glared at Alchemist. But, reluctantly, she explained. "I got a new power when I got to this layer. It's called the Watchful Eye, just like the symbol. It's a magic mark I can put on someone. Once I do that, I can see them constantly, no matter how far away they get, like I've got an extra set of eyes where they are."
+
+"Do it," I said. "Stick it on me!"
+
+"No," she said.
+
 "YOU HAVE TO!" I yelled, grabbing her shoulders and shaking her desperately. "YOU HAVE TO, YOU HAVE TO, I DON'T WANT YOU TO DIE!!"
 
 She caught my wrists and held me away from her. "It's not right! Watching you every minute of every day –"
@@ -106,9 +129,9 @@ She caught my wrists and held me away from her. "It's not right! Watching you ev
 
 "You're in no state to consent to this! It's not consent if you're, if you're –"
 
-"LIKE ANY OF THIS WAS CONSENSUAL!! ?????! PLEASE! PLEASE! PLEASE! I'M BEGGING YOU!" I sobbed, straining against Yali's arms to bring my face closer to hers.
+"LIKE ANY OF THIS WAS CONSENSUAL!! I DIDN'T CONSENT TO HAVE THE BLOOD GOD IN MY HEAD! I DIDN'T CONSENT TO STABBING YOU! PLEASE! PLEASE! PLEASE! I'M BEGGING YOU!" I sobbed, straining against Yali's arms to bring my face closer to hers.
 
-Yali went silent again. For a long moment, everything was quiet except for the sound of my struggling. Tears poured down my face. It was hopeless. Yali's face loomed cold and impenetrable in front of me. She was going to deny me again. There was nothing I could do to change it. And then it would happen again. And it would be all my fault, all my fault…
+Yali went silent again. For a long moment, everything was quiet except for the sound of my struggling. Tears poured down my face. It was hopeless. Yali's face loomed cold and impenetrable in front of me. She was going to deny me again. There was nothing I could do to change it. And then it would happen again. And it would be all my fault, all my fault… [????? Ordering of the sentences in this paragraph?]
 
 "I'm begging you," I whimpered again.
 
@@ -132,9 +155,21 @@ Yali hesitantly pressed her fingers over my heart.
 
 It didn't feel anything like when she'd touched my heart on that first day. It was just an object touching another object. A cold and hot feeling briefly sank into my chest, then it passed. I looked down. The Watchful Eye was on me, a black symbol of an eye with an hourglass inside, drawn right there on my chest.
 
-"Did it work? What's it like?"
+"Did it work?" I said. "What's it like?"
 
 "It's like I can see you from all sides at once," said Yali.
+
+?????
+
+[????? Rinn trying to do the paths the way she did before, through dissociation – the Rinn part of her recoils against that and they reach an awkward "compromise that no part of me was really okay with"]
+
+?????
+
+How had this all gone so horribly wrong? I had only done what I felt like I was supposed to! Wasn't it RIGHT to destroy the Farseer? I'd thought it was the one thing I knew for sure! And I had finally tried to do it! I should have been happy. Why did I only feel all this guilt and fear?
+
+?????
+
+
 
 [????? Walking along the Blood Child's Path: the statues ask Rinn to say moderately negative things about herself, and Rinn tries to say very negative things instead, but they reject it]
 
