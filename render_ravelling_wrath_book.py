4c3bd03bdeeaa0bf0eb7ab0ee3d2faf100604b94
Eli Dupree <vcs@elidupree.com>
2019-11-02 23:19:25 -0400

    started making script to convert Ravelling Wrath to PDF for printing

---
+++ render_ravelling_wrath_book.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import os.path
+import re
+import subprocess
+import datetime
+import traceback
+
+from weasyprint import HTML, CSS
+
+import blog_server_shared
+import utils
+
+import ravelling_wrath.main
+
+build_path ="./build/ravelling_wrath_book"
+html_path = os.path.join (build_path, "ravelling_wrath.html")
+pdf_path = os.path.join (build_path, "ravelling_wrath.pdf")
+os.makedirs (build_path, exist_ok=True)
+
+def chapter_html (chapter):
+  contents = utils.auto_paragraphs (chapter ["contents"])
+  contents, _, _ = blog_server_shared.postprocess_post_string (contents, None, chapter ["title"], False, False)
+  contents = re.sub(r"<bigbreak>", "", contents)
+  return contents
+
+chapters = [
+  chapter_html (chapter) for chapter in ravelling_wrath.main.posts
+]
+
+def wrap(html):
+
+ return '''<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Ravelling Wrath</title>
+    <style>
+@page {
+  size: 6in 9in;
+  margin: 0.75in;
+}
+p {
+  margin: 0;
+  line-height: 1.2em;
+  text-indent: 2em;
+}
+    </style>
+  </head>
+  <body>
+'''+html+'''
+  </body>
+</html>'''
+
+full_html = wrap("".join (chapters))
+
+with open (html_path, "w") as file:
+  file.write (full_html)
+  
+print("starting rendering book at "+ datetime.datetime.now().isoformat())
+
+weasyprint = True
+if weasyprint:
+  #for index, chapter in enumerate(chapters):
+    #for paragraph in re.finditer(r"<p>.*</p>", chapter):
+    #try:
+      HTML (string = wrap(full_html)).write_pdf (pdf_path)
+    #except Exception as e:
+    #  print (paragraph.group(0))
+    #  print(traceback.format_exc())
+    #print(f"done rendering chapter {index+1} at "+ datetime.datetime.now().isoformat())
+else:
+  subprocess.run(["wkhtmltopdf", html_path, pdf_path])
+print("done rendering book at "+ datetime.datetime.now().isoformat())
