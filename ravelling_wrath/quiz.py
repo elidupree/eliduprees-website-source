53d9103451061beae8f3655cd8be93f0895da484
elidupree <vcs@elidupree.com>
2021-08-09 22:04:20 -0400

    started writing gods quiz

---
+++ ravelling_wrath/quiz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+waiting = '''
+I spend lots of time planning out things I'm going to do or say in the future.
+If I know I will get something good in the future, that's almost as good as having it right now.
+????? I obsess about the past a lot
+I hold a grudge for a long time.
+????? something about having a picture of your whole life history and legacy
+'''
+seeking = '''
+I'm always learning new things.
+????? curiosity
+????? life changes vs. stasis
+I like to solve problems in unexpected ways rather than well-known, practical ways.
+'''
+stern = '''
+????? I work hard
+????? I sacrifice
+????? I like rules
+????? Principles are more important
+'''
+broken = '''
+????? people don't make sense
+????? some problems will never be solved and you just give up on them
+I don't have a strong sense of identity or purpose in life.
+'''
+blood = '''
+I'm happy to be the person I am.
+I take my own needs seriously, even when other people are having worse problems.
+Logic and emotion aren't opposites. I am both emotional and logical, and they work well together.
+'''
+
+multiple = '''
+I'm more interested in how things <em>are</em> than how they <em>should</em> be. (Seeking, Broken)
+????? education (Waiting, Seeking)
+I keep my promises even when they turn out to be much harder than I expected when I made them. (Waiting, Stern)
+?????? Sometimes it's better to just let an injustice slide rather than fight against it immediately. (Waiting, Broken)
+When there's something I don't understand, I'm never satisfied until I've figured it out. (Seeking, Stern)
+When I act on my momentary desires, I'm usually satisfied with how it turns out. (Seeking, Blood)
+I don't let other people push me around. (Stern, Blood)
+'''
