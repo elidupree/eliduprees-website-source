4989a8f28ec5ae475652e5331e0162e2980b1db0
elidupree <vcs@elidupree.com>
2022-01-01 17:02:11 -0500

    refactored the book rendering, splitting into multiple files to prepare for ebook distinction

---
+++ ravelling_wrath/book_versions/ebook.py
@@ -0,0 +1,32 @@
+import datetime
+import ravelling_wrath.main
+  
+tags = "Fiction, Young Adult, Fantasy, Urban Fantasy, Adventure, Consent, Healthy Relationships, Mental Health, Coming of Age, LGBT, Lesbian,"
+tags = [match.group (1) for match in re.finditer (r"([^,\s][^,]*)", tags)]
+
+content_opf = '''
+<?xml version="1.0" encoding="UTF-8">
+<package xmlns="http://www.idpf.org/2007/opf" unique-identifier="uuid_id" version="2.0">
+  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf"
+    <dc:title>Ravelling Wrath</dc:title>
+    <dc:creator opf:role="aut" opf:file-as="Dupree, Eli">Eli Dupree</dc:creator>
+    <dc:publisher>Eli Dupree</dc:publisher>
+    <dc:date>'''+datetime.date.today().isoformat()+'''</dc:date>
+    <dc:contributor opf:role="cov" opf:file-as="Fensore, Sarah">Sarah Fensore</dc:contributor>
+    <dc:identifier id="uuid_id" opf:scheme="uuid">c0b3ea68-aced-4746-966b-7b0fc27ba1fc</dc:identifier>
+    '''+'''
+    '''.join(f'''<dc:subject>{tag}</dc:subject>''' for tag in tags) + '''
+    <dc:description>'''+utils.strip_tags(ravelling_wrath.main.long_blurb)+'''</dc:description>
+    <dc:language>en</dc:language>
+    <dc:identifier opf:scheme="ISBN">TODO</dc:identifier>
+    <meta name="cover" content="cover"/>
+    <meta name="dcterms:modified">'''+datetime.datetime.now().isoformat()+'''</meta>
+  </metadata>
+  <manifest>
+  
+  </manifest>
+  <spine>
+  
+  </spine>
+</package>
+'''
---
+++ ravelling_wrath/book_versions/shared.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import os.path
+import sys
+import re
+import shutil
+
+from num2words import num2words
+
+import blog_server_shared
+import post_contents_utils
+import utils
+
+import ravelling_wrath.main
+import ravelling_wrath.definitions
+
+def replace_media_path(match, rav_media_paths):
+  full_path = match.group(1)
+  relevant_path = os.path.basename(full_path)
+  if rav_media_paths.get(relevant_path, full_path) != full_path:
+    raise RuntimeError(f"multiple media named {relevant_path}")
+  rav_media_paths[relevant_path] = full_path
+  return relevant_path
+
+def replace_media_paths(contents, rav_media_paths):
+  return re.sub(r"/media/(.*?)\?rr", lambda match: replace_media_path(match, rav_media_paths), contents)
+
+def chapter_html (chapter, is_print, rav_media_paths):
+  ravelling_wrath.main.replace_section_breaks(chapter, "/media/ravelling-wrath/symbols")
+  contents = post_contents_utils.auto_paragraphs (chapter ["contents"])
+  #contents, _, _ = blog_server_shared.postprocess_post_string (contents, None, None, False, False)
+  contents = ravelling_wrath.main.replace_all_emoji(contents, "/media/vendor/ravelling-wrath/emoji/black")
+  
+  if is_print:
+    contents = re.sub("<not_print>.+?</not_print>", "", contents)
+    contents = re.sub("</?print_only>", "", contents)
+  else:
+    contents = re.sub("<print_only>.+?</print_only>", "", contents)
+    contents = re.sub("</?not_print>", "", contents)
+    
+  contents = replace_media_paths(contents, rav_media_paths)
+  
+  symbols = chapter["symbols"]
+  running_symbol_filename = f'{symbols}-small.png'
+  if symbols == "nonaligned":
+    running_symbol_element = ''
+  else:
+    running_symbol_element = f'<img class="runningsymbols {symbols}" src="{running_symbol_filename}" alt="" />'
+  
+  rav_media_paths[running_symbol_filename] = running_symbol_filename
+  
+  contents = f'''
+  <div class="chapter chapter_{chapter ["chapter_number"]} {chapter.get("post_class", "")}">
+  <h2>Chapter {num2words(chapter ["chapter_number"]).capitalize()}</h2>
+  <div class="chapter-title">{chapter ["chapter_title"]}</div>
+  
+  <div class="runningleft">
+    {running_symbol_element}
+    Ravelling Wrath
+    {running_symbol_element}
+  </div>
+  <div class="runningright">
+    {running_symbol_element}
+    Chapter {num2words(chapter ["chapter_number"]).capitalize()}: {chapter ["chapter_title"]}
+    {running_symbol_element}
+  </div>
+  
+  
+  {contents}</div>'''
+  return contents
+
+
+def generate_html_and_linked_media_files(build_path, *, is_print, specific_chapter = None):
+  os.makedirs (build_path, exist_ok=True)
+  rav_media_paths = {}
+  chapters = ravelling_wrath.main.chapters
+  if specific_chapter:
+    chapters = [chapters[specific_chapter]]
+  chapters = [
+    chapter_html (chapter, is_print, rav_media_paths) for chapter in chapters
+  ]
+
+  fonts_css = ravelling_wrath.definitions.fonts_css("", mode="book")
+
+  for match in re.finditer(r"url\('(.+?.ttf)", fonts_css):
+    rav_media_paths[match.group(1)] = "/media/fonts/"+match.group(1)
+
+  with open("./ravelling_wrath/book_versions/shared.css") as shared_css, open("./ravelling_wrath/book_versions/print.css") as print_css, open("./ravelling_wrath/book_versions/ebook.css") as ebook_css:
+    css_string = shared_css.read() + (print_css.read() if is_print else ebook_css.read()) + fonts_css
+  css_string = replace_media_paths(css_string, rav_media_paths)
+
+  print(rav_media_paths)
+  media_dir = "./media/"
+  media_subdirs = os.listdir(media_dir)
+  for media_subdir in media_subdirs:
+    media_subdir_fullpath = os.path.join(media_dir, media_subdir)
+    for root, dirs, files in os.walk(media_subdir_fullpath):
+      for media_filename in files:
+        source = os.path.join(root, media_filename)
+        if media_filename in rav_media_paths:
+          if ("color" in rav_media_paths[media_filename]) == ("color" in root):
+            destination = os.path.join(build_path, media_filename)
+            os.makedirs (os.path.dirname(destination), exist_ok = True)
+            shutil.copy(source, destination)
+
+
+  def wrap(html):
+
+   return '''<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <title>Ravelling Wrath</title>
+      <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+  '''+html+'''
+    </body>
+  </html>'''
+
+  full_html = wrap("".join (chapters))
+
+
+  with open (os.path.join (build_path, "ravelling_wrath.html"), "w") as file:
+    file.write (full_html)
+    
+  with open(os.path.join (build_path, "style.css"), "w") as file:
+    file.write (css_string)
