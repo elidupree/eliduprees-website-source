ed23ed1d7152711cd77c4bca5d547111e9cfe2f3
elidupree <vcs@elidupree.com>
2022-02-19 23:54:24 -0500

    writing work

--- tftmuf/chapter_01.py
+++ tftmuf/chapter_01.py
@@ -20,13 +20,17 @@ But for me, it started when I was hired by the ????? University Neuroscience Dep
 
 Even then, I could tell they weren't just a normal neuroscience department. [my work location wasn't on ????? University campus] – my employers had somehow purchased the entire old Riker Hospital building, which I'd thought was abandoned when Riker moved to a new, modernized facility 4 years ago. [Should use absolute date?] And my paycheck came from a company called NeuroSci Innovations, which no one had ever heard of. [I should have known there was something cloak-and-dagger about it all.] But [I was in a hurry to get a new job after the startup I've been working for went under]. And the pay was good. <em>Suspiciously</em> good. Even a prestigious research university like ????? wouldn't normally have the budget to pay competitive, market rates for a computer graphics engineer. But that's what they were paying me. And I didn't say no to it.
 
-An abandoned hospital wasn't a normal workplace for a computer graphics engineer, either. [The former Riker hospital… Give a sense of physical progression – the commute there along desolate streets, where the hurricane a few years back had damaged buildings, and not as many people moved back because climate change was going to put a lot of this land underwater anyway [and that's why Riker moved their campus?]; now coyote scat was a common sighting and the City Council had to warn you not to leave food out; towering, square brick face of the building, very anonymous; and then the interior:] Some attempt had been made to clean up the first-floor treatment rooms and convert them into offices, but in many places, racks of unused medical equipment had simply been pushed into back corners ????? my direct boss, another programmer, didn't know what the deal was either, told me the higher-ups (Fuller?) don't know how to work with a programmer, don't get things done too quickly because that'll increase their expectations – ????? other floors locked off, electronic locks and surveillance cameras all over the place ?????
+An abandoned hospital wasn't a normal workplace for a computer graphics engineer, either. [The former Riker hospital… Give a sense of physical progression – the commute there along desolate streets, where the hurricane a few years back had damaged buildings, and not as many people moved back because climate change was going to put a lot of this land underwater anyway [and that's why Riker moved their campus?]; now coyote scat was a common sighting and the City Council had to warn you not to leave food out; towering, square brick face of the building, very anonymous; and then the interior:] Some attempt had been made to clean up the first-floor treatment rooms and convert them into offices, but in many places, racks of unused medical equipment had simply been pushed into back corners ????? other floors locked off, basically every door in the building had electronic locks and surveillance cameras all over the place ?????
 
-[????? They got me set up on the actual work, which turned out to be this weird graphics stuff, I guess it was for some psychological research but I have no idea what it was for ?????]
+[????? They got me set up on the actual work, which turned out to be this weird graphics stuff, I guess it was for some psychological research but I have no idea what it was for ????? they surveilled us to make sure we didn't look at the true-color version of the pattern, supposedly because of research ethics, but ????? told me they heard some of the other programmers looked at it and they got fired, and then the higher-ups put these color filters, guess we should just play along ????? my direct boss, another programmer, didn't know what the deal was either, told me the higher-ups (Fuller?) don't know how to work with a programmer, don't get things done too quickly because that'll increase their expectations – ?????]
 
-[????? But my one flaw has always been that I can't look away from something once I get curious. I wanted to know what was on the locked-off floors, what all this security was for ????? Looking at the security cameras – what Reginald would later call "a profoundly amoral decision"]
+[????? But my one flaw has always been that I can't look away from something once I get curious. I wanted to know what was on the locked-off floors, what all this security was for ????? I looked up the brand of the security cameras, and saw that they had a default password, and tried the default password. Looking at the security cameras – I wasn't trying to be some sort of citizen-investigator, finding what they were up to, I was just doing it because I was curious. Because I <em>could</em>. What Reginald would later call "a profoundly amoral decision"]
 
 [????? I made it a whole little project, detecting motion and applying image classifiers. If you don't know what an image classifier is, it's one of those AI tools that they use at places like Google – you put in an image, and it gives you a bad guess about what it's a picture of.
+
+[seemed like Reggie was in two places at once, I thought there was like an error or something]
+
+[wrote out what was happening in each room, to try to get a sense of the big picture]
 
 [????? If it wasn't for the image classifiers, I would never have noticed, I would have just glanced past and assumed there wasn't anything there. Even after it said "child on bed", I still just assumed the classifier was wrong, like usual. But because I was keeping notes on each camera, it eventually became conspicuous that those few rooms were the ones that were still blank. I sat at home staring at the images, wondering what was in them that made them get classified as children, when there obviously weren't any children in the pictures. Or, as I eventually began to wonder – why my brain couldn't explain what they were instead of children.]
 
@@ -38,13 +42,13 @@ An abandoned hospital wasn't a normal workplace for a computer graphics engineer
 
 This is what happened on [????? that day].
 
-The experiment was scheduled for 11:00 AM. At 9:56, two assistants unlocked the room where Kayla was being held, after which they led her to the experiment room and strapped her into the fMRI machine. ?????, Which resulted in Kayla being left unattended for a total of six minutes, starting at 10:07. Visible only to the room's security cameras, which no one was monitoring.
+The ?????phorin Experiment was scheduled for 11:00 AM. At 9:56, two assistants unlocked the room where Kayla was being held, and told her she was to be taken to the experiment room. She complied. ????? Kayla, who was physically weak and stumbling, accidentally-on-purpose knocked one of the assistants into a ?????/Kayla was already physically weak and stumbling, and they knew this; this gave her an excuse to… ?????, vials of medicine shattered on the floor. Once they had strapped her into the fMRI machine, there was a hasty discussion. Dr. Fuller would be very angry, they agreed; it was imperative that this mess be gone before he learned of it. At 10:05, they agreed to split up – one to fetch replacements, the other to stay and clean. But as soon as the second assistant was alone, they realized they had no broom or dustpan to clear the broken glass. They glanced at Kayla; they must have known it would break protocol to leave her unattended. But she was fully restrained, lying motionless in the machine. Surely nothing would happen in just a few minutes. Surely Dr. Fuller's anger was a more urgent concern. This resulted in Kayla being left unattended for a total of six minutes, starting at 10:07. Visible only to the room's security cameras, which no one was monitoring.
 
-During the first two of those minutes, Kayla slipped out of her restraints and grabbed what she needed. By 10:09 AM, she was out the door. ?????
+26 seconds from the moment the door closed, Kayla had fully freed herself from the restraints. Over the next 18 seconds, she rushed about the room, gathering the things she would need. By 10:09 AM, she was out the door. ????? sped down the hallway, following the exit signs to find ????? stairs.
 
-Of course, at the time, I didn't know any of that. All I knew was what I could see on the camera: a frightened, teenage girl, wearing nothing but a hospital gown, holding a scalpel in front of her like a weapon. In the twelve seconds she was in view, she entered the lobby on the fourth floor, looked quickly around her, and crept to the nearest stairs, then disappeared downwards. My mind jumped to an explanation: she was a captive, trying to escape the building. I was almost right. Right in the ways that mattered.
+And that's where I came in. At that moment, I just so happened to be <em>watching</em> the camera that she ran past. Of course, at the time, I had no idea of what had just happened. All I knew was what I could see on the camera. And what I saw – the way my mind first interpreted it – was a frightened, teenage girl, wearing nothing but a hospital gown, holding a scalpel in front of her like a weapon. In the twelve seconds she was in view, she entered the lobby on the fourth floor, looked quickly around her, and crept to the nearest stairs, then disappeared downwards. My mind jumped to an explanation: she was a captive, trying to escape the building. I was almost right. Right in the ways that mattered.
 
-I've done a lot of things I'm not proud of, but I'm proud of how I reacted in that moment. My worst fears about this place were realized, and I knew I had to do something – now – or I would never forgive myself. If I did nothing, I thought, Kayla was going to step out of the stairs onto the lobby floor, in plain view of half a dozen people – people I could see from my view of the camera, who she couldn't see from where she was standing. [????? I couldn't just let it happen.]
+I've done a lot of things I'm not proud of, but I'm proud of how I reacted in that moment. My worst fears about this place were realized, and I knew that if I didn't take action – immediately – then I would never forgive myself. If I did nothing, I thought, Kayla was going to step out of the stairs onto the lobby floor, in plain view of half a dozen people – people I could see from my view of the camera, who she couldn't see from where she was standing. [????? I couldn't just let it happen.]
 
 At 10:10 AM, I turned off every security camera in the building, stood up, and sprinted for the stairs. [????? I turned off the security cameras – not just the ones we were going to go past, because that would be conspicuous, but every security camera in the building.] ????? Cracked open the door and called out softly, "this way! I can help you." ????? Face-to-face with Kayla for the first time.
 
@@ -62,30 +66,39 @@ At 10:10 AM, I turned off every security camera in the building, stood up, and s
 
 "What?"
 
-"Even if I get away, they'll still catch me. They have a…" she began. Her voice rattled on, hurriedly explaining things to me. But something bizarre was happening as she spoke. There was nothing wrong with her words – they were plain English, they fit together in order – but even as ten seconds passed, then twenty, my brain simply didn't make sense of them. ?????
+Her words came quickly, low and urgent. "None of that would work! I can't run, they'll still catch me even if I get away. They have a…"
+
+But as she kept speaking, something bizarre began to happen. There was nothing wrong with her words – they were plain English, they fit together in order – but even as ten seconds passed, then twenty, my brain simply didn't make sense of them. ?????
 
 "I'm sorry," I interrupted, "I don't understand what you're saying –"
 
+I had been afraid that she ????? wouldn't know what I meant, that I would have to explain how it was that I didn't understand]. But I knew instantly that she had understood. The look that blossomed on her face wasn't confusion or frustration – it was <em>fear</em>. [She knew that she could keep trying to tell me these things, What she desperately needed me to know, but I would never understand, and then her time would run out. Or, as I later understood, that her only option was to do something horrible
+
 [????? When she understood what I had said," a look of fear crossed her face. Not the immediate fear of something being going to jump out at you, but the slow dread of realizing you're trapped in a situation that will give you no choice but to do something horrible] "You – you're really not in on it," she said.
 
-"In on what?"
+"In on what?" [????? this response shouldn't get its own paragraph, but the earlier ones are okay]
 
 In response, she uttered some more of those words I couldn't understand. But she knew right away that it wasn't working. "Damn it! I don't want to do this to you!" she said tightly. "But I need you to understand!"
 
 "Do what?!"
 
-[????? She couldn't answer me. she could only steel her resolve to do the thing. She swallowed and steeled her resolve to do whatever it was. Then ????? finally the meaning of the tablet she was carrying was revealed. She lowered it onto the desk, turning the screen to face me, and pressed a button.]
+[????? She couldn't answer me. she could only steel her resolve to do the thing. She swallowed and steeled her resolve to do whatever it was. Then ????? finally the meaning of the tablet she was carrying was revealed. She lowered it onto the desk, turning the screen to face me, and pressed a button labeled OVERSTIM.]
 
-The next few minutes were a ?????horrifying blur. It was the animation I'd been working on. But it was different ?????
+The next few minutes were a ?????horrifying blur.
 
-[????? When my mind came out of the blur, I took stock of what had happened. I had a stinging sensation in my arm, and I dug through half formed memories to find –]
+On some level, deep in my mind, I understood what my eyes were staring at. It was the animation I'd been working on. The only difference was the color, no longer green and red, but purple and white. But somehow, the colors reached deep into my brain, gripping my deepest instincts, holding me in a dreamlike state where I couldn't form a coherent thought, [my brain refusing to acknowledge that the Pattern was even there] [probably something humiliating from Marvin's past here] [more details referring back to the initial description] ?????
 
-"Is this some kind of mind control?! Did you <em>inject</em> me with something?!!" [????? I freaked out and pulled back]
+[????? my head still spinning, a stinging sensation in my arm, brain frantically digging through half-formed memories to find any idea of where I was, what had happened in the last hour –]
+
+[????? Finally a fragmented image stayed in place, Kayla crouching in front of me, speaking quickly with an expression of worry on her face, the injector in her hand – I looked back at my stinging arm –, "Is this some kind of mind control?! Did you <em>inject</em> me with something?!!" [????? I freaked out and pulled back]
+
+But then, my mind caught up with the words she had been saying.
 
 "– a time machine, they have a time machine. Tell me when you can understand me. They have a time machine. They have a time machine. Tell me when you can understand –"
 
+And it all fell into place. The ????? Reggie being in 2 places at once. The ????? . "A time machine –" I gasped "– the machine in the [underground floor]"
 
-"Everyone but the scientists in charge is under some kind of mind control"
+[Kayla was instantly relieved that I could understand, she rushed to explain] "You were already being mind controlled. Everyone here is, everyone but the scientists in charge. What I did is, I broke the control. The overstim, the ?????cyanosol injections, they're what they tested on me, they're why I can resist it. Everyone else, you can talk about time travel right in front of them and they just stare past you – "
 
 ?????
 
