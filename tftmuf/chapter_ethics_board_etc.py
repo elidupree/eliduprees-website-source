0b6f23231bfe39059ab7d0284dde0e5edea39341
elidupree <vcs@elidupree.com>
2022-03-03 00:38:46 -0500

    writing work

---
+++ tftmuf/chapter_ethics_board_etc.py
@@ -0,0 +1,39 @@
+????? We owe it to Aster to let them use the antidote
+
+[reg being different than Aster - deeper, gravelly voice, look of fear - in all the time we'd known Aster, we'd never seen them afraid, not even when the gun was being pointed at them. but Reginald was deeply afraid]
+
+"I'm dissociating, you need to take me back to my office –"
+
+"I'm sorry. We can't let you leave until we've explained what's going on"
+
+?????
+
+[Reginald feeling utterly betrayed by Aster; as a grave whisper, "God damn it, Aster, what have you done?"; K+M offer excuses and Reginald argues against them; at some point, "'Reggie' is only a name for my intimates, I can't believe Aster gave it out to everyone"]
+
+[With deeply aggrieved conviction] "If I get any say in this at all, we are going to have some god damn <em>ethics</em> around here."
+
+[Chapter break, "the ethics board"]
+
+
+[Reginald saying how everything up to this point has been an utter mess ethics wise, including some harsh words for Marvin's choices ("profoundly amoral act") and saying Kayla was "excused but not justified"]
+
+
+
+
+"Are you vegan?" Kayla cut in.
+
+"I happen to enjoy a good steak on occasion," said Reginald [physicality: "Reginald sniffed", haughty-ish, sarcastic excuse-me tone].
+
+Kayla [settled a little, not trying to press the issue but not feeling like Reginald's judgment was as much of a threat]
+
+Reginald [understood the implication; implicitly, he recognized it as a threat to him being taken seriously]. "Are you going to [say I'm evil because I'm not vegan?]" [With a incredulous tone – a couple degrees less severe than "voice dripping with skepticism"]
+
+Kayla shrugged. "Your lifestyle requires the murder of sapient beings," she said. Her tone wasn't accusatory – it was the tone of someone who is discussing the plain facts. "[You have no more right to judge than any of us]"
+
+Reginald [got back on top of things]. "Even if I take that to be true, it only illustrates my point. If none of us are morally pure, then that is why we cannot simply defer to any one person's judgment, but must actively [work together to be rigorously protected against our individual flaws]"
+
+
+
+[maybe somewhere while discussing the "morality of a god" thing – "then we are a necessary evil"
+
+"Believing yourself to be a good person, and believing yourself to be an evil person, are both just ways of avoiding responsibility for your choices. [What's important is the process of looking for the best option available, and when you tell yourself a just-so story about what kind of a moral agent you are, you settle into a habit of not evaluating the options]"
