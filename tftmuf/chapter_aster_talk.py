484a9412f1420d28c51ac286957770f9a8af4edc
elidupree <vcs@elidupree.com>
2022-03-03 20:10:48 -0500

    writing work

---
+++ tftmuf/chapter_aster_talk.py
@@ -0,0 +1,30 @@
+[The phrenochondria show evidence of incredibly advanced genetic engineering, but who could possibly have the technology to do that?]
+
+"Time travellers, right?" I said.
+
+Both of them looked at me [like that was a ridiculous thing to say]
+
+"What? Isn't that the obvious guess? You've just discovered time travel, and suddenly someone shows up with [impossibly advanced technology]. They're obviously time travellers! The [brain censorship] is even specifically <em>about time travel!</em>"
+
+Aster leaned way back in their chair [making it creak], a thoughtful expression on their face. "[None of our time machines go back more than a day]"
+
+But my brain was already rushing ahead ?????. "But you don't know <em>why</em> they only go back by a day! Maybe we're going to discover how to remove that limitation – heck, maybe the time travellers are <em>us</em> from the future! Think about it – maybe we [did lots more time travel but then discovered that time travel was actually really bad, and so we went back in time and used the phrenochondria to prevent anyone from discovering time travel in the first place]"
+
+[Aster argued back, but they were clearly enjoying my theory and meant to encourage me to find workarounds to their point:] "[fundamentally the way these time machines work is that they only go back to when they were built, but the coyotes have been invisible since decades before we built any time machines] These mysterious 'time travellers' were active at least decades earlier [and possibly much longer than that – for all we know, the phrenochondria have existed for as long as <em>Homo sapiens</em>. Not longer, the great apes don't have them – we tested. But maybe that long] "
+
+"[Maybe someone else invented time machines first]"
+
+"They're not us," said Kayla [decisively – dour tone]. "Whoever did this, they weren't good people."
+
+Aster's eyebrows shot up. "I'm eager to hear why you say that!"
+
+"You mean, besides the fact that they nonconsensually mind controlled every human being on the planet?"
+
+"Yes, besides that," said Aster, as if it was the most natural thing in the world.
+
+????? Maybe cut the two lines above? they are cool but let's make sure they don't detract from the transition into:
+
+"They had the technology to mind control every human being on the planet, and they only used it to stop us from time travelling. They didn't stop us from abusing each other. They didn't stop us from destroying the planet we live on. They didn't use their genetic engineering to ????? or cure diseases. With that kind of technology, they should have been able to solve all the problems in the world. But it's like they just didn't care."
+
+[Maybe: an idea came sneaking into my head] "But what if they <em>did</em>?" I said. "We think the phrenochondria cause personality changes, right? If they go back as far as <em>Homo sapiens</em>, we wouldn't have any way of knowing what humans would be like without them. What if people used to be even worse than they are now? And then the time travellers went back and tried to fix them, but they came from a timeline that was so horrible that even after you fixed all of their biggest problems, our problems are the ones that were left?"
+?????
--- tftmuf/chapter_ethics_board_etc.py
+++ tftmuf/chapter_ethics_board_etc.py
@@ -15,7 +15,23 @@
 [Chapter break, "the ethics board"]
 
 
-[Reginald saying how everything up to this point has been an utter mess ethics wise, including some harsh words for Marvin's choices ("profoundly amoral act"/I hung my head/"at least you have the decency to be ashamed") and saying Kayla was "excused but not justified"]
+[Reginald saying how everything up to this point has been an utter mess ethics wise, including some harsh words for Marvin's choices ("profoundly amoral act"/I hung my head/"at least you have the decency to be ashamed") and saying Kayla was "excused but not justified" (there is a difference between a justification and an excuse! We can excuse Kayla's [brutal murder of a man who had already surrendered] because [of her natural, extreme feelings about the situation he had put her in], but I will not sit here and allow you to justify it. More specifics, not sure of relative order:]
+
+"[Marvin spied on innocent people.] Some of whom were children, I might add –"
+
+[Need to fit in around here: discussion of Marvin delaying several days before doing anything about it even once he knew about the children,. "I was going to report it," I said, but I knew I wasn't sure if I really would have.]
+
+[Kayla defensive of Marvin's spying because it led to her rescue]
+
+"Did he know that?" Reginald demanded. ????? "[Did he know they were evil when he started]"
+
+[Kayla defensive some more – "of course he knew they were evil! They put surveillance cameras all over the place, they turned this building into the goddamn Panopticon! Even if they hadn't [done monk consensual experiments] they would still be evil –"]
+
+But I knew that Reginald was right. [I hadn't thought about it at all – I was just lucky] I hung my head.
+
+"At least you have the decency to be ashamed," said Reginald brusquely.
+
+
 
 
 
@@ -31,6 +47,70 @@ Reginald [understood the implication; implicitly, he recognized it as a threat t
 Kayla shrugged. "Your lifestyle requires the murder of sapient beings," she said. Her tone wasn't accusatory – it was the tone of someone who is discussing the plain facts. "[You have no more right to judge than any of us]"
 
 Reginald [got back on top of things]. "Even if I take that to be true, it only illustrates my point. If none of us are morally pure, then that is why we cannot simply defer to any one person's judgment, but must actively [work together to be rigorously protected against our individual flaws]"
+
+
+
+
+
+If you were involved with the University of all, it was hard not to know about Dr. Ethel Wright.
+
+One of the University's most respected and prolific physicists, she had authored and co-authored hundreds of papers in her long career. She was one of those people who simply never stops: Even in her old age, she was constantly moving on to the next project, the next ?????. She was old enough to have been the first woman on the physics faculty, and then later, the first woman head-of-department, a position she still held thirty years later.
+
+I sat down with her in her office [physical description; she had arrived several hours before the 4 PM meeting, so we had time; she didn't know I was there to judge her]
+
+"I'm a writer," I lied blandly. [I told her that Dr. Fuller had hired me, to prepare to have a favorable story to release to the public if we ever ended up in the public eye, and so now I was going around and talking to all of the scientists to have good background material]
+
+[She was a little surprised, but not unfavorable to the idea. I started with easy questions, learning a long story of interesting details about her history with the University]
+
+
+[An experiment involving an iron vessel, but the results were very confusing, and she kept forgetting to follow up on it]
+
+"At that time I had a great deal of worry, as you can imagine, that age was catching up with me. [I have always defined my personal value based on the abilities of my mind, so losing that is like losing myself]. [But I thought about it in an analytical way, and it was conspicuous that the problem only appeared on this specific project – for reasons I'm sure you are aware of]"
+
+?????
+
+And then I came to the [big question that was the biggest purpose of this talk]. Keeping my voice casual, I continued, "how do you feel about the children?"
+
+[Instantly icy] "Dr. Fuller didn't send you," she said [crisply? no, feeling-threatened-ly? no, more restrained than either].
+
+?????
+
+[She tries to call Doctor Fuller, the phone rings and rings – or is Marvin carrying the phone right now?] "
+
+[Marvin didn't expect to be angry, but seeing that Dr. Wright knew about the children and tried to cover it up makes him start saying things in a somewhat angry way]
+
+?????
+
+She bowed her head. "Is this my judgment, then?"
+
+"I am not God," I said, a bit reproachfully.
+
+
+
+
+
+
+
+
+
+
+[Ethics board in session – everyone is here]
+
+
+
+[Also, is going back in time murder? Kayla says we SHOULD consider the idea that we are murdering and/or torturing billions of people by going back in time even once. So they have to talk about the basis of their moral system. Maybe Reginald says "when I said ethics, I didn't necessarily mean we needed to go deep into moral philosophy"/?????/"I guess maybe we do need to go deep into moral philosophy… I suppose we are not short of [he said the word with absolute disdain] <em>time</em> for such a discussion"… So what is the fundamental basis of morality?. Someone says that suffering is bad and satisfaction is good, and it's the total amount of suffering and satisfaction that makes the world good and bad. How do you tell whether to Something is murder? Well, the reason murder is bad is because it causes people to suffer]
+
+"But then destroying the entire universe would be a morally neutral act" [because no one suffers afterwards]
+
+"Yes. Destroying the entire universe is a morally neutral act."
+
+[Ethics of going back in time and repeating someone's suffering – does the suffering count twice, or once, or zero times? Ontoh says it only counts if it isn't reverted]
+
+[Something like [I feel like I've forgotten the proper way to state this though]: Moral statements generally runs from someone considering two possible ways things could go and decide that they like one of them better than the other, yes? But if you destroy the entire universe, there is no one who can make that comparison]
+
+
+
+[Ethics of reengineering the phrenochondria to make people stop abusing each other; ethics of removing them (removing them is nonconsensual mind control, but leaving them in is also nonconsensual mind control)]
 
 
 
