b54a3071f2396b260612abbd36b844e5ef7acd6d
elidupree <vcs@elidupree.com>
2022-04-01 18:08:34 -0400

    Split off a chapter file

---
+++ tftmuf/chapter_02.py
@@ -0,0 +1,142 @@
+
+
+[????? Reestablish scene, maybe mention more details here]
+
+She continued in that same, dead-tired voice.
+
+"You need to get to one of the time machines as soon as you can. Once you go back, you can send this video to yourselves, so it turns into a stable time loop. Don't think about how that works right now, you'll have plenty of time to think about it once you're back at 3 AM. Right now, you have to worry about getting there.
+
+"The time machines are [????? on floor]. You can take the stairs at the end of the North Wing, they're safe because no one uses them all day. But even so, you want to spend as little time in the halls as possible [????? to make sure that you don't change your own past]. And while you're walking there, don't look at anything if you can help it! Keep your head down, don't look in any of the rooms you go past, definitely don't look at other places on the security cameras –"
+
+"– but leave them on!" croaked a voice – my voice, Marvin's voice, speaking for the first time since the video started. It was breathy and breaking, the voice of someone who has been crying for hours, who is only forcing themselves to speak because of a sudden need. "Leave the cameras on! No one ch–" he gasped, before his throat failed him again, lapsing into voiceless breaths.
+
+"– no one checks them until the meeting at 4 PM," video-Kayla finished for him. "He's right – leave them on, but don't look at them. That'll save you a lot of the trouble we had to deal with. But right now, you need to… uh…" she looked to Marvin again. "Damn it, Marv, you know I'll say it wrong if I have to tell them the computer stuff…"
+
+But once you've forced out the first few words, the rest of the words come easier. Marvin lifted his head halfway out of his hands, not quite daring to look directly at the camera. "We – we're going to put this video in a zip file, with – with some other documents. You'll need to copy the whole zip file onto a USB stick so you can bring it back with you. I – I guess you should start copying it now, don't wait for the video to finish, it might take a little while. And then – we – we'll make some instructions for you to print out – so I guess you should start printing those, next, after you start the copy. [????? Video to show you how to use the time machine]" [????? he kept rambling, weak voice, not really believing in it]
+
+????? The Kayla crouching next to me – not [tired] like the one from the video, but gaunt, strung tight with terror – ????? "Why aren't you doing what he said?" she hissed.
+
+Her words [????? knocked me out of my hypnotized state]. I fumbled through Marvin's instructions, digging a USB drive out of a drawer in my desk, then finding the file to print – 02_print_this_out.png. It was definitely me who had chosen these filenames – practical names, telling me what I needed to know, using two digits out of habit even though the files only went up to ????? "09". It was eerie, seeing my own handiwork on something I had no memory of creating.
+
+"–
+
+?????
+
+?????
+
+"
+
+<bigbreak>
+
+
+There are few things as maddening as being told that you must not look at something, when the thing is all around you. [????? The walk down the hallway shouldn't have been hard, but) maybe: should have been the easiest part of what we needed to do ????? repeatedly wanting to look into the rooms, look over my shoulder]
+
+"What do you think happens if we change our own past?" I said nervously.
+
+"I don't know, but it's bad! They said it was bad, and we have to trust them, right?"
+
+"It feels like we're being followed," I muttered to myself.
+
+I shouldn't have said it in front of Kayla [????? who was still on high alert. She whipped around, eyes scanning the corridor behind us.]
+
+
+
+[????? Kayla's harsh/sudden whisper] "I feel like we're being followed," [????? she said]
+
+
+
+"Unless
+
+<bigbreak>
+
+
+
+?????
+
+????? Face-to-face with the time machine
+
+The time machine didn't look like something from a sci-fi movie. It was a bulky structure of rough metal. Not like something from a sci-fi movie, more like an relic from the Industrial Revolution. ????? Other than being plugged into a computer, it was old-looking ????? But maybe I SHOULD have been scared by something from the Industrial Revolution. A new invention, nowhere near perfected, but already able to radically alter the world as we knew it. And maybe not for the better.
+
+We had no idea of the danger we were in. Even the Marvin from the video, sobbing in terror of what would happen to him, [hadn't seen the full implications.]
+
+We had already seen what the time machine looked like, in our future selves' instructional video. Its central structure was an enormous egg, stretching from floor to ceiling, held in place by ????? [physical description, Hatch]
+
+[But we couldn't climb into that Hatch right away. The procedure was: [/ First, we had to [enter into] the computer the exact mass of the matter to be transported.]]
+
+"How can it be accurate to the nearest gram if it's sometimes off by 20 pounds?!"
+
+"I don't know, but it worked for our future selves" If I had known the real reason for the extra 20 pounds, I wouldn't have made it through the day. I would have had a complete breakdown.
+
+Its central structure was a [steel eggs]
+
+[????? We had already seen it in the video, so it's appearance wasn't surprising, but it was still intimidating. The first thing that demanded your attention when you walked into the room, an 8 foot tall, gleaming egg of steel, towering above us. It had clearly been built in several pieces, and then bolted together here, huge steel plates sticking out of the surface where the join lines were. On the face closest to us, a step ladder leading up to a hatch, also made of bulky metal plates, not tall enough to stand up, but you could crawl through it]
+
+[????? It was just sitting there, deceptively inactive, looking like a bulky antique from the Industrial Revolution. Not like something that could delete us from existence. But it was. Even the Marvin from the video, sobbing in terror of what would happen to him, hadn't even begun to understand the danger we were in.]
+
+[????? But we couldn't climb into that hatch right away. Before anyone could time travel, you had to program their exact mass into the machine, or bad things would happen]
+
+[????? A massive industrial scale, a metallic platform wide enough for both of us to stand on, with a label boasting that it could weigh up to a thousand pounds, to a precision of a single gram.
+
+
+?????
+
+"Now. Kayla." [Video-Kayla's voice was suddenly stern, serious] "I really hope you went to the room I told you to. We picked this room because there's food in the fridge – [and this is the private break room for Simon, who is not working today and only shows up for the meeting], so it won't change anyone's timeline. You need to eat, <em>now</em>. Suppressing our hunger was a stupid decision. It was smart when we were [????? captive/only run], but it was stupid to keep hiding it for hours after we could have asked Marvin to get food for us. Marvin, if she doesn't want to eat, you have to –"
+
+But [the Kayla with me had already flung open the fridge and was stuffing her face. And the other Marvin had realized it. He mumbled "She's probably already doing it"]
+
+"Oh God damn it, you're right, she's probably not even listening to me. Listen, uh… Marvin? Marvin, make sure she doesn't eat more than half of what's in that fridge, she'll vomit. Ask me how I know. She can eat half now, and half… maybe an hour from now ?????"
+
+[The Kayla with me was glaring back at her indignantly. "I AM listening" she tried to say amid mouthfuls of food, humiliated rage from having her flaws pointed out in front of me, by someone whose opinion she couldn't possibly deny. She pointedly started dividing everything in the fridge into two equal piles – well, three piles, one of which was all the animal products]
+
+[After she forced herself to stop eating, possibly Marvin trying to make conversation to distract her:]
+
+"Are you vegan?"
+
+[Physicality] "[explanation of how the captives were fed; I think Kayla should mention what vegan ethics mean to her here, although not in detail]"
+
+[I was in awe] "You were literally starving, and you still refused [food]?"
+
+She answered with a single word. "Yes." [And in that word, I could feel/but she felt that word with/[enormous] pride/smoldering hatred. The will to destroy those who had wronged her, no matter what was thrown in her way. I thought back to the rifle she was carrying in the video,. She had
+
+?????
+
+The Kayla in the video was still talking, almost forgotten behind us. I skipped back to where we had left off.
+
+"– she needs to be eating about 4000 calories per day. I'm not joking, we looked it up. There was research in the 1940s – the Minnesota Starvation Experiment, you can look it up – where people volunteered to go on a starvation diet, so that humankind could learn what it takes to help someone recover from starvation. And that's what they found out – you need about 4000 calories. Now, I don't want you to tell me how many calories things are. I don't want to know, diet culture kills people. But you – Marvin – you told me you've tried dieting a few times yourself, so you know this stuff, and you can help. ????? you'll need to stock up for that when you go shopping later. But we can't do that yet, first we have to tell you the time travel rules."
+
+<bigbreak>
+
+?????
+
+[quote from Minnesota Starvation Experiment guidebook] "refrain from arguments; the starving are ready to argue on little provocation, but they usually regret it immediately"
+
+?????
+
+"They've got time. We should explain this from the beginning"
+
+"Do we need to? We're going to give them our notes, right?"
+
+"Yeah, but we wrote them for <em>us</em>."
+
+"They <em>are</em> us!"
+
+"I don't understand half the comments I wrote on my own code two years ago.
+
+
+?????
+
+
+"We went back in time, just like you're about to do," she said. "We went back in time over and over. And we were careful. We made sure not to let our own past selves see us, or do anything that would change what happened to them. We didn't let anyone use the time machines except for us. And we – <em>I</em> – killed the scientists who created them."
+
+[????? An expression of unmistakable pride ????? The revelation, already somewhat expected due to the gun and blood, that the Kayla in the video had killed real, living people. I felt disturbed by it/dissociated, feeling at a distance from things. But the Kayla crouching next to me was watching with rapt attention. In awe/admiration of the version of herself who had succeeded (?). The promise that killing those people – the ones who had held her captive – was within her power.]
+
+"But somehow, we must have made a mistake. [????? When we were done killing them, we noticed that the original Kayla and Marvin hadn't gone back in time the way we had. Somehow, they must have seen one of the changes we made. And so they made different choices. They were still arguing about what to do. They were supposed to go back in time to become us, but if we let them go back in time ?????, they would become a <em>new</em> version of us. They would do different things than we did. And everything <em>we</em> did would be forgotten by history.
+
+"And worse, they would meet the same fate we did. Because in the new timeline, with them instead of us, the first Kayla and Marvin <em>wouldn't</em> see the same changes we made. They'd be different again. It would be an endless loop of them almost winning, then having it all wiped away again.
+
+"But there's a way to fix it. Reggie had some notes on his computer – God damn Reggie, I thought he was evil like the rest of them, but maybe it's more complicated. The last thing he did before he died – literally while I was shooting all of them – he wrote… well, you can look at it." Video-Kayla held something up to the camera – a slightly crumpled sticky note, with some gibberish hastily scrawled across it in ballpoint pen. And under the gibberish, it said, "LAPTOP PASSWORD – TIME TRAVELERS YOU NEED THIS".
+
+"So we looked on his laptop. We found a file full of notes. And the notes, they said how you could make a stable loop. So that's what we're trying to do now. That's where you come in. We've gone back in time again, and we're going to send you this video, and the notes, and some other files. And then after you go back in time, you can send the same files again, to yourself. Marvin made a script that you can use, to send them at the exact same time we're sending them. And then things will be the same for the next version of you – the same as they are for the first version of you. We'll be gone – we might be already gone when you hear this – but <em>you</em> don't have to disappear."
+
+
+[????? "So the first thing you need to keep in mind – you don't want to change your past, but right now, you ARE the past version. So that means you don't want to BE changed. Which basically means, don't look at anything you don't have to. If you look at something, and then you go back and change it, that's a problem. But if you didn't look at it, it's fine to change it. Of course, after you've gone back in time <em>once</em>, you'll be the future for one version of you, but the past for others. The rule is – you can always look at what your past selves are doing, but you don't want to look at anywhere you think your future selves might be.]
