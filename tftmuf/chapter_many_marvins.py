a3fa418f1c39ba9ad7c46ef4dd48caf9f4e73834
elidupree <vcs@elidupree.com>
2022-04-24 13:10:02 -0400

    Writing work

---
+++ tftmuf/chapter_many_marvins.py
@@ -0,0 +1,37 @@
+
+
+[When PoV-Marvin is being debriefed: "and we've only had a few time travel mishaps". I'm sure for [the debriefer] this felt like something to be proud of, because they had been programming for umpteen person-months, and only had a few mishaps. But for me, who had just recently [ranted about unit testing, thought about how I would do stuff reliably], it was like all my planning had immediately been rewarded by "haha, you failed anyway!". "A few mishaps?" I exploded. "????? [Every single mishap could immediately end the world as we know it]]
+
+
+
+"We want to recruit an expert on formal verification."
+
+[Marvin [not me] was presenting to the Ethics Board. This Marvin had an air of knowing what he was talking about – he was good at making professional presentations – he must have practiced a lot, because I wasn't that good at this]
+
+?????
+
+[Why should we approve this]
+
+"[To prevent a mishap that would alert Nochli to our activities]"
+
+[Reginald: "yes, of course it's [to defeat Nochli], that's the justification for EVERY request. You have to justify why [it is worth the risk of alerting Nochli that comes with every outing]"
+
+[Marvin had a presentation for this]
+
+["Based on our expected rate of software development that we will need, for plans that have already been agreed upon, we project [pointing at graphs] that the total amount of lines of code written will be 400000, resulting in 400 bugs and 80 additional time travel mishaps. With an estimated 3% chance of alerting Nochli for each time travel mishap… If we don't make drastic improvements to our programming practices… the projected probability of humanity's defeat due to software bugs is… 92%."]
+
+[Having a powerful statistic like that does wonders to make a presentation more convincing. In business, people are always asking for money, and usually it's a hard sell, where even if you know that something is a good idea, it's hard to prove it to the stakeholders. But "92% chance of ending the world as we know it" did not leave much room for doubt. Everyone at the table was stunned. "Well," said Reginald, obviously struggling to maintain the polite decorum. The atmosphere in the room had clearly switched from "Marvin needs to convince Reginald" to "Reginald has a problem and is helping Marvin will have a solution". Reginald started asking about formal verification]
+
+[it's where you mathematically prove that your code is correct. With unit tests, you can guarantee that the code behaves correctly in the situations that you thought of testing. But when the code goes into production, there's always going to be situations that you didn't think of. With mathematical proofs, you can guarantee that]
+
+[Isn't that too good to be true? Why doesn't everyone do this?]
+
+[Because it's difficult, takes a long time, and only a few people know how to do it. We've been following industry best practices for testing, like they use that big tech companies like Google and Microsoft.]
+
+[Why can't Marvin's do this?]
+
+[Some of us tried learning, and hired tutors as usual. But frankly, we aren't mathematicians. Next to regular programming, formal verification is like learning a whole new language. Kaylas might be able to do it, because she learns faster AND maintains a higher time ratio, but she's starting from 0 programming knowledge – getting a Kayla that point would take weeks of global time, probably over 5 subjective years, and, there are, ah –" he glanced at the Kayla who was sitting on the ethics board]
+
+"– unresolved challenges with extending a Kayla beyond 5 subjective years," said Kayla crisply. She looked like she was [about 21, and thus, near the end]. No doubt, she wanted to [nip this topic in the bud] [so as not to have it be the topic of debate] "We are working on improvements to Kayla Management. But there are no guarantees."
+
+[There was some more debate, but the overwhelming necessity may be outcome inevitable – the ethics board unanimously approved the proposal]
