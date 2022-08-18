fef8d5b67ba9c4822987fa7223074019124e520d
elidupree <vcs@elidupree.com>
2022-08-17 20:53:10 -0400

    writing work

---
+++ tftmuf/chapter_we_uh_work.py
@@ -0,0 +1,31 @@
+[Chapter title: "We, Uh, Work for a Secret Organization, Studying Aliens"]
+
+[Marvin talking to formal verification expert; Marvin is dressed in a black suit and playing a role, and Kayla is his "colleague" or "assistant" (Marvin didn't think he had the personality for it, but Kayla said he would be more convincing just because he is a middle-aged white man). THIS Marvin was chosen because he was the one who was spearheading the project because he was the one who was most motivated about preventing errors, after getting the "few time travel mishaps" shock during his introduction]
+
+["We're going to tell you something that may be difficult to believe. [But we will show evidence?] And this is totally optional – if at any time you want us to leave… it'll be as if we were never here"]
+
+[Anyway, Marvin was getting comfortable with the role because this wasn't their first time, a lot of people had already refused, and they had indeed used the time machines so they were never there. A bunch of people conspicuously didn't trust them because they look like some sort of CIA spooks, some of them even asked them to leave right away; many of them hadn't believed us even after we showed them the proof, or been angry about it; others had heard them out, but weren't comfortable going through with it; various details]
+
+[It had been a lot of debate for the ethics board to decide on this approach:]
+
+["We need to tell the truth as much as we can", Reginald had said. We lay down a whole bunch of things that needed to be gotten across and assembled the story about aliens
+
+K: "This is no good! No reasonable person would believe this –"
+
+R: "But every part of it has to be included, otherwise you're manipulating people –"
+
+K: "The what if we tell this to people and someone DOES believe it? There's no way they'd believe it unless they were super gullible. So then we'd basically be exploiting a gullible person."
+
+M: "actually, she's right, we absolutely do NOT want to get someone who is easy to convince that things will be fine, then they might accept code that is not reliable enough"
+
+So we realized we had to do something to prove it]
+
+["We can demonstrate the alien mind control. Think of something that we shouldn't be able to guess… It's 'Zorn's Lemma'". The mathematician looked somewhat taken aback. "Now look down at the paper in front of you." Which also had "Zorn's Lemma" written on it, in big marker. The mathematician was more disturbed now. "My colleague put that there while we were talking, but we used the alien mind control so that you wouldn't notice."]
+
+["That doesn't prove exactly what you claimed. It could perhaps be mind reading and teleportation"]
+
+["We need someone with your particular skills in formal verification. You see, what we are doing… It requires extremely reliable software, because a single mistake could allow the aliens to learn what we are doing, and then they would put all of Earth in danger"]
+
+[The mathematician was taking an interest, and seemed to be taking it seriously. I was surprised: we'd been clear about how he wouldn't have contact with any of his friends, and had, I thought, given him very little to motivate him to do it. But as I really understood later on, it appealed to him as follows: we were telling him that his particular field of study, which he had devoted his life to, was vital to the future of humanity, when most of the people he had to work with were impatient with his obsession with correctness.]
+
+[Yeah now we need to take you in this, uh, transporter (I stumbled over my words because we hadn't gotten to this part with anyone else), and then give you this vaccine against the alien brain parasites]
