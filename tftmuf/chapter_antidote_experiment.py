bdc9cf0cedc17e7df450f055412abf49c8252074
elidupree <vcs@elidupree.com>
2022-10-10 14:52:48 -0400

    writing work

--- tftmuf/chapter_antidote_experiment.py
+++ tftmuf/chapter_antidote_experiment.py
@@ -1,3 +1,20 @@
+[The plan: they go back in time, schedule a failure message to themselves, and then go to do the experiment; if successful, they cancel the failure message. Thus, in the case of success, the originals perceive the same thing as the new-originals (no message), but in case of failure, the new-originals see the failure message, check what happened, and do a stable time loop to fix it. For the experiment, the plan is for Aster to show up with the current Kayla shortly after the original Kayla escapes, pretending that they've just recaptured the original Kayla, and pulling rank to change the experiment, pretending it's on Fuller's instructions; unbeknownst to them, one of the assistants get suspicious and messages Fuller to ask if it's legit, at which point he goes for the time machines (maybe with the idea of stopping Aster before they start, or even just showing up immediately after the message instead of with a delay]
+
+
+
+
+
+
+"Why isn't he shooting us?" whispered Kayla.
+
+But my eyes were already going to the gun, [to the missing magazine. It was a good thing I was there, because only I, the one who was experienced with guns, knew what to look for. He had grabbed it in a hurry, not bothering to reload] "He has only one shot," I whispered back.
+
+Kayla and Aster exchanged a look [and both stepped forward, spreading out]
+
+To my horror, I realized what they were doing. [If Fuller shot one of them, he wouldn't have any shots left, and then the other 2 of us could rush him, then go back in time so this never happened. If you were willing to sacrifice your own life, so that another version of you would go on…]
+
+
+
 [I didn't dare approach Fuller myself, but maybe, while he was distracted by the others – I (fished in my pocket? Went for the backpack?), With the rough idea of texting one of my previous selves]
 
 But Fuller saw me move. "<em>Marvin</em>," he said warningly.
@@ -18,12 +35,62 @@ Aster took a step closer. "Precisely <em>because</em> of my memory disorder, I'v
 
 [Aster was trying to goad Fuller to take the attention off Kayla. Now it was Fuller's turn to start losing his composure. He yelled back, but now that his performance of authority had flickered, I couldn't see him as anything but pathetic]
 
-[A couple more lines of argument, Aster gets close enough that Fuller points the gun at them, and then Kayla rushes him]
+[A couple more lines of argument; Aster stepping close enough that Fuller feels threatened]
+
+Everything happened in an instant. [Aster was close enough that Fuller finally had to switch to them; he tried to keep it disciplined, swiftly sweeping the gun from one to the other. But the moment he began the movement, Kayla charged. She didn't waste time going for a kick or a grab – she just slammed her frail body into him at full speed, coming in past his guard so the gun was pointing uselessly into the air. They crashed to the floor with the sound of [equipment] and a high scream as it triggered Kayla's neuropathy [which of course she had known about and done willingly]]
+
+[A split-second later, Aster realized how the situation had transformed and what they needed to do. It was surprising to see, since they had clearly never been cut out for aggression. But they rushed forward to help Kayla, and as Fuller tried to recover himself, struggling to push Kayla off of him and get back control of the gun, Aster stomped on his wrist]
+
+"Marvin! Grab the gun!" Kayla screamed.
+
+"…Right!" [It had taken a bit longer for me to be shocked out of having been frozen, but Kayla's words achieved that. I ran forward to do the thing]
+
+[Heart beating hard; panic; but having seen the other Marvin handle the gun before shooting Fuller, firearms handling was fresh in my mind. I remembered how my father had taught me: "if you ever, ever have to pick up a gun while emotions are running high, that is the easiest time to make a mistake and kill someone you care about, and the most important time to focus on all of the details of safety". I crouched, careful to take it with a steady grip with my shaking hands, and then stepped away from the scuffle.]
+
+[It looked like Kayla and Aster were winning. I stood on the other side of the room, knowing that my most important duty now was to keep control of the gun and keep it away from the fight]
 
 
 
 
-"Lock the doors! Lock every door in the building! We'll sort it out later –"
+[They get the text from future Kayla: maybe "My phone buzzed; I wanted to keep talking to Kayla but she told me to check it in case it is important" or "I checked it" or "Kayla checked her phone". Regardless of which, "wait a minute, these are our future selves, the ones doing the experiment? They are not supposed to contact us, doesn't that mean they've just put us in a paradox?"]
+
+[The text message was one word long:]
+
+"Help".
+
+[Chapter break?]
+
+["Well if we're already in a paradox, there's no reason we CAN'T look at them [on the cameras]"]
+
+What we saw was the scene of a massacre.
+
+The bodies of all the assistants were strewn around the room. Some of them had vomited; one of them was slumped over a toppled medical cart, as if they had tried to cling to it in their final moments. [Whatever had happened, it was nothing like the silent, unsettling way Kayla and Aster had died from the "kill phrase"; one of them was still twitching on the ground, struggling for breath.]
+
+Of all the people in the room, only Kayla and Marvin were still alive. [And, contrary to our plans, it was Marvin who was on the cot, with the IV in his arm, and Kayla who was standing to the side. Only when we reviewed the footage later did we notice the details – how Kayla already had the bandage and her arm, how she had already received the treatment before administering it to Marvin.]
+
+[My head swam. I sank back in the chair, trying to process what I was seeing. "Jesus Christ, what happened to them?" Watching numbly; both Kayla and Marvin were terrified, and as we watched, Kayla's body begin to jerk violently. She collapsed to the floor, dying like the others.]
+
+But the Kayla next to me [intense focus on the screen, neck leaning forwards?] "Lock the doors! Lock them in the room, right now!"
+
+"W-what?"
+
+What I didn't have, that Kayla did, was Kayla's knowledge of herself. She knew that if the experiment was simply going to kill everyone in the room, the other Kayla would have no reason to message us, because she could simply allow the prearranged failure message to arrive. So if she messaged us for help, that meant a completely different form of danger, one that would need a response from us <em>immediately</em>. And our Kayla already had a guess of what it was.
+
+"It's in there with them! The thing that was following me! Lock it in!"
+
+"S-shouldn't we just copy down the footage and go back in time? So we have time to figure out –"
+
+"Right now is the ONLY time when we know where it is! This is our chance to trap it –"
+
+"But –"
+
+"Do it," said Aster, the most serious they'd ever sounded. "If she's wrong, we can sort it out later."
+
+"O-okay." [It was hard to type with my shaking, sweaty fingers, ; couple more details of the process]
+
+"And change the password! Change the password so Marvin can't unlock it again!"
+
+
 
 [Shouldn't we be going back in time?]
 
@@ -34,19 +101,24 @@ Aster took a step closer. "Precisely <em>because</em> of my memory disorder, I'v
 
 
 
+[The other Marvin tries to get through the door, obviously in distress; Marvin can't resist using the security cameras to talk to him. "You need to let me use the time machine!" "What's going on? Why?"]
+
+
 
 
 ["it's, uh, I can't tell you because you won't be able to understand"]
 
-[But ?????. I had spent a lot of time watching myself on camera in the last few days, and I'd learned more about my mannerisms than I had ever wanted to. So I very precisely recognized the tone of voice he was using – the tone of being excited to have realized something – in this case, excited to have thought of a plausible excuse
+[But ?????. I had spent a lot of time watching myself on camera in the last few days, and I'd learned more about my own mannerisms than I had ever wanted to. So I very precisely recognized the tone of voice he was using – the tone of being excited to have realized something – in this case, excited to have thought of a plausible excuse
 
 "Marvin, you need to turn off that [video feed] right now! You keep [spacing out], you're clearly seeing/hearing things you can't see, I don't want you to get distracted or die." "But what about needing to watch them?" "Watch the outside of the door!"
 
 
 
-["I've listened to the kill statement and I'm still alive, please come over here and take it yourself, I need your help to make sense of this"]
+["I've listened to the kill statement and I'm still alive, please come over here and take it yourself, I need your help to make sense of this" ("understood the kill phrase but don't understand the implications")]
 
-[why is "you can duplicate yourself with time travel"-ish the kill statement??]
+[Maybe if it hadn't been in such an high-stakes situation, I would have worried more about things like "possible permanent changes to my personality", but right now I was desperate to understand what was happening to us]
+
+[Opening the file labeled "kill phrase DO NOT PLAY THIS.ogg". The somewhat low-quality sound recording, "so you can understand everything now? Why is "you can duplicate yourself with time travel"-ish the kill statement??]
 
 
 
