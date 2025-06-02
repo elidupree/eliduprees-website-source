a7e8c045e430bb42045131c22fa57038d09be239
elidupree <vcs@elidupree.com>
2025-06-01 22:22:08 -0400

    writing work I forgot to commit a while back?

---
+++ tftmuf/chapter_marvin_alpha_talk.py
@@ -0,0 +1,64 @@
+[Marvin Alpha was being sour and avoiding me and the Broken Kayla]
+
+[I approached Marvins manager, "what's up with Alpha?"]
+
+["Have you talked to him?" "Weeeellllll,"]
+ 
+"Has <em>anyone</em> talked to him?!" I demanded.
+
+[I realized, not a single one of these Marvins was going to do anything about it. One thing that had rubbed off on me from Kaylas: if there's a problem, whether technical or social, the thing to do is immediately go and fix it.]
+
+"I'm going to go talk to him."
+
+"Woah, woah, woah! [He's in a really touchy/unstable mental state]! [You'll mess things up]"
+
+Me angry now, "what do you think's going to happen to his mental state from everyone avoiding him?" [Resonance of "I hate Marvin's! Marvin's never solve the problem!"]
+
+?????
+
+I wasn't <em>entirely</em> like Kayla. I didn't stomp over and knock on Alpha's door while I was still angry. I collected myself first.
+
+????? knock
+
+"can I come in? I just want to talk. It's me, Kazon"
+
+"go to hell!" [Even knowing this was going to be a conflict, I was surprised by how seriously aggrieved he sounded]
+
+?????
+
+[Marvin Alpha beat around the bush, POV pushes for what's the real issue]
+
+"you brought a Kayla here!"
+
+?????
+
+[If I can justify them being on cams right now:]
+
+[Alpha wants to say more [about hating Kayla] but glances at the cameras]
+
+[Kazon calls management, "requesting authorization to temporarily turn off cameras in this room, look we're in the interior of the building and we're not doing anything remotely time travel,]
+
+[... or possibly they are not normally that strict with the cams, so Alpha just carefully verifies that they aren't on cams, when they already aren't. "This room isn't on cams right? It's in the middle of the building and not a time travel lab, [checks on the computer anyway]"]
+
+"I hate Kaylas! I hate every single one of them, I never want to see any of them again, I don't want to hear their [negative description] voices,
+
+"I can't talk to anyone about this, I can't mess it up for the rest of you, the rest of you damn Marvins are working so well with her. And that was okay while I could just stay here and ignore that there were Kaylas in the other half of the building, but then YOU screwed it up! ?????"
+
+(Maybe Kazon gets defensive? It's not like he is a perfect therapist at this point anyway)
+
+"yeah, yeah, I know this damn Kaylas Management is important so we don't all get killed by Kayla any more than we're all going to get killed by Nochli. Why are you even talking to me? You should be out there saving the world, ?????"
+
+"what happened between you and her?"
+
+"I don't want to mess up the good thing you have going on,"
+
+"look, I get it! Do you think if you tell me you had a big fight, I won't be able to work with Kaylas tomorrow? I don't go a day without being frustrated with her either, I don't like being yelled at, I don't like standing around while she's arguing with each other about the finer points of when you're morally obligated to kill people. But –" <em>But at least she doesn't [avoid issues] like you Marvins.</em> I didn't say that one. "Look, just tell me what happened and then it will be over with
+
+"[some sort of protest, maybe "no you'll keep worrying about it"]"
+
+"Oh for God's sakes. Are you saying it's some kind of infohazard? If it's an infohazard then [we can do a time travel thing where you tell me and then if I'm harmed by it then we go back in time,]
+
+[Alpha blurting out an interruption] "[actual thing he was most upset about from the fight]"
+
+
+
---
+++ tftmuf/chapter_tftmuf.py
@@ -0,0 +1,65 @@
+
+[Nothing could have prepared me for seeing the truth of Salnthu with my own eyes.
+
+Yes, with Ontoh's input and the translations of Nochli's conversations, I knew many facts and figures: how they'd had a population of quadrillions, how they had bred human beings in tiny pens, ?????. It had been hard to hear, but I had struggled through those conversations, knowing that it was necessary, knowing that we were the only ones who could address this. And when you repeat something enough times, no matter how extreme or horrific, you can't react as strongly the hundreds time as the first. I had become numb to it. Or so I told myself.
+
+But when I saw the videos, the wound was torn fresh again.
+
+I'll spare you the horror of the [details of the slavery]. Kayla says that this is "lying" (you need to be able to acknowledge the truth of what happened), but I really believe no one should have to look at it – it's enough to know that it should never happen again.
+
+The view from above is bad enough.
+
+No continents can be seen on the sphere of the earth. [Colored regions of toxic clouds, not even in true color because the radiation damages the cameras, roiling vertices of the boundaries between the territories of different alchiia.]
+
+Underneath the clouds, there is no view to the horizon. All Nochlis are crammed into tiny workshops, meticulously isolated from their neighbors for time travel reasons. Their _ethra_, even more crammed. 
+
+How did it come to this?
+
+I will tell the story as best we were able to interpret it. We spent person-centuries studying those videos, ????? Even a planetful of historians could not document the full history of the septillions of subjective years that occurred there.
+
+<bigbreak>
+
+Time travel wasn't invented overnight.
+
+????? Duplication, but it took the best efforts of a blacksmith for a ?????month to prepare even one vessel,. You wouldn't use that to duplicate food, because it just wasn't worth it. You wouldn't even duplicate a pile of swords – the blacksmith could make more swords in less time.
+
+<em>we</em> had drawn up after the Industrial Revolution, and we were used to mass production – but what does an ancient village need with 10,000 of the same thing?
+
+[The duplication wars, where people made a lot of duplicate people and then didn't have enough food to sustain them, and then there was population collapse and duplication was often forbidden, and basically only a king could muster the resources]
+
+Our story begins with a king who thought differently.
+
+
+
+
+
+
+[What follows is a summary of the history, not exactly reflecting the order I want to explain it, or the attitude/perspective I want to use to explain it]
+
+[the history goes: there was a king who thought he was better than everyone else, who duplicated himself a bunch of times; but Nochli wasn't the king, he was a scribe. As the king duplicated himself, he had also duplicated Nochli, to organize his operations. But the difference between a king and a scribe is, a king isn't accustomed to working efficiently with other people as equals. As the king duplicated himself more and more, resentment began between different factions of the king, but the copies of Nochli always worked together with each other. Soon the Nochlis realized that the king's infighting was slowly getting them wiped out (by better-organized rivals), which would drive them to extinction if Nochli didn't do something, so Nochli worked together to kill (poison?) all the copies of the king and take over the faction themselves, collectively looking out for the interests of Nochli as a whole. And that's how a scribe and historian ended up being in charge of an entire faction]
+
+[the Donald Trumps and Elon Musks of our world wouldn't survive ten seconds in Salnthu, they need people under them to do the actual work]
+
+[The original human Nochli doesn't seem especially evil. He grew up upper-class in that society, and is notable for being intrinsically interested in history and scholarship. He travels to record the expertise of artisans, and pays them well for it, and respects the boundaries of those who wants to keep their secrets. He's opportunistic in a positive sense, leaving situations that don't work for him instead of making a fight about it, and maintains highly trusting relationships on an individual level; on the other hand, he doesn't rock the boat of the oppression he benefits from (he is kind and pays well to his own servants, who are theoretically free to quit, but he also does logistics for construction work performed by slaves, and only improves their lot when he sees a easy opportunity to, which is infrequent). If he never abused anyone individually back then, how did he end up vivisecting people later? Maybe it's because he hadn't found the Ring of Gyges yet… "he was an ordinary, complicated person, influenced by his society… until he found the Ring of Gyges, and became a monster." Wait, hang on. The Gyges story is about being unconstrained by social expectations, but the evils of the early Nochli are about _following_ social expectatio.]
+
+[Marvin angst about "but this early Nochli doesn't seem that evil". That was a topic of discussion with other Marvin's for a while; then I made the mistake/correct-decision of saying it in front of a Kayla. Kayla: "yes he does! he oversaw slave labor, and also [a bunch of other not quite as blatant things]!" "I mean yeah that's bad, but he didn't actually, you know, directly abuse anyone. And it was the king's orders, he couldn't just disobey," "yes he could! And obeying evil orders is more evil than giving them!" "...what" "if everyone gives evil orders and no one obeys them, then no one gets oppressed; if only one person gives evil orders but everyone obeys them, then everyone gets oppressed. The amount of oppression is proportional to the amount of obeyers, and the amount of tyrants is basically irrelevant" "okay but... he's doing the best he can in the society he's in. do you really think things would be better there if Nochli just left?"]
+
+[Human duplication was already known in his childhood, but it took decades to become the order of the whole world – in the meantime there were quite a few localized cases of mass duplication, but it usually resulted in disaster because of logistics issues (e.g. duplicating people faster than you can duplicate food for them, and then rebellion by the starving people breaks the human and material infrastructure that was being used to make more time machines.) Nochli's logistics skills contributed to pioneering more stable duplication (which the king later took advantage of), although Nochli wasn't quite an inventor, he mainly collected knowledge from others.]
+
+[The camera was impressively neutral, watching even the everyday moments of his life; the cameraflies had no concern for privacy. Other ones viewed society as a whole, both in Salnthu and Huvinthu]
+
+[Once the world was dominated by the factions of duplicates, Nochli was largely isolationist, defending his own territory vigorously but staying out of the conflicts of other factions that rose and fell. He used knowledge of historical conflicts to make sure he was not a desirable target to conquer, and make sure to know the strengths and weaknesses of each of his rivals, in case they did attempt it. As before, he didn't make major innovations himself, but copied and perfected the innovations by other factions. Like almost every faction, he maintained an ever-steeper power imbalance between the time travellers (his selves) and the non-time travellers he managed; Nochli maintained humanlike relationships with some of them, but those relationships steadily got more coercive and illusionary; security motivations caused Nochli to further and further restrict their access to the time machines, and by the end, anytime Nochli interacted with a non-time traveller as if there were equals, it was basically role-playing a relationship between equals]
+
+[Like Kayla, he made his tree immortal by duplicating copies of early selves]
+
+[Maybe he used copies of people he'd once had good relationships with, to role-play those relationships? But it got more and more isolating, because he couldn't actually share his current life with them. And as his appearance got less and less human, there was more explaining to do. Meanwhile, major advancements in brain surgery and biological treatments (which he learned from other factions, experimenting on non-Nochlis, and experimenting on Nochlis) allowed him to relieve the physical desires by removing them rather than indulging them, allowing him to become more focused in his pursuit of studies (like EliDupree, making himself more stimulated by the idea of things rather than the physical acts); he heavily optimized himself for his particular situation of "studying history using old time machines", "performing unethical breeding experiments on both humans and other organisms", and "being in constant conflict with other factions of time travellers"]
+
+[How did this change when he copied the immortality tech from Ontoh's faction?]
+
+[Ultimately he obsessed over Project Huvith; Marvin sees this via a bunch of physical details of the project, so it takes a while to infer what the motivations were, but they are basically (1) because of his long-term thinking, Nochli realizes the inevitably that some other faction will eventually make a superweapon and wipe him out, unless there is first a complete resolution to the conflicts; (2) he still remembers how he grew up without much time travel, and misses the human relationships from that world (and it's hard to replicate them; he makes experimental societies (like Nemesis from Odyssey) but they basically have to live their entire lives in artificial environments inside time dilation chambers), and Nochli's overarching organizational approach favors spending spacetime on useful experiments rather than indulging in role-playing (in fact, perhaps he has had to take active steps to prevent Nochlis from spending all their time rewatching past events – theoretically motivated by external threats of course)]
+
+[Physically, Project Huvith had multiple research aspects: (1) finding the oldest time machines using repeated, brief excursions into the past; (2) developing the huvith themselves through repeated breeding experiments on artificial societies; (3) developing the assemblages of time machines (the collapsible ones for transporting large amounts of stuff in the tiny original time machine, the nested budget-extenders, and the synchronization system)]
+
+[here's where it gets plot-relevant: the synchronization system. In order to synchronize, Huvinthu must witness the same first-departure as Salnthu did, except with the Huvinthu instance of the ancient time machine instead of the Salnthu instance; the only way to do this is to copy the first-departure from Salnthu, but swap the time machines. So the departure takes place inside a pair of sealed chambers – the time travellers wait in the first chamber while the ancient time machine is placed in the second chamber from the other side, then they open the second chamber and use whatever time machine is there. To synchronize, the Huvinthu Nochlis first go back to and "steal" this entire assembly back to Huvinthu (before the ancient time machine is inserted), then place the Huvinthu instance in the second chamber and seal it. If this doesn't go exactly right, you get a paradox, but that's not a huge problem because the overseers on Salnthu can just initiate a retry from a recent backup of Huvinthu]
+
+[can Ontoh be prevented from understanding the synchronization system? It would certainly be the most secret part of the project – from the perspective of rival factions, the other parts don't necessarily indicate that they're about to perform a coup over the world. The huvith are useful for managing non-time travellers, the historical excursions are a known quirk of Nochli, the collapsible time machines and budget-extenders are probably just improvements of existing technology… but the synchronization chambers have little use in Salnthu (why do you care to send a physical copy of something from the present into the past and have it stick, when you can't go very far back anyway because of paradox traps? If you wanted to try something destructive and obtain the resulting physical entity, you could just do it in an alternate past and "steal" it). So Nochli might be careful about access to the knowledge of the synchronization chamber project, such that the large majority of Nochli actually DON'T know how total the coup is going to be, and think it is just a particularly large historical excursion (and only the few Nochli who were "in the know" would actually go on the excursion themselves); Ontoh could plausibly have only learned this more widespread knowledge, then followed one of the various teams who were loading the goods for the excursion (Ontoh would then pass through the synchronization chambers and be able to describe them to the protagonists without knowing what they were for)]
